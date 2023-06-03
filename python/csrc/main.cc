1193fd7664969f3335658717ced1e93c9118ae60
Wang, Eikan <eikan.wang@intel.com>
2023-06-03 04:33:26 +0000

    Rename libintel_extension_backend_for_triton to libintel_xpu_backend_for_triton

--- python/csrc/main.cc
+++ python/csrc/main.cc
@@ -2,7 +2,7 @@
 
 void init_intel_xpu_backend_for_triton(pybind11::module &m);
 
-PYBIND11_MODULE(libintel_extension_for_triton, m) {
+PYBIND11_MODULE(libintel_xpu_backend_for_triton, m) {
   m.doc() = "Python bindings to the C++ Intel XPU Backend for Triton API";
   init_intel_xpu_backend_for_triton(m);
 }
