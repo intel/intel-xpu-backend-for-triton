7dc35b1f93951c6ce18f9427a7e54c744d9b1774
Stonepia <tong.su@intel.com>
2023-08-31 01:39:28 +0000

    update scripts for the test

--- .github/scripts/case_update.sh
+++ .github/scripts/case_update.sh
@@ -10,8 +10,9 @@ sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-run
 sed -i '/def test_abs_fp8(in_dtype, device):/ a\    pytest.skip("fp8 is not supported for xpu")' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
 sed -i 's/MmaLayout(/# &/' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
 sed -i 's/f.name/&, device_type=device/' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "float8e4", "float8e5"]:\n        pytest.skip("fp8 is not supported yet")' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
+sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "tl.float8e4b15x4", "float8e4nv", "float8e5"]:\n        pytest.skip("fp8 is not supported yet")' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
 sed -i "/ptx = pgm.asm\['ptx'\]/,/^$/d" ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
+sed -i "/ptx = pgm.asm\["ptx"\]/,/^$/d" ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
 
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_blocksparse.py
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_cross_entropy.py
