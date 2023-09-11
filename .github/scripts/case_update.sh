80771a2a454ab1e6247e296c5a2989b18eec0ee3
Zhong, Ruijie <ruijie.zhong@intel.com>
2023-09-11 06:50:00 +0000

    modify case update path

--- .github/scripts/case_update.sh
+++ .github/scripts/case_update.sh
@@ -1,29 +1,29 @@
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/interpreter/test_interpreter.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/interpreter/test_interpreter.py
 
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/assert_helper.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/print_helper.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_annotations.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_block_pointer.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_line_info.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_random.py
-sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i '/def test_abs_fp8(in_dtype, device):/ a\    pytest.skip("fp8 is not supported for xpu")' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i 's/MmaLayout(/# &/' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i 's/f.name/&, device_type=device/' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "tl.float8e4b15x4", "float8e4nv", "float8e5"]:\n        pytest.skip("fp8 is not supported yet")' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i "/ptx = pgm.asm\['ptx'\]/,/^$/d" ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i '/capability = torch.cuda.get_device_capability.*/i \    check_cuda_only(device)' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
-sed -i -r '/h\.asm\["ptx"\]/,/^$/d' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/assert_helper.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/print_helper.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/test_annotations.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/test_block_pointer.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/test_line_info.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/test_random.py
+sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i '/def test_abs_fp8(in_dtype, device):/ a\    pytest.skip("fp8 is not supported for xpu")' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i 's/MmaLayout(/# &/' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i 's/f.name/&, device_type=device/' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "tl.float8e4b15x4", "float8e4nv", "float8e5"]:\n        pytest.skip("fp8 is not supported yet")' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i "/ptx = pgm.asm\['ptx'\]/,/^$/d" ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i '/capability = torch.cuda.get_device_capability.*/i \    check_cuda_only(device)' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
+sed -i -r '/h\.asm\["ptx"\]/,/^$/d' ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
 # TODO : This following change will affect the test_dot, we need to modify this after the dot is enabled
-sed -i -r '/pgm\.asm\["ptx"\]/,/(assert "shared" not in red_code)|(^$)/d' test_core.py  ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/language/test_core.py
+sed -i -r '/pgm\.asm\["ptx"\]/,/(assert "shared" not in red_code)|(^$)/d' test_core.py  ${HOME}/triton-preci/triton/python/test/unit/language/test_core.py
 
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_blocksparse.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_cross_entropy.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_flash_attention.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_inductor.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/operators/test_matmul.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/operators/test_blocksparse.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/operators/test_cross_entropy.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/operators/test_flash_attention.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/operators/test_inductor.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/operators/test_matmul.py
 
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/runtime/test_autotuner.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/runtime/test_cache.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/runtime/test_launch.py
-# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/actions-runner/_work/intel-xpu-backend-for-triton/intel-xpu-backend-for-triton/triton/python/test/unit/runtime/test_subproc.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/runtime/test_autotuner.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/runtime/test_cache.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/runtime/test_launch.py
+# sed -i '/import torch/ a\import intel_extension_for_pytorch' ${HOME}/triton-preci/triton/python/test/unit/runtime/test_subproc.py
