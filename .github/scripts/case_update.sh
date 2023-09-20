08d67e276bd9ad98d1b3dfe3ab119fcdf06932d0
Mao, Yunfei <yunfei.mao@intel.com>
2023-09-19 18:54:28 -0700

    Fix UT:
    1. add missing skipped fp8 datatype
    2. Avoid (u)int16 % float16
    3. remove cuda / cpu_pinned for test pointer
    4. allow TypeError for overflow case

--- .github/scripts/case_update.sh
+++ .github/scripts/case_update.sh
@@ -11,13 +11,14 @@ sed -i '/import torch/ a\import intel_extension_for_pytorch' ${TRITON_ROOT}/trit
 sed -i '/def test_abs_fp8(in_dtype, device):/ a\    pytest.skip("fp8 is not supported for xpu")' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i 's/MmaLayout(/# &/' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i 's/f.name/&, device_type=device/' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
-sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "tl.float8e4b15x4", "float8e4nv", "float8e5"]:\n        pytest.skip("fp8 is not supported yet")' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
+sed -i '/pytest.skip("bfloat16 is only supported on NVGPU with cc >= 80")/ a\    if dtype in ["float8e4b15", "float8e4b15x4", "float8e4nv", "float8e5", tl.float8e4b15, tl.float8e4b15x4, tl.float8e4nv, tl.float8e5]:\n        pytest.skip("fp8 is not supported yet")' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i "/ptx = pgm.asm\['ptx'\]/,/^$/d" ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i '/capability = torch.cuda.get_device_capability.*/i \    check_cuda_only(device)' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i -r '/h\.asm\["ptx"\]/,/^$/d' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 # TODO : This following change will affect the test_dot, we need to modify this after the dot is enabled
 sed -i -r '/pgm\.asm\["ptx"\]/,/(assert "shared" not in red_code)|(^$)/d' test_core.py  ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 sed -i 's/slice_kernel\[(1,)](XBLOCK=32/slice_kernel\[(1,)](XBLOCK=32, device_type=device/g' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
+python ${TRITON_ROOT}/triton/third_party/intel_xpu_backend/.github/scripts/case_update.py ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${TRITON_ROOT}/triton/python/test/unit/operators/test_blocksparse.py
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${TRITON_ROOT}/triton/python/test/unit/operators/test_cross_entropy.py
