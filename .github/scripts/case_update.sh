7fa8da96270d7e971b6ca2a69a01fec048507feb
Stonepia <tong.su@intel.com>
2023-09-20 09:06:28 +0800

    Rebase the backend to align with upstream (#96)
    * Unify slow/fast reduce codegen
    This aligns with the upstream changes in https://github.com/openai/triton/pull/2220
    
    * Rebase from upstream #2287
    
    https://github.com/openai/triton/pull/2287
    
    * Port from (#2292)
    https://github.com/openai/triton/pull/2292
    
    * Fix the JIT error when the signature is empty
    
    * Fix the issue where threadsPerWarp Logic changed
    
    * Delete unused code
    
    * Update expected failure case
    
    * Update Triton Commit

--- .github/scripts/case_update.sh
+++ .github/scripts/case_update.sh
@@ -17,6 +17,7 @@ sed -i '/capability = torch.cuda.get_device_capability.*/i \    check_cuda_only(
 sed -i -r '/h\.asm\["ptx"\]/,/^$/d' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 # TODO : This following change will affect the test_dot, we need to modify this after the dot is enabled
 sed -i -r '/pgm\.asm\["ptx"\]/,/(assert "shared" not in red_code)|(^$)/d' test_core.py  ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
+sed -i 's/slice_kernel\[(1,)](XBLOCK=32/slice_kernel\[(1,)](XBLOCK=32, device_type=device/g' ${TRITON_ROOT}/triton/python/test/unit/language/test_core.py
 
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${TRITON_ROOT}/triton/python/test/unit/operators/test_blocksparse.py
 # sed -i '/import torch/ a\import intel_extension_for_pytorch' ${TRITON_ROOT}/triton/python/test/unit/operators/test_cross_entropy.py
--- lib/Conversion/TritonGPUToSPIRV/ConvertLayoutOpToSPIRV.cpp
+++ lib/Conversion/TritonGPUToSPIRV/ConvertLayoutOpToSPIRV.cpp
@@ -117,12 +117,30 @@ private:
     llvm_unreachable("unexpected layout in getMultiDimOffset");
   }
 
+  SmallVector<Value>
+  getWrappedMultiDimOffset(ConversionPatternRewriter &rewriter, Location loc,
+                           ArrayRef<Value> multiDimOffset,
+                           ArrayRef<unsigned> shape,
+                           SmallVector<unsigned> shapePerCTATile,
+                           SmallVector<int64_t> shapePerCTA) const {
+    unsigned rank = shape.size();
+    SmallVector<Value> multiDimOffsetWrapped(rank);
+    for (unsigned d = 0; d < rank; ++d) {
+      if (shapePerCTATile[d] > shapePerCTA[d])
+        multiDimOffsetWrapped[d] = urem(multiDimOffset[d], i32_val(shape[d]));
+      else
+        multiDimOffsetWrapped[d] = multiDimOffset[d];
+    }
+    return multiDimOffsetWrapped;
+  }
+
   // shared memory rd/st for blocked or mma layout with data padding
   void processReplica(Location loc, ConversionPatternRewriter &rewriter,
                       bool stNotRd, RankedTensorType type,
                       ArrayRef<unsigned> numCTAsEachRep,
                       ArrayRef<unsigned> multiDimRepId, unsigned vec,
                       ArrayRef<unsigned> paddedRepShape,
+                      ArrayRef<unsigned> origRepShape,
                       ArrayRef<unsigned> outOrd, SmallVector<Value> &vals,
                       Value smemBase) const {
     auto accumNumCTAsEachRep = product<unsigned>(numCTAsEachRep);
@@ -166,8 +184,11 @@ private:
         SmallVector<Value> multiDimOffset =
             getMultiDimOffset(layout, loc, rewriter, elemId, type,
                               multiDimCTAInRepId, shapePerCTATile);
-        Value offset =
-            linearize(rewriter, loc, multiDimOffset, paddedRepShape, outOrd);
+        SmallVector<Value> multiDimOffsetWrapped = getWrappedMultiDimOffset(
+            rewriter, loc, multiDimOffset, origRepShape, shapePerCTATile,
+            shapePerCTA);
+        Value offset = linearize(rewriter, loc, multiDimOffsetWrapped,
+                                 paddedRepShape, outOrd);
         auto elemPtrTy = ptr_ty(llvmElemTy, spirv::StorageClass::Workgroup);
         Value ptr = gep(elemPtrTy, bitcast(smemBase, elemPtrTy), offset);
         if (vec == 1) {
@@ -301,6 +322,7 @@ private:
                                                      rewriter, srcTy);
     unsigned inVec = 0;
     unsigned outVec = 0;
+    auto origRepShape = getRepShapeForCvtLayout(op);
     auto paddedRepShape = getScratchConfigForCvtLayout(op, inVec, outVec);
 
     unsigned outElems = getTotalElemsPerThread(dstTy);
@@ -322,7 +344,7 @@ private:
         else
           processReplica(loc, rewriter, /*stNotRd*/ true, srcTy,
                          inNumCTAsEachRep, multiDimRepId, inVec, paddedRepShape,
-                         outOrd, vals, smemBase);
+                         origRepShape, outOrd, vals, smemBase);
       } else {
         assert(0 && "ConvertLayout with input layout not implemented");
         return failure();
@@ -339,7 +361,8 @@ private:
         else
           processReplica(loc, rewriter, /*stNotRd*/ false, dstTy,
                          outNumCTAsEachRep, multiDimRepId, outVec,
-                         paddedRepShape, outOrd, outVals, smemBase);
+                         paddedRepShape, origRepShape, outOrd, outVals,
+                         smemBase);
       } else {
         assert(0 && "ConvertLayout with output layout not implemented");
         return failure();
