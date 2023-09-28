de02eeaba478d36d8d991971a5e73bbc6d20ab2c
chuanqiw <chuanqi.wang@intel.com>
2023-09-27 22:55:32 -0700

    [ci] Add oneAPI basekit version as input

--- .github/scripts/env_triton.sh
+++ .github/scripts/env_triton.sh
@@ -1,7 +1,8 @@
+oneapi_ver=${1:-2023.2.0}
 # compiler
-source ${HOME}/intel/oneapi/compiler/latest/env/vars.sh
+source ${HOME}/intel/oneapi/compiler/${oneapi_ver}/env/vars.sh
 # oneMKL
-source ${HOME}/intel/oneapi/mkl/latest/env/vars.sh
+source ${HOME}/intel/oneapi/mkl/${oneapi_ver}/env/vars.sh
 export MKL_DPCPP_ROOT=${MKLROOT}
 export LD_LIBRARY_PATH=${MKL_DPCPP_ROOT}/lib:${MKL_DPCPP_ROOT}/lib64:${MKL_DPCPP_ROOT}/lib/intel64:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=${MKL_DPCPP_ROOT}/lib:${MKL_DPCPP_ROOT}/lib64:${MKL_DPCPP_ROOT}/lib/intel64:$LIBRARY_PATH
@@ -10,7 +11,7 @@ export LIBRARY_PATH=${MKL_DPCPP_ROOT}/lib:${MKL_DPCPP_ROOT}/lib64:${MKL_DPCPP_RO
 source ${HOME}/intel/oneapi/tbb/latest/env/vars.sh
 
 # dnnl
-source ${HOME}/intel/oneapi/dnnl/latest/env/vars.sh
+source ${HOME}/intel/oneapi/dnnl/${oneapi_ver}/env/vars.sh
 
 # IPEX
 export USE_AOT_DEVLIST='12.55.8'
