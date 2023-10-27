148d96b839947aee20e64c1e7bd1d12b947237b9
chuanqiw <chuanqi.wang@intel.com>
2023-10-27 05:21:20 -0700

    [CI] Adjust inductor test parameters for ipex 2.1

--- .github/scripts/inductor_xpu_test.sh
+++ .github/scripts/inductor_xpu_test.sh
@@ -23,7 +23,14 @@ if [[ -n "$MODEL_ONLY" ]]; then
     Model_only_extra="--only ${MODEL_ONLY}"
 fi
 
-Mode_extra=""
+
+Cur_Ver=`pip list | grep "^torch " | awk '{print $2}' | cut -d"+" -f 1`
+if [ $(printf "${Cur_Ver}\n2.0.2"|sort|head -1) = "${Cur_Ver}" ]; then
+    Mode_extra="";
+else
+    # For PT 2.1
+    Mode_extra="--inference --freezing ";
+fi
 if [[ $MODE == "training" ]]; then
     echo "Testing with training mode."
     Mode_extra="--training "
