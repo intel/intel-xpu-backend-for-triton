304080325ced028177408f1b942868765381f7a6
Wang, Quintin <quintin.wang@intel.com>
2024-01-12 00:31:29 -0800

    update test_line_info.py due to add a new line

--- python/test/unit/language/test_line_info.py
+++ python/test/unit/language/test_line_info.py
@@ -3,7 +3,7 @@ import tempfile
 
 import pytest
 import torch
-import intel_extension_for_pytorch  # type: ignore # noqa: F401
+import intel_extension_for_pytorch
 
 import triton
 import triton.language as tl
@@ -153,17 +153,17 @@ def test_line_info(func: str):
     file_lines = extract_file_lines(kernel_info.asm["spv"])
 
     if func == "single":
-        assert (check_file_lines(file_lines, "test_line_info.py", 16))
         assert (check_file_lines(file_lines, "test_line_info.py", 17))
+        assert (check_file_lines(file_lines, "test_line_info.py", 18))
     elif func == "call":
-        assert (check_file_lines(file_lines, "test_line_info.py", 29))
-        assert (check_file_lines(file_lines, "test_line_info.py", 22))
-        assert (check_file_lines(file_lines, "test_line_info.py", 31))
+        assert (check_file_lines(file_lines, "test_line_info.py", 30))
+        assert (check_file_lines(file_lines, "test_line_info.py", 23))
+        assert (check_file_lines(file_lines, "test_line_info.py", 32))
     elif func == "call_noinline":
-        assert (check_file_lines(file_lines, "test_line_info.py", 43))
-        assert (check_file_lines(file_lines, "test_line_info.py", 36))
+        assert (check_file_lines(file_lines, "test_line_info.py", 44))
         assert (check_file_lines(file_lines, "test_line_info.py", 37))
         assert (check_file_lines(file_lines, "test_line_info.py", 38))
+        assert (check_file_lines(file_lines, "test_line_info.py", 39))
     elif func == "multi_files":
         assert (check_file_lines(file_lines, "test_line_info.py", 48))
         assert (check_file_lines(file_lines, "test_line_info.py", 50))
@@ -171,9 +171,9 @@ def test_line_info(func: str):
         assert (check_file_lines(file_lines, "standard.py", 34))
         assert (check_file_lines(file_lines, "standard.py", 36))
     elif func == "autotune":
-        assert (check_file_lines(file_lines, "test_line_info.py", 61))
         assert (check_file_lines(file_lines, "test_line_info.py", 62))
         assert (check_file_lines(file_lines, "test_line_info.py", 63))
+        assert (check_file_lines(file_lines, "test_line_info.py", 64))
     elif func == "dot_combine":
-        assert (check_file_lines(file_lines, "test_line_info.py", 73))
-        assert (check_file_lines(file_lines, "test_line_info.py", 74, should_contain=False))
+        assert (check_file_lines(file_lines, "test_line_info.py", 74))
+        assert (check_file_lines(file_lines, "test_line_info.py", 75, should_contain=False))
