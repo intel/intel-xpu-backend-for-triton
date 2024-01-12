b9a782dbee9aa20ef45ceb4b9c0e5cfccf1f9a7f
Sarbojit Sarkar <sarbojit.sarkar@intel.com>
2024-01-12 04:39:24 -0800

    reverting the script change

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -53,8 +53,7 @@ export TRITON_PROJ_BUILD=$TRITON_PROJ/python/build
 
 python3 -m pip install lit
 python3 -m pip install pytest
-#python3 -m pip install torch==1.13.0a0+git6c9b55e intel_extension_for_pytorch==1.13.120+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
-python3 -m pip install torch==2.0.1a0 torchvision==0.15.2a0 intel_extension_for_pytorch==2.0.110+xpu --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/
+python3 -m pip install torch==1.13.0a0+git6c9b55e intel_extension_for_pytorch==1.13.120+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
 if [ $? -ne 0 ]; then
   echo "FAILED: return code $?"
   exit $?
