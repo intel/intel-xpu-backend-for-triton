8a2fc2e48b15da2bf63156614951f927663a2a1c
Wang, Quintin <quintin.wang@intel.com>
2024-01-18 21:39:50 -0800

    fix a typo

--- lib/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -2623,18 +2623,18 @@ void mlir::triton::populateElementwiseOpToLLVMPatterns(
                                       benefit);
   patterns.add<ClampFOpConversion>(typeConverter, axisInfoAnalysis,
                                    computeCapability, target, benefit);
-  PatternBenefit benefitForProgNan = benefit;
+  PatternBenefit benefitForPropNan = benefit;
   if (target == mlir::triton::Target::GENX) {
     // TODO(FIXME): spirv's OpenCL extension (fmin/fmax) does not support
     // nan propagation. Set these conversion benefit to the max benefit:
     // PatternBenefit::ImpossibleToMatchSentinel - 1 to make sure the
     // correctness
-    benefitForProgNan = 65534;
+    benefitForPropNan = 65534;
   }
   patterns.add<MinMaxFOpConversion<arith::MinimumFOp>>(
       typeConverter, axisInfoAnalysis, computeCapability, target,
-      benefitForProgNan);
+      benefitForPropNan);
   patterns.add<MinMaxFOpConversion<arith::MaximumFOp>>(
       typeConverter, axisInfoAnalysis, computeCapability, target,
-      benefitForProgNan);
+      benefitForPropNan);
 }
