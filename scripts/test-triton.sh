07de7df4aec8818f8dfc73a8d322a535d7c42d20
prathams417 <pratham.sood@intel.com>
2024-01-24 15:33:42 -0700

    Update tutorial tests 05 and 11 to include fixme comments for tolerance

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -165,6 +165,7 @@ function run_tutorial_tests {
   run_tutorial_test "04-low-memory-dropout" 04-low-memory-dropout.py
   run_tutorial_test "05-layer-norm" 05-layer-norm.py
   run_tutorial_test "07-math-functions" 07-math-functions.py
+  run_tutorial_test "11-grouped-gemm" 11-grouped-gemm.py
 }
 
 function test_triton {
