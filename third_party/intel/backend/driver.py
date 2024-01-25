26e46b357ece206a876e36cee363f2cfc2d56ac6
Tsang, Whitney <whitney.tsang@intel.com>
2024-01-25 12:58:39 -0800

    Address review comment
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- third_party/intel/backend/driver.py
+++ third_party/intel/backend/driver.py
@@ -57,9 +57,8 @@ class XPUUtils(object):
         self.get_l0_imm_cmd_list = mod.get_l0_imm_cmd_list
         self.get_l0_dev_ptr = mod.get_l0_dev_ptr
         self.get_l0_ctxt_ptr = mod.get_l0_ctxt_ptr
-        import torch
-        self.context = mod.init_context(torch.xpu.current_stream().sycl_queue)
-        self.device_count = mod.init_devices(torch.xpu.current_stream().sycl_queue)
+        self.context = mod.init_context(self.get_sycl_queue())
+        self.device_count = mod.init_devices(self.get_sycl_queue())
         self.event_pool = mod.init_event_pool()[0]
         self.current_device = 0 if self.device_count[0] > 0 else -1
 
