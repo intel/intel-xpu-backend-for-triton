6357122a70048f901c556d65e750ec5b6781a4ae
Whitney Tsang <whitney.tsang@intel.com>
2024-02-02 03:55:37 +0000

    address review comment
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- scripts/compile-triton.sh
+++ scripts/compile-triton.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set +o xtrace
-if [ ! -z "$BASE" ]; then
+if [ -z "$BASE" ]; then
   echo "**** BASE is not given *****"
   BASE=$(cd $(dirname "$0")/../.. && pwd)
   echo "**** Default BASE is set to $BASE ****"
@@ -81,11 +81,11 @@ fi
 ############################################################################
 ## Configure and build the llvm project.
 
-if [ ! -z "$C_COMPILER" ]; then
+if [ -z "$C_COMPILER" ]; then
   C_COMPILER=`which gcc`
   echo "**** C_COMPILER is set to $C_COMPILER ****"
 fi
-if [ ! -z "$CXX_COMPILER" ]; then
+if [ -z "$CXX_COMPILER" ]; then
   CXX_COMPILER=`which g++`
   echo "**** CXX_COMPILER is set to $CXX_COMPILER ****"
 fi
