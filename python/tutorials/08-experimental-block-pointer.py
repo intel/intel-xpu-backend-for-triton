Fix up precision
Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

python/tutorials/08-experimental-block-pointer.py
@@ -228,7 +228,8 @@ torch_output = torch.matmul(a, b)
 print(f"triton_output={triton_output}")
 print(f"torch_output={torch_output}")
 
-#FIXME: Once tl.dot is lowered to DPAS instructions put back the precision to (atol=1e-2).
+# Note: the torch.matmul implementation uses a different algorithm so
+# we need to raise atol.
 if torch.allclose(triton_output, torch_output, atol=4e-2, rtol=0):
     print("✅ Triton and Torch match")
 else: