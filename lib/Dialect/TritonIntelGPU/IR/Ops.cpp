a2cfdc52f01ca543f9d172ca87b258920f949308
Lu, Chengjun <chengjun.lu@intel.com>
2024-02-21 16:23:40 +0000

    Add prefetch ops

--- lib/Dialect/TritonIntelGPU/IR/Ops.cpp
+++ lib/Dialect/TritonIntelGPU/IR/Ops.cpp
@@ -7,7 +7,17 @@
 namespace mlir {
 namespace triton {
 namespace gpu {
-namespace intel {} // namespace intel
+namespace intel {
+
+void PrefetchCacheOp::getEffects(
+    SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
+        &effects) {
+  // The write effect to protect optimization to move the ops away.
+  effects.emplace_back(MemoryEffects::Write::get(),
+                       SideEffects::DefaultResource::get());
+}
+
+} // namespace intel
 } // namespace gpu
 } // namespace triton
 } // namespace mlir
