45756ab6552ab25a972d9a5016586bb45d655d50
Whitney Tsang <whitney.tsang@intel.com>
2024-02-22 19:20:41 -0700

    Remove unnecessary rounding mode
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/lib/TritonIntelGPUToLLVM/ElementwiseOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -1669,10 +1669,7 @@ struct FpToFpOpConversion
     } break;
     case mlir::triton::Target::GENX: {
       auto ctx = rewriter.getContext();
-      // TODO: Remove unnecessary rounding mode.
-      return rewriter.create<GENX::FpToFpOp>(
-          loc, f32_ty, v,
-          GENX::RoundingModeAttr::get(ctx, GENX::RoundingMode::RTE));
+      return rewriter.create<GENX::FpToFpOp>(loc, f32_ty, v);
     }
     default:
       assert(false && "TODO");
