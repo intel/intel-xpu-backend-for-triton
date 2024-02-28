1b93a714134a2d5fc03055fb0199d91f424b13a6
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-02-28 01:33:49 +0000

    [GEN]: Add conversion patterns for GEN dialect in Triton - part 2
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

---
+++ third_party/intel/include/GENToLLVM/GENToLLVMPass.h
@@ -0,0 +1,34 @@
+//===- GENToLLVMPass.h - GEN to LLVM dialect conversion ---------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TRITON_CONVERSION_GENTOLLVMPASS_H
+#define TRITON_CONVERSION_GENTOLLVMPASS_H
+
+#include <memory>
+
+namespace mlir {
+
+class DialectRegistry;
+class LLVMTypeConverter;
+class RewritePatternSet;
+class Pass;
+
+#define GEN_PASS_DECL_CONVERTGENTOLLVM
+#include "intel/include/GENToLLVM/Passes.h.inc"
+
+namespace triton {
+
+void populateGENToLLVMConversionPatterns(LLVMTypeConverter &converter,
+                                         RewritePatternSet &patterns);
+
+void registerConvertGENToLLVMInterface(DialectRegistry &registry);
+
+} // namespace triton
+} // namespace mlir
+
+#endif // TRITON_CONVERSION_GENTOLLVMPASS_H
---
+++ third_party/intel/include/GENToLLVM/Passes.h
@@ -0,0 +1,22 @@
+//===- Passes.h - GEN Conversion Pass Construction and Registration -------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef GEN_CONVERSION_PASSES_H
+#define GEN_CONVERSION_PASSES_H
+
+#include "intel/include/GENToLLVM/GENToLLVMPass.h"
+
+namespace mlir {
+
+/// Generate the code for registering the conversion pass.
+#define GEN_PASS_REGISTRATION
+#include "intel/include/GENToLLVM/Passes.h.inc"
+
+} // namespace mlir
+
+#endif // GEN_CONVERSION_PASSES_H
