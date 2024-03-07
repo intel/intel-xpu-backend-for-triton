508a7782e0abd1022312d93ed5261952483490d6
Dewei Wang <dewei.wang@intel.com>
2024-03-07 04:14:11 -0800

    [BACKEND][TRANSFORM] add a pass to prefetch around a loop

--- include/triton/Dialect/TritonGPU/Transforms/Passes.h
+++ include/triton/Dialect/TritonGPU/Transforms/Passes.h
@@ -16,6 +16,8 @@ std::unique_ptr<Pass> createAccelerateMatmulPass(int computeCapability = 80);
 
 std::unique_ptr<Pass> createPrefetchPass();
 
+std::unique_ptr<Pass> createPrefetchBlockPass(int numWarps = 4);
+
 std::unique_ptr<Pass> createCoalescePass();
 
 std::unique_ptr<Pass> createReorderInstructionsPass();
