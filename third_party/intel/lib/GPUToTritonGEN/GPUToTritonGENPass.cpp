f4132b680756be0e0749d44ad62490be9818d566
Whitney Tsang <whitney.tsang@intel.com>
2024-03-12 12:18:18 -0700

    [GPUToGEN] Add math functions lowering
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/lib/GPUToTritonGEN/GPUToTritonGENPass.cpp
+++ third_party/intel/lib/GPUToTritonGEN/GPUToTritonGENPass.cpp
@@ -174,16 +174,52 @@ void mlir::triton::populateGPUToTritonGENConversionPatterns(
       StringAttr::get(&converter.getContext(),
                       TritonGEN::TritonGENDialect::getKernelFuncAttrName()));
 
-  const llvm::StringRef prefix("_Z15__spirv_ocl_");
-
-  populateOpPatterns<math::ExpOp>(converter, patterns, (prefix + "expf").str(),
-                                  (prefix + "expd").str());
-  populateOpPatterns<math::LogOp>(converter, patterns, (prefix + "logf").str(),
-                                  (prefix + "logd").str());
-  populateOpPatterns<math::CosOp>(converter, patterns, (prefix + "cosf").str(),
-                                  (prefix + "cosd").str());
-  populateOpPatterns<math::SinOp>(converter, patterns, (prefix + "sinf").str(),
-                                  (prefix + "sind").str());
+  populateOpPatterns<math::AbsFOp>(converter, patterns, "__imf_fabsf",
+                                   "__imf_fabs");
+  populateOpPatterns<math::AtanOp>(converter, patterns, "__imf_atanf",
+                                   "__imf_atan");
+  populateOpPatterns<math::Atan2Op>(converter, patterns, "__imf_atan2f",
+                                    "__imf_atan2");
+  populateOpPatterns<math::CbrtOp>(converter, patterns, "__imf_cbrtf",
+                                   "__imf_cbrt");
+  populateOpPatterns<math::CeilOp>(converter, patterns, "__imf_ceilf",
+                                   "__imf_ceil");
+  populateOpPatterns<math::CosOp>(converter, patterns, "__imf_cosf",
+                                  "__imf_cos");
+  populateOpPatterns<math::ErfOp>(converter, patterns, "__imf_erff",
+                                  "__imf_erf");
+  populateOpPatterns<math::ExpOp>(converter, patterns, "__imf_expf",
+                                  "__imf_exp");
+  populateOpPatterns<math::Exp2Op>(converter, patterns, "__imf_exp2f",
+                                   "__imf_exp2");
+  populateOpPatterns<math::ExpM1Op>(converter, patterns, "__imf_expm1f",
+                                    "__imf_expm1");
+  populateOpPatterns<math::FloorOp>(converter, patterns, "__imf_floorf",
+                                    "__imf_floor");
+  populateOpPatterns<arith::RemFOp>(converter, patterns, "__imf_fmodf",
+                                    "__imf_fmod");
+  populateOpPatterns<math::LogOp>(converter, patterns, "__imf_logf",
+                                  "__imf_log");
+  populateOpPatterns<math::Log1pOp>(converter, patterns, "__imf_log1pf",
+                                    "__imf_log1p");
+  populateOpPatterns<math::Log10Op>(converter, patterns, "__imf_log10f",
+                                    "__imf_log10");
+  populateOpPatterns<math::Log2Op>(converter, patterns, "__imf_log2f",
+                                   "__imf_log2");
+  populateOpPatterns<math::PowFOp>(converter, patterns, "__imf_powf",
+                                   "__imf_pow");
+  populateOpPatterns<math::RsqrtOp>(converter, patterns, "__imf_rsqrtf",
+                                    "__imf_rsqrt");
+  populateOpPatterns<math::SinOp>(converter, patterns, "__imf_sinf",
+                                  "__imf_sin");
+  populateOpPatterns<math::SqrtOp>(converter, patterns, "__imf_sqrtf",
+                                   "__imf_sqrt");
+  populateOpPatterns<math::TanhOp>(converter, patterns, "__imf_tanhf",
+                                   "__imf_tanh");
+  populateOpPatterns<math::TanOp>(converter, patterns, "__imf_tanf",
+                                  "__imf_tan");
+  populateOpPatterns<math::ErfOp>(converter, patterns, "__imf_erff",
+                                  "__imf_erf");
 }
 
 std::unique_ptr<OperationPass<mlir::gpu::GPUModuleOp>>
