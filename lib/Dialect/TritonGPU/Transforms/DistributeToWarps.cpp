4a65794c267e66a7f9d5078d790968a54bc75661
Dewei Wang <dewei.wang@intel.com>
2024-03-13 23:33:00 -0700

    fix rebase error

--- lib/Dialect/TritonGPU/Transforms/DistributeToWarps.cpp
+++ lib/Dialect/TritonGPU/Transforms/DistributeToWarps.cpp
@@ -6,6 +6,7 @@
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "triton/Analysis/Utility.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/Triton/IR/Utility.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
