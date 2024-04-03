a3f1abd39a4604a1bdf255ee87dbd28f280e493f
Whitney Tsang <whitney.tsang@intel.com>
2024-04-03 23:11:44 +0000

    Fix typo
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -151,7 +151,7 @@ run_tutorial_tests() {
   run_tutorial_test "03-matrix-multiplication"
   run_tutorial_test "04-low-memory-dropout"
   run_tutorial_test "05-layer-norm"
-  run_tutorial_test "06-fused-attention.py"
+  run_tutorial_test "06-fused-attention"
   run_tutorial_test "07-extern-functions"
   run_tutorial_test "08-grouped-gemm"
   run_tutorial_test "09-experimental-block-pointer"
