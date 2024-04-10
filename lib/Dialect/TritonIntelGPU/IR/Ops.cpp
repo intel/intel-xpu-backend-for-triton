cc5254a7deee8440f404be7271ac4867ddf15f2c
Ettore Tiotto <ettore.tiotto@intel.com>
2024-04-10 15:04:17 -0400

    [NFI]: Cleanup for TritonIntelGPU (#854)
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- lib/Dialect/TritonIntelGPU/IR/Ops.cpp
+++ lib/Dialect/TritonIntelGPU/IR/Ops.cpp
@@ -1,3 +1,11 @@
+//===- Ops.cpp - TritonIntelGPU Operations ------------------------------*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "mlir/IR/Builders.h"
 #include "triton/Dialect/TritonIntelGPU/IR/Dialect.h"
 
