774b49dc1b5a876a5caab2747d14eac0b3309b4c
Si, Yudong <yudong.si@intel.com>
2024-04-09 19:07:52 -0700

    update issue number

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -1035,7 +1035,7 @@ def test_precise_math(expr_prec, expr_ref, num_ctas, device):
     kernel[(1, )](x, y, out, out_ref, BLOCK=shape[0], num_ctas=num_ctas)
 
     if is_xpu():
-        # use cpu result as reference, see https://github.com/intel/llvm/issues/13329
+        # use cpu result as reference, see https://github.com/llvm/llvm-project/issues/88222
         out_ref = torch.div(x.to(torch.float64).cpu(), y.to(torch.float64).cpu()).to(torch.float32).to(device=device)
     assert torch.all(out == out_ref)  # bitwise exact
 
