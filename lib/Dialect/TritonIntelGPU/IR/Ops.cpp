06e42392fbc6c976699be87b4594b8e25f37d30c
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-04-10 17:04:46 +0000

    Address code review comments
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- lib/Dialect/TritonIntelGPU/IR/Ops.cpp
+++ lib/Dialect/TritonIntelGPU/IR/Ops.cpp
@@ -1,4 +1,4 @@
-//===----------------------------------------------------------------------===//
+//===- Ops.cpp - TritonIntelGPU Operations ------------------------------*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
