4730ca7fe98f61bb847433f030d93ff9d6fe802c
Wang, Quintin <quintin.wang@intel.com>
2024-04-12 03:22:21 +0000

    Fix UT error

--- python/test/unit/runtime/test_subproc.py
+++ python/test/unit/runtime/test_subproc.py
@@ -40,7 +40,7 @@ def test_compile_in_subproc() -> None:
         major, minor = torch.cuda.get_device_capability(0)
         cc = major * 10 + minor
     elif torch.xpu.is_available():
-        capability = torch.xpu.device(torch.xpu.current_device()).sycl_device
+        cc = torch.xpu.device(torch.xpu.current_device()).sycl_device
 
     config = triton.compiler.AttrsDescriptor(tuple(range(4)), ())
 
