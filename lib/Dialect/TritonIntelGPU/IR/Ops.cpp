ee28033ede8ca3bf09cf6afcfc200a6f714da31a
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-04-18 20:17:31 +0000

    [NFI]: Rename ExtractOp argument to
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- lib/Dialect/TritonIntelGPU/IR/Ops.cpp
+++ lib/Dialect/TritonIntelGPU/IR/Ops.cpp
@@ -192,7 +192,7 @@ OpFoldResult ExtractOp::fold(FoldAdaptor adaptor) {
 
   // %0 =  .... : tensor<16x8xf16>
   // extract %0[0] : tensor<16x8xf16> -> %0
-  if (getIndex() == 0 && getOperand().getType() == getType())
+  if (getIndex() == 0 && getBase().getType() == getType())
     return getOperand();
 
   return {};
