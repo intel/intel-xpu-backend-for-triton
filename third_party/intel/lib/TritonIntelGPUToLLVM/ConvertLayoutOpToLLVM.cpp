a25ab783f67c93ed3ef7428dfb6aaab14bb303aa
Ilya Enkovich <ilya.enkovich@intel.com>
2024-04-18 15:08:07 -0700

    Fix uninitialized variables.

--- third_party/intel/lib/TritonIntelGPUToLLVM/ConvertLayoutOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/ConvertLayoutOpToLLVM.cpp
@@ -242,7 +242,7 @@ private:
       // [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]
       // clang-format on
       auto sizePerThreads = getSizePerThread(dpasLayout);
-      int rowsPerWarp = rowsPerWarp =
+      int rowsPerWarp =
           dpasLayout.getSubGroupSize() / dpasLayout.getExecutionSize();
       SmallVector<Value> elemOffset = {
           i32_val((elemId / sizePerThreads[1]) * rowsPerWarp),
