64e7b839dce4e3704263464c123516c7ed4c1e3b
Whitney Tsang <whitney.tsang@intel.com>
2024-04-18 21:01:08 -0400

    [NFI] Copy RemoveLayoutConversions under TritonIntelGPU (#922)
    This PR copies RemoveLayoutConversions pass from common area to
    TritonIntelGPU.
    This is to prepare specializing RemoveLayoutConversions for Intel
    backend.
    
    Other than name differences, the only difference is 
    ```
    242c242
    <   if (isa<DotOp, nvidia_gpu::DotAsyncOp, AtomicRMWOp, AtomicCASOp>(op))
    ---
    >   if (isa<DotOp, AtomicRMWOp, AtomicCASOp>(op))
    359c359
    <             ConvertLayoutOp, nvidia_gpu::DotWaitOp>(user)) {
    ---
    >             ConvertLayoutOp>(user)) {
    778c778
    <           ConvertLayoutOp, nvidia_gpu::DotWaitOp>(op)) {
    ---
    >           ConvertLayoutOp>(op)) {
    ```

--- include/triton/Dialect/TritonIntelGPU/Transforms/Passes.h
+++ include/triton/Dialect/TritonIntelGPU/Transforms/Passes.h
@@ -28,6 +28,8 @@ std::unique_ptr<Pass> createTritonIntelGPUAccelerateMatmulPass(
 
 std::unique_ptr<Pass> createTritonIntelGPUDistributeToWarpsPass();
 
+std::unique_ptr<Pass> createTritonIntelGPURemoveLayoutConversionsPass();
+
 std::unique_ptr<Pass> createPrefetchBlockPass();
 
 std::unique_ptr<Pass> createMatchTargetSizePass();
