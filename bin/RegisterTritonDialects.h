dd5409fb66713ae501f70d3275ad4ae4513e14f6
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-05-03 19:42:55 +0000

    Revert unnecessary change
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -5,6 +5,7 @@
 #include "intel/include/TritonGENToLLVM/Passes.h"
 #include "intel/include/TritonIntelGPUToLLVM/Passes.h"
 #include "third_party/nvidia/include/Dialect/NVGPU/IR/Dialect.h"
+
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
