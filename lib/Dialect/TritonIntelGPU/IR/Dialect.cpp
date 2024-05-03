f341c4a1174d8dc91e18e8054c458f3ceead2f9b
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-05-03 15:52:09 +0000

    Fix precommit
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- lib/Dialect/TritonIntelGPU/IR/Dialect.cpp
+++ lib/Dialect/TritonIntelGPU/IR/Dialect.cpp
@@ -352,14 +352,17 @@ void DpasEncodingAttr::print(AsmPrinter &printer) const {
   auto shapeC = getShapeC();
   llvm::ArrayRef<unsigned> rC = shapeC;
   auto warpsPerCTA = getWarpsPerCTA();
-  printer << "<{" << "repeatCount = " << getRepeatCount() << ", "
+  printer << "<{"
+          << "repeatCount = " << getRepeatCount() << ", "
           << "systolicDepth = " << getSystolicDepth() << ", "
           << "executionSize = " << getExecutionSize() << ", "
           << "opsPerChan = " << getOpsPerChannel() << ", "
           << "threadsPerWarp = " << getSubGroupSize() << ", "
           << "warpsPerCTA = [" << llvm::ArrayRef<unsigned>(warpsPerCTA) << "], "
-          << "A = [" << rA << "], " << "B = [" << rB << "], " << "C = [" << rC
-          << "]" << "}>";
+          << "A = [" << rA << "], "
+          << "B = [" << rB << "], "
+          << "C = [" << rC << "]"
+          << "}>";
 }
 
 //===----------------------------------------------------------------------===//
@@ -427,10 +430,13 @@ Attribute WarpEncodingAttr::parse(AsmParser &parser, Type type) {
 void WarpEncodingAttr::print(mlir::AsmPrinter &printer) const {
   auto threadsPerWarp = getThreadsPerWarp();
   auto sizePerThread = getSizePerThread();
-  printer << "<{" << "sizePerThread = ["
-          << llvm::ArrayRef<unsigned>(sizePerThread) << "]"
+  printer << "<{"
+          << "sizePerThread = [" << llvm::ArrayRef<unsigned>(sizePerThread)
+          << "]"
           << ", threadsPerWarp = [" << llvm::ArrayRef<unsigned>(threadsPerWarp)
-          << "]" << ", order = [" << getOrder() << "]" << "}>";
+          << "]"
+          << ", order = [" << getOrder() << "]"
+          << "}>";
 }
 
 //===----------------------------------------------------------------------===//
