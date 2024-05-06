674f44712ee5328e20a6cd8c08cdf6e25ebe4078
Si, Yudong <yudong.si@intel.com>
2024-05-06 09:40:08 +0000

    add triton-benchmark action

--- benchmark/cmake/FindXeTLALibrary.cmake
+++ benchmark/cmake/FindXeTLALibrary.cmake
@@ -18,7 +18,10 @@ if (NOT XeTLALibrary_FOUND)
             SOURCE_DIR ${XeTLALibrary_SOURCE_DIR}
             )
 
-    FetchContent_Declare(xetla-library)
+    FetchContent_GetProperties(xetla-library)
+    if(NOT xetla-library_POPULATED)
+       FetchContent_Populate(xetla-library)
+    endif()
 
     # add the XeTLA library.
     #set(XETLA_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/xetla/include)
