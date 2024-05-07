257578917644f514c8d7d908522106c650297378
Vadim Musin <vadim.musin@intel.com>
2024-05-07 02:10:42 +0200

    Fix tag version

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -75,7 +75,7 @@ export SCRIPTS_DIR=$(cd $(dirname "$0") && pwd)
 python3 -m pip install lit pytest pytest-xdist pytest-rerunfailures pytest-select
 
 if [ "$TRITON_TEST_WARNING_REPORTS" == true ]; then
-    python3 -m pip install git+https://github.com/kwasd/pytest-capturewarnings-ng@feature/main
+    python3 -m pip install git+https://github.com/kwasd/pytest-capturewarnings-ng@v1.0
 fi
 
 source $SCRIPTS_DIR/pytest-utils.sh
