67bbdb36a400b11e6b251a58ed34298dccb36975
Vadim Musin <vadim.musin@intel.com>
2024-05-08 15:08:58 +0200

    Fix package reference

--- python/setup.py
+++ python/setup.py
@@ -602,7 +602,7 @@ setup(
             "numpy",
             "pytest",
             "scipy>=1.7.1",
-            "pytest-capturewarnings-ng @ git+https://github.com/kwasd/pytest-capturewarnings-ng.git@v1.1"
+            "pytest-capturewarnings-ng @ git+https://github.com/kwasd/pytest-capturewarnings-ng.git@v1.1#egg=src/pytest_capturewarnings_ng"
         ],
         "tutorials": [
             "matplotlib",
