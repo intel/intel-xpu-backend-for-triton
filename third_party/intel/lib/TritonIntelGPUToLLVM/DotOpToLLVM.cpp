090e23ce237d1693932f9d7cd443b08390174694
Lu, Chengjun <chengjun.lu@intel.com>
2024-05-10 18:55:49 +0000

    Add tensor print utils.

--- third_party/intel/lib/TritonIntelGPUToLLVM/DotOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/DotOpToLLVM.cpp
@@ -9,14 +9,18 @@ using ::mlir::triton::gpu::intel::DpasEncodingAttr;
 namespace fma_details {
 LogicalResult convertDPAS(triton::DotOp op, triton::DotOp::Adaptor adaptor,
                           TritonIntelGPUToLLVMTypeConverter *typeConverter,
-                          ConversionPatternRewriter &rewriter);
+                          ConversionPatternRewriter &rewriter,
+                          const TargetInfoBase &targetInfo);
 } // namespace fma_details
 
 namespace {
 struct DotOpConversion : public ConvertTritonGPUOpToLLVMPattern<triton::DotOp> {
-  using ConvertTritonGPUOpToLLVMPattern<
-      triton::DotOp>::ConvertTritonGPUOpToLLVMPattern;
+  DotOpConversion(LLVMTypeConverter &converter,
+                  const TargetInfoBase &targetInfo, PatternBenefit benefit)
+      : ConvertTritonGPUOpToLLVMPattern<triton::DotOp>(converter, benefit),
+        targetInfo(targetInfo) {}
 
+  const TargetInfoBase &targetInfo;
   LogicalResult
   matchAndRewrite(triton::DotOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
@@ -33,8 +37,8 @@ struct DotOpConversion : public ConvertTritonGPUOpToLLVMPattern<triton::DotOp> {
 
     if (!isOuter && isa<DpasEncodingAttr>(
                         cast<RankedTensorType>(D.getType()).getEncoding())) {
-      return fma_details::convertDPAS(op, adaptor, getTypeConverter(),
-                                      rewriter);
+      return fma_details::convertDPAS(op, adaptor, this->getTypeConverter(),
+                                      rewriter, targetInfo);
     }
 
     if (isa<BlockedEncodingAttr>(
@@ -48,7 +52,7 @@ struct DotOpConversion : public ConvertTritonGPUOpToLLVMPattern<triton::DotOp> {
 } // namespace
 
 void mlir::triton::intel::populateDotOpToLLVMPatterns(
-    LLVMTypeConverter &typeConverter, RewritePatternSet &patterns,
-    PatternBenefit benefit) {
-  patterns.add<DotOpConversion>(typeConverter, benefit);
+    LLVMTypeConverter &typeConverter, const TargetInfoBase &targetInfo,
+    RewritePatternSet &patterns, PatternBenefit benefit) {
+  patterns.add<DotOpConversion>(typeConverter, targetInfo, benefit);
 }
