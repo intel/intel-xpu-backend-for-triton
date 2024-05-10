97db3c562c80623f74fc9756bd0d5fc551dab52b
Si, Yudong <yudong.si@intel.com>
2024-05-10 04:23:54 -0700

    run all shapes
    update xetla patch

--- python/tutorials/collect.sh
+++ python/tutorials/collect.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-rm -rf log.txt
-python 09-experimental-block-pointer.py 2>&1 | tee log.txt
+
+# export env variables
+source env.sh
+
+# store result
+rm -rf result.csv
+echo "M/N/K,oneDNN(peak),Triton(peak)" | tee result.csv
+
+for ((i=1; i<=16; i++))
+do
+    shape_size=$((256 * $i))
+    echo "shape size: $shape_size"
+    
+    rm -rf log.txt
+
+    # update shape size in driver.py and 09-experimental-block-pointer.py
+    sed -i "s/for i in \[[0-9]*\]\]/for i in \[$i\]\]/g" 09-experimental-block-pointer.py
+    sed -i "s/float M = 256 \* [0-9]*;/float M = 256 \* $i;/g" ../../third_party/intel/backend/driver.py
 
 
-oneDNN=`cat log.txt | tail -n 1 | awk '{print $5}'`
-Triton=`cat log.txt | tail -n 1 | awk '{print $6}'`
+    python 09-experimental-block-pointer.py 2>&1 | tee log.txt
+
+    oneDNN=`cat log.txt | tail -n 1 | awk '{print $5}'`
+    Triton=`cat log.txt | tail -n 1 | awk '{print $6}'`
 
 
-oneDNN1=`grep "oneDNN Peak TFlops" log.txt | awk '{print $NF}'`
-Triton1=`grep "Triton Peak TFlops" log.txt | awk '{print $NF}' | awk 'BEGIN{max=0} {if ($1>max) max=$1} END{print max}'`
+    oneDNN1=`grep "oneDNN Peak TFlops" log.txt | awk '{print $NF}'`
+    Triton1=`grep "Triton Peak TFlops" log.txt | awk '{print $NF}' | awk 'BEGIN{max=0} {if ($1>max) max=$1} END{print max}'`
 
-echo $oneDNN,$Triton,$oneDNN1,$Triton1 | tee -a data.csv
+    echo $shape_size,$oneDNN1,$Triton1 | tee -a result.csv
+done
+
+cat result.csv
