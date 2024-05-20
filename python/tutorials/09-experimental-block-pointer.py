126725587d4a12e93eadc0a47660f4eab256430d
Julian Oppermann <julian.oppermann@codeplay.com>
2024-05-20 18:02:15 +0100

    Cleanup, pass result type as argument.
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- python/tutorials/09-experimental-block-pointer.py
+++ python/tutorials/09-experimental-block-pointer.py
@@ -196,7 +196,7 @@ def matmul_kernel_with_block_pointers(
 
 # We can now create a convenience wrapper function that only takes two input tensors,
 # and (1) checks any shape constraint; (2) allocates the output; (3) launches the above kernel.
-def matmul(a, b):
+def matmul(a, b, res_dtype):
     # Check constraints.
     assert a.shape[1] == b.shape[0], "Incompatible dimensions"
     assert a.is_contiguous(), "Matrix A must be contiguous"
@@ -204,8 +204,7 @@ def matmul(a, b):
     M, K = a.shape
     K, N = b.shape
     # Allocates output.
-    c_dtype = torch.float32 if a.is_floating_point() else torch.int32
-    c = torch.empty((M, N), device=a.device, dtype=c_dtype)
+    c = torch.empty((M, N), device=a.device, dtype=res_dtype)
     # 1D launch kernel where each block gets its own program.
     grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
     matmul_kernel_with_block_pointers[grid](
@@ -230,9 +229,10 @@ torch.manual_seed(0)
 for dtype in [torch.float16, torch.bfloat16]:
     a = torch.randn((512, 512), device='xpu', dtype=dtype)
     b = torch.randn((512, 512), device='xpu', dtype=dtype)
+    res_dtype = torch.float32
 
-    triton_output = matmul(a, b)
-    torch_output = torch.matmul(a,b).to(triton_output.dtype)
+    triton_output = matmul(a, b, res_dtype)
+    torch_output = torch.matmul(a, b).to(res_dtype)
     print(f"triton_output={triton_output}")
     print(f"torch_output={torch_output}")
 
@@ -248,12 +248,13 @@ for dtype in [torch.float16, torch.bfloat16]:
 for dtype in [torch.int8]:
     a = torch.randint(low=-127, high=128, size=(512, 512), device='xpu', dtype=dtype)
     b = torch.randint(low=-127, high=128, size=(512, 512), device='xpu', dtype=dtype)
+    res_dtype = torch.int32
 
-    triton_output = matmul(a, b)
+    triton_output = matmul(a, b, res_dtype)
     # torch.matmul clamps values to input dtype; IPEX doesn't support int32 matmul
     torch_output = torch.matmul(
-        a.to(device='cpu', dtype=triton_output.dtype),
-        b.to(device='cpu', dtype=triton_output.dtype)).to(device='xpu')
+        a.to(device='cpu', dtype=res_dtype),
+        b.to(device='cpu', dtype=res_dtype)).to(device='xpu')
     print(f"triton_output={triton_output}")
     print(f"torch_output={torch_output}")
 
