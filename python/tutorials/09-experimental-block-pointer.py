a1c6a680171e4583ededd7400d7f83947ea1cb09
Whitney Tsang <whitney.tsang@intel.com>
2024-05-23 15:57:50 -0400

    Fix intermittent failure from `PrefetchBlockPass` (#1183)
    `transformLoop` insert new operations to `func`, so we cannot iterate
    with `func.getOps<scf::ForOp>()`, we should iterate the prepopulated
    list of loops.
    
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- python/tutorials/09-experimental-block-pointer.py
+++ python/tutorials/09-experimental-block-pointer.py
@@ -113,8 +113,6 @@ import triton.language as tl
                       num_warps=32),
         triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4}, num_stages=4,
                       num_warps=32),
-        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4}, num_stages=4,
-                      num_warps=32),
         triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 512, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 4}, num_stages=4,
                       num_warps=32),
         triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 4}, num_stages=4,
