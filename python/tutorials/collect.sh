cf99961c14e33ac23332e99e2d10288785e172d1
ESI-SYD <yudong.si@intel.com>
2024-05-27 20:10:32 -0700

    Correct xetla cfg and add a script

--- python/tutorials/collect.sh
+++ python/tutorials/collect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 M=${1:-4096}
 K=${2:-4096}
 N=${3:-4096}
@@ -34,6 +34,10 @@ IGC_VATemp=1 \
 UR_L0_IN_ORDER_BARRIER_BY_SIGNAL=0 \
 IGC_DisableLoopUnroll=1 \
 python 09-experimental-block-pointer.py 2>&1 | tee result.txt
+
+if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+    exit 1
+fi
 
 Triton_tflops_max=`grep "Triton Peak TFlops" result.txt | awk '{print $NF}' |  tail -n10 | awk 'BEGIN{max=0} {if ($1>max) max=$1} END{print max}'`
 Triton_tflops_min=`grep "Triton Peak TFlops" result.txt | awk '{print $NF}'  | tail -n10 | awk 'BEGIN{min=9999} {if ($1<min) min=$1} END{print min}'`
