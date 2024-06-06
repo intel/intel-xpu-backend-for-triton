c2970aca6187d1f75e3cf6faf94359061eb98510
Whitney Tsang <whitney.tsang@intel.com>
2024-06-06 03:09:47 +0000

    address review comment
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- python/tutorials/09-experimental-block-pointer.py
+++ python/tutorials/09-experimental-block-pointer.py
@@ -103,6 +103,8 @@ import triton.language as tl
                       num_warps=32),
         triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4}, num_stages=3,
                       num_warps=32),
+        triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4}, num_stages=2,
+                      num_warps=32),
         triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4}, num_stages=2,
                       num_warps=32),
     ],
