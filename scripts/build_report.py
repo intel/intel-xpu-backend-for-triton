f32e130bb5517602e38297f677e842d762c1d9b0
Egor Krivov <egor.krivov@intel.com>
2024-06-17 16:38:09 +0000

    Fixed styling

--- scripts/build_report.py
+++ scripts/build_report.py
@@ -7,38 +7,23 @@ import datetime
 import pandas as pd
 
 
-host_info = {
-    n: os.getenv(n.upper(), default="")
-    for n in ["libigc1_version", "level_zero_version", "gpu_device", "agama_version"]
-}
-assert host_info['gpu_device'], "Could not find GPU device description, was capture_device.sh called?"
-
-
-
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Build report based on triton-benchmark run"
-    )
-    parser.add_argument("source", help="Path to source csv file with benchmark results")
+    parser = argparse.ArgumentParser(description="Build report based on triton-benchmark run")
+    parser.add_argument("source", help="Path to source csv file with benchmark results", required=True)
     parser.add_argument(
         "target",
+        required=True,
         help="Path to result csv file with benchmark results including host info and dates",
     )
-    parser.add_argument(
-        "--param_cols", help="Names of parameter columns, separated by commas.", required=True
-    )
+    parser.add_argument("--param_cols", help="Names of parameter columns, separated by commas.", required=True)
     parser.add_argument("--name", help="Name of the benchmark.", required=True)
-    parser.add_argument(
-        "--result_cols", help="Names of the result columns, separated by commas.", required=True
-    )
+    parser.add_argument("--result_cols", help="Names of the result columns, separated by commas.", required=True)
     return parser.parse_args()
 
 
 def check_cols(target_cols, all_cols):
     diff = set(target_cols).difference(all_cols)
-    assert (
-        len(diff) == 0
-    ), f"Couldn't find required columns: '{diff}' among available '{all_cols}'"
+    assert (len(diff) == 0), f"Couldn't find required columns: '{diff}' among available '{all_cols}'"
 
 
 def parse_result(name):
@@ -57,9 +42,7 @@ def transform_df(df, param_cols, result_cols, bench_name):
     check_cols(result_cols, df.columns)
     # Build json with parameters
     df_results = df[result_cols].copy()
-    df_results["params"] = [
-        json.dumps(j) for j in df[param_cols].astype(int).to_dict("records")
-    ]
+    df_results["params"] = [json.dumps(j) for j in df[param_cols].astype(int).to_dict("records")]
 
     df = pd.melt(
         df_results,
@@ -73,6 +56,12 @@ def transform_df(df, param_cols, result_cols, bench_name):
     df["run_uuid"] = uuid.uuid4().hex
     df["datetime"] = datetime.datetime.now()
     df["benchmark"] = bench_name
+
+    host_info = {
+        n: os.getenv(n.upper(), default="")
+        for n in ["libigc1_version", "level_zero_version", "gpu_device", "agama_version"]
+    }
+    assert host_info['gpu_device'], "Could not find GPU device description, was capture_device.sh called?"
     for name, val in host_info.items():
         df[name] = val
 
@@ -84,9 +73,7 @@ def main():
     param_cols = args.param_cols.split(",")
     result_cols = args.result_cols.split(",")
     df = pd.read_csv(args.source)
-    result_df = transform_df(
-        df, param_cols=param_cols, result_cols=result_cols, bench_name=args.name
-    )
+    result_df = transform_df(df, param_cols=param_cols, result_cols=result_cols, bench_name=args.name)
     result_df.to_csv(args.target, index=False)
 
 
