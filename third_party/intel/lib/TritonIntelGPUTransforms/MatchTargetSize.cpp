be4986db72c42fdde4885849c1727fcd185bb97e
Julian Oppermann <julian.oppermann@codeplay.com>
2024-06-21 15:56:50 +0200

    Prevent 32-element wide prefetches for 32-bit dtypes on the block pointer path (#1401)
    Ensures that no overly wide prefetches are emitted and go unnoticed.
    
    ---------
    
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- third_party/intel/lib/TritonIntelGPUTransforms/MatchTargetSize.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/MatchTargetSize.cpp
@@ -514,7 +514,8 @@ MatchTargetSizePass::getSubOpSize(RankedTensorType type) const {
   case 2: {
     if (isa<ttgi::WarpEncodingAttr>(layout)) {
       // 32 = 2 * 16(subgroupSize) which is for large load/store
-      subSize[1] = std::min(32L, shape[1]);
+      // max 2d block prefetch width is 16 for 32-bit datatype
+      subSize[1] = std::min(sizeInBits == 32 ? 16L : 32L, shape[1]);
       // FIXME: From gfxspec, max 2d block load height is 32
       subSize[0] = std::min(32L, shape[0]);
     } else if (auto dotLayout = dyn_cast<ttg::DotOperandEncodingAttr>(layout)) {
