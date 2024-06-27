41ac84ec36b0762943315f0a7971d5d51b09bb3e
Anatoly Myachev <anatoly.myachev@intel.com>
2024-06-27 15:56:49 +0000

    address review comments
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/language/test_subprocess.py
+++ python/test/unit/language/test_subprocess.py
@@ -40,10 +40,11 @@ def is_interpreter():
     ("device_print_hex", "int64"),
     ("device_print_pointer", "int32"),
 ])
-def test_print(func_type: str, data_type: str):
-    if data_type == "float64" and not tr.driver.active.get_current_target().arch['has_fp64']:
+@pytest.mark.parametrize("device", ['xpu'])
+def test_print(func_type: str, data_type: str, device: str):
+    if device == "xpu" and data_type == "float64" and not tr.driver.active.get_current_target().arch['has_fp64']:
         pytest.xfail("float64 not supported on current xpu hardware")
-    proc = subprocess.Popen([sys.executable, print_path, func_type, data_type], stdout=subprocess.PIPE,
+    proc = subprocess.Popen([sys.executable, print_path, func_type, data_type, device], stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, shell=False)
     outs, err = proc.communicate()
     assert proc.returncode == 0
