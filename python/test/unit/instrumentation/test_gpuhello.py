41e78948f1c7dd2bd6b2fda47fd163d7c60b37ae
Anatoly Myachev <anatoly.myachev@intel.com>
2024-07-22 16:03:47 +0000

    Enable 'instrumentation/test_gpuhello.py' for XPU
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/instrumentation/test_gpuhello.py
+++ python/test/unit/instrumentation/test_gpuhello.py
@@ -1,4 +1,5 @@
 import torch
+import intel_extension_for_pytorch  # type: ignore # noqa: F401
 
 import pytest
 import os
@@ -6,9 +7,9 @@ import os
 import triton
 import triton.language as tl
 
-test_stdout = 'Hello From First Instruction of GPU Kernel: kernel1\ttest_gpuhello.py:17:4\n\
-Hello From First Instruction of GPU Kernel: kernel2\ttest_gpuhello.py:23:4\n\
-Hello From First Instruction of GPU Kernel: kernel3\ttest_gpuhello.py:29:4\n'
+test_stdout = 'Hello From First Instruction of GPU Kernel: kernel1\ttest_gpuhello.py:18:4\n\
+Hello From First Instruction of GPU Kernel: kernel2\ttest_gpuhello.py:24:4\n\
+Hello From First Instruction of GPU Kernel: kernel3\ttest_gpuhello.py:30:4\n'
 
 
 @pytest.mark.parametrize(None, [None])
@@ -31,7 +32,6 @@ def kernel3(BLOCK_SIZE: tl.constexpr):
 
 def func(x: torch.Tensor, y: torch.Tensor):
     output = torch.empty_like(x)
-    assert x.is_cuda and y.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
     kernel1[grid](BLOCK_SIZE=1024)
@@ -39,10 +39,10 @@ def func(x: torch.Tensor, y: torch.Tensor):
     kernel3[grid](BLOCK_SIZE=1024)
 
 
-def test_op(capfd):
+def test_op(capfd, device: str):
     size = 98432
-    x = torch.rand(size, device='cuda')
-    y = torch.rand(size, device='cuda')
+    x = torch.rand(size, device=device)
+    y = torch.rand(size, device=device)
     func(x, y)
     stdout, stderr = capfd.readouterr()
     if 'LLVM_PASS_PLUGIN_PATH' in os.environ:
