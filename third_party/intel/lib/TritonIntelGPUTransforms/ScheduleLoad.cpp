eee2d27c02f445d299c78f28fa0c80cf4c9aa87d
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-07-31 20:20:36 +0000

    Address code review comments
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- third_party/intel/lib/TritonIntelGPUTransforms/ScheduleLoad.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/ScheduleLoad.cpp
@@ -9,16 +9,14 @@
 /// This file implements a naive scheduler for loop with load/dot
 //===----------------------------------------------------------------------===//
 
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/Support/Debug.h"
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "mlir/IR/Value.h"
-#include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+// #include "mlir/Support/LogicalResult.h"
+// #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include "intel/include/Dialect/TritonIntelGPU/IR/Dialect.h"
 #include "intel/include/Dialect/TritonIntelGPU/Transforms/Passes.h"
@@ -33,7 +31,6 @@ namespace mlir::triton::gpu::intel {
 
 using namespace mlir;
 namespace tt = mlir::triton;
-namespace ttg = mlir::triton::gpu;
 namespace ttgi = mlir::triton::gpu::intel;
 
 #define DEBUG_TYPE "tritonintelgpu-schedule-load"
@@ -44,34 +41,13 @@ class ScheduleLoadPass
     : public triton::gpu::intel::impl::TritonIntelGPUScheduleLoadBase<
           ScheduleLoadPass> {
 public:
-  // hack!!! only trace dot A/B, only back 1 level
-  SmallVector<Value> getNotVisitedUses(SmallVector<tt::DotOp> dots,
-                                       unsigned opIdx = 0) {
-    SmallVector<Value> notVisited;
-    for (auto &dot : dots) {
-      Value val = opIdx == 1 ? dot.getB() : dot.getA();
-      if (visited.count(val) != 0)
-        continue;
-      auto def = val.getDefiningOp();
-      if (auto extract = dyn_cast<ttgi::ExtractOp>(def)) {
-        auto base = extract.getBase();
-        if (visited.count(base) == 0) {
-          notVisited.push_back(base);
-          visited.insert(base);
-        }
-      }
-      notVisited.push_back(val);
-      visited.insert(val);
-    }
-    return notVisited;
-  }
-
   void runOnOperation() override {
     if (!triton::tools::getBoolEnv("TRITON_INTEL_ENABLE_INSTR_SCHED"))
       return;
-    auto *ctx = &getContext();
-    ModuleOp m = getOperation();
-    m.walk<WalkOrder::PreOrder>([&](scf::ForOp loop) {
+
+    MLIRContext *ctx = &getContext();
+    ModuleOp mod = getOperation();
+    mod.walk<WalkOrder::PreOrder>([&](scf::ForOp loop) {
       visited.clear();
       unsigned group = -1;
       SmallVector<SmallVector<tt::DotOp>> dotsGroup;
@@ -83,21 +59,23 @@ public:
           dotsGroup.push_back(dots);
           dots.clear();
         }
-        if (currGroup == 0)
-          getNotVisitedUses({dot}, 1);
+        if (currGroup == 0) {
+          SmallVector<tt::DotOp> vec{dot};
+          getNotVisitedUses(vec, 1);
+        }
         dots.push_back(dot);
         group = currGroup;
       }
-      assert(!dots.empty());
+      assert(!dots.empty() && "No dot found in the loop");
       dotsGroup.push_back(dots);
 
       unsigned i = 0;
       Operation *start = &loop.getBody()->front();
-      for (auto dots : dotsGroup) {
+      for (SmallVector<tt::DotOp> &dots : dotsGroup) {
         auto notVisited = getNotVisitedUses(dots, 1);
         if (i == 0)
-          notVisited.append(getNotVisitedUses(dots));
-        for (auto val : notVisited) {
+          notVisited.append(getNotVisitedUses(dots, 0));
+        for (Value val : notVisited) {
           auto op = val.getDefiningOp();
           if (i == 0)
             op->moveBefore(start);
@@ -111,14 +89,14 @@ public:
     });
 
     // HoHo, move trunc forward
-    m.walk([&](arith::TruncFOp op) {
+    mod.walk([&](arith::TruncFOp op) {
       auto def = op.getIn().getDefiningOp();
       op->moveAfter(def);
     });
 
     // HoHo, add fastmath for all
     // may do this after llvm ir according to user fmath flag
-    m.walk([&](Operation *op) {
+    mod.walk([&](Operation *op) {
       if (auto fmIf = dyn_cast<arith::ArithFastMathInterface>(op))
         op->setAttr(
             fmIf.getFastMathAttrName(),
@@ -127,6 +105,31 @@ public:
   }
 
 private:
+  // hack!!! only trace dot A/B, only back 1 level
+  SmallVector<Value> getNotVisitedUses(SmallVector<tt::DotOp> &dots,
+                                       unsigned opIdx) {
+    assert((opIdx == 1 || opIdx == 0) && "opIdx should be 0 or 1");
+
+    SmallVector<Value> notVisited;
+    for (tt::DotOp &dot : dots) {
+      Value val = (opIdx == 1) ? dot.getB() : dot.getA();
+      if (visited.contains(val))
+        continue;
+
+      Operation *def = val.getDefiningOp();
+      if (auto extract = dyn_cast<ttgi::ExtractOp>(def)) {
+        Value base = extract.getBase();
+        if (!visited.contains(base)) {
+          notVisited.push_back(base);
+          visited.insert(base);
+        }
+      }
+      notVisited.push_back(val);
+      visited.insert(val);
+    }
+    return notVisited;
+  }
+
   DenseSet<Value> visited;
 };
 
