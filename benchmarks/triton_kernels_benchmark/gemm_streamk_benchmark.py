ed31a1492c53baf68a12c04583d0d83273ef4b7a
Whitney Tsang <whitney.tsang@intel.com>
2024-08-01 00:23:27 +0000

    Remove from CI

--- benchmarks/triton_kernels_benchmark/gemm_streamk_benchmark.py
+++ benchmarks/triton_kernels_benchmark/gemm_streamk_benchmark.py
@@ -275,9 +275,8 @@ def benchmark(M, N, K, provider):
                                                                quantiles=quantiles, fast_flush=False)
     if provider == 'triton':
         triton_fn = lambda: matmul(a, b)
-        # torch_fn = lambda: torch.matmul(a, b).to(torch.float32)
-        # rtol = 1e-2 if a.dtype == torch.bfloat16 else 1e-3
-        # benchmark_suit.assert_close(triton_fn(), torch_fn(), atol=1e-4, rtol=rtol, err_msg="triton to torch")
+        torch_fn = lambda: torch.matmul(a, b).to(torch.float32)
+        benchmark_suit.assert_close(triton_fn(), torch_fn(), atol=1e-4, rtol=1e-2, err_msg="triton to torch")
         ms, min_ms, max_ms, mean, cv = benchmark_suit.do_bench(triton_fn, warmup=100, rep=100, quantiles=quantiles,
                                                                fast_flush=False)
 
