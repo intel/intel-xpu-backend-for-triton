135dddc03850e7ac1b42d408bc2545fbe028d9e5
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-08-12 18:40:17 +0000

    Add VERBOSE tirton compiler support

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -71,6 +71,10 @@ def _build(name, src, srcdir, library_dirs, include_dirs, libraries):
     cc_cmd += [f'-l{lib}' for lib in libraries]
     cc_cmd += [f"-L{dir}" for dir in library_dirs]
     cc_cmd += [f"-I{dir}" for dir in include_dirs if dir is not None]
+
+    if os.getenv("VERBOSE"):
+        print(" ".join(cc_cmd))
+
     ret = subprocess.check_call(cc_cmd)
     if ret == 0:
         return so
