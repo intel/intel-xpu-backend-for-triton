38d590f71329f4e5ae2fcb4d83e38ceb630e78c4
Hyoungwook Nam <hyoungwook.nam@intel.com>
2024-08-15 17:43:17 +0000

    Adding a comment to explain the assertion.

--- third_party/intel/lib/Target/LLVMIR/PostProcess.cpp
+++ third_party/intel/lib/Target/LLVMIR/PostProcess.cpp
@@ -24,6 +24,11 @@ void postProcessLLVMIR(llvm::Module &mod) {
       break;
     }
   };
+
+  // __devicelib_assert_fail must be a declaration so that
+  // IGC can replace it with a runtime assert function.
+  // If a 'fallback' implementation is defined in SYCL libarary, the
+  // assertion does not work correctly.
   for (auto &f : mod) {
     if (f.getName().str() == "__devicelib_assert_fail") {
       assert(f.isDeclaration() &&
