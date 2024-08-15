9ee12b2e5d9e210131a7db83a0bd130cd38f4017
Alex Baden <alexander.baden@intel.com>
2024-08-15 16:03:19 +0000

    Update SPIRVRunner for driver.c/launcher refactor WIP

--- utils/SPIRVRunner/SPIRVRunner.cpp
+++ utils/SPIRVRunner/SPIRVRunner.cpp
@@ -65,14 +65,11 @@ std::vector<char> read_spirv(const std::string &filename) {
 
 /** SYCL Globals **/
 
-SyclQueueMap sycl_queue_map;
-static ze_context_handle_t context = {nullptr};
+SyclQueueMap g_sycl_queue_map;
 
-static std::vector<ze_device_handle_t> devices;
+static std::vector<ze_device_handle_t> g_devices;
 static std::vector<std::pair<sycl::device, ze_device_handle_t>>
-    sycl_l0_device_list;
-
-std::vector<std::unique_ptr<sycl::kernel>> compiled_kernels;
+    g_sycl_l0_device_list;
 
 static inline void gpuAssert(ze_result_t code) {
   if (code != ZE_RESULT_SUCCESS) {
@@ -88,24 +85,24 @@ static inline T checkSyclErrors(const std::tuple<T, ze_result_t> tuple) {
 }
 
 /** SYCL Functions **/
-std::tuple<sycl::kernel_bundle<sycl::bundle_state::executable> *,
-           sycl::kernel *, int32_t, int32_t>
+std::tuple<std::unique_ptr<sycl::kernel_bundle<sycl::bundle_state::executable>>,
+           std::unique_ptr<sycl::kernel>, int32_t, int32_t>
 loadBinary(const std::string &kernel_name, uint8_t *binary_ptr,
            const size_t binary_size, const size_t deviceId) {
   int32_t n_regs = 0;
   int32_t n_spills = 0;
 
-  if (!(deviceId < sycl_l0_device_list.size())) {
+  if (!(deviceId < g_sycl_l0_device_list.size())) {
     throw std::runtime_error("Device is not found " + std::to_string(deviceId));
   }
 
-  auto sycl_l0_device_pair = sycl_l0_device_list[deviceId];
-  sycl::device sycl_device = sycl_l0_device_pair.first;
+  const auto &sycl_l0_device_pair = g_sycl_l0_device_list[deviceId];
+  const sycl::device sycl_device = sycl_l0_device_pair.first; // TODO: reference ok? 
 
-  auto ctx = sycl_device.get_platform().ext_oneapi_get_default_context();
-  auto l0_device =
+  const auto ctx = sycl_device.get_platform().ext_oneapi_get_default_context();
+  const auto l0_device =
       sycl::get_native<sycl::backend::ext_oneapi_level_zero>(sycl_device);
-  auto l0_context = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(ctx);
+  const auto l0_context = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(ctx);
   const char *build_flags = "";
   auto l0_module = checkSyclErrors(create_module(
       l0_context, l0_device, binary_ptr, binary_size, build_flags));
@@ -116,45 +113,30 @@ loadBinary(const std::string &kernel_name, uint8_t *binary_ptr,
   props.pNext = nullptr;
   gpuAssert((zeKernelGetProperties(l0_kernel, &props)));
   n_spills = props.spillMemSize;
-  auto mod = sycl::make_kernel_bundle<sycl::backend::ext_oneapi_level_zero,
+  auto mod = std::make_unique<sycl::kernel_bundle<sycl::bundle_state::executable>>(sycl::make_kernel_bundle<sycl::backend::ext_oneapi_level_zero,
                                       sycl::bundle_state::executable>(
-      {l0_module, sycl::ext::oneapi::level_zero::ownership::transfer}, ctx);
-  auto fun = sycl::make_kernel<sycl::backend::ext_oneapi_level_zero>(
-      {mod, l0_kernel, sycl::ext::oneapi::level_zero::ownership::transfer},
-      ctx);
-  compiled_kernels.push_back(std::make_unique<sycl::kernel>(fun));
-  sycl::kernel *ptr = compiled_kernels[compiled_kernels.size() - 1].get();
-  if (getBoolEnv("MLIR_ENABLE_DUMP")) {
-    std::cout << "compiled kernel ptr: " << ptr << std::endl;
-    std::cout << "total kernels:" << compiled_kernels.size() << std::endl;
-    for (auto &k : compiled_kernels) {
-      std::cout << "  kernel:"
-                << k->get_info<sycl::info::kernel::function_name>() << " @"
-                << k.get() << std::endl;
-    }
-  }
-  sycl::kernel *k = new sycl::kernel(*ptr);
-  sycl::kernel_bundle<sycl::bundle_state::executable> *kb =
-      new sycl::kernel_bundle<sycl::bundle_state::executable>(mod);
+      {l0_module, sycl::ext::oneapi::level_zero::ownership::transfer}, ctx));
+  auto fun = std::make_unique<sycl::kernel>(sycl::make_kernel<sycl::backend::ext_oneapi_level_zero>(
+      {*mod, l0_kernel, sycl::ext::oneapi::level_zero::ownership::transfer},
+      ctx));
 
-  return std::make_tuple(kb, k, n_regs, n_spills);
+  return std::make_tuple(std::move(mod), std::move(fun), n_regs, n_spills);
 }
 
 ze_context_handle_t initContext(sycl::queue *sycl_queue) {
-  if (sycl_queue_map.find(*sycl_queue) == sycl_queue_map.end()) {
-    auto updated_sycl_devices = update(*sycl_queue, sycl_queue_map);
+  if (g_sycl_queue_map.find(*sycl_queue) == g_sycl_queue_map.end()) {
+    const auto updated_sycl_devices = update(*sycl_queue, g_sycl_queue_map);
     if (!updated_sycl_devices.empty()) {
       // Update global data
-      context = sycl_queue_map[*sycl_queue].context;
-      uint32_t deviceCount =
-          std::min(updated_sycl_devices.size(), devices.size());
+      const uint32_t deviceCount =
+          std::min(updated_sycl_devices.size(), g_devices.size());
       for (uint32_t i = 0; i < deviceCount; ++i) {
-        devices[i] = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
+        g_devices[i] = sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
             updated_sycl_devices[i]);
       }
     }
   }
-  context = sycl_queue_map[*sycl_queue].context;
+  auto context = g_sycl_queue_map[*sycl_queue].context;
   return context;
 }
 
@@ -162,15 +144,15 @@ size_t initDevices(sycl::queue *sycl_queue) {
   auto sycl_context = sycl_queue->get_context();
 
   // Get sycl-device
-  std::vector<sycl::device> sycl_devices = sycl_context.get_devices();
+  const std::vector<sycl::device> &sycl_devices = sycl_context.get_devices();
 
   // Retrieve l0 devices
-  uint32_t deviceCount = sycl_devices.size();
+  const uint32_t deviceCount = sycl_devices.size();
   for (uint32_t i = 0; i < deviceCount; ++i) {
-    sycl_l0_device_list.push_back(std::make_pair(
+    g_sycl_l0_device_list.push_back(std::make_pair(
         sycl_devices[i], sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
                              sycl_devices[i])));
-    devices.push_back(sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
+    g_devices.push_back(sycl::get_native<sycl::backend::ext_oneapi_level_zero>(
         sycl_devices[i]));
   }
 
@@ -201,15 +183,10 @@ static void sycl_kernel_launch(uint32_t gridX, uint32_t gridY, uint32_t gridZ,
                                int num_warps, int threads_per_warp,
                                int shared_memory, sycl::queue &stream,
                                sycl::kernel &kernel_ptr, void *arg0, void *arg1,
-                               void *arg2, int32_t arg3, int32_t arg4,
-                               int32_t arg5, int32_t arg6, int32_t arg7,
-                               int32_t arg8, int32_t arg9, int32_t arg10,
-                               int32_t arg11) {
-
+                               void *arg2, int32_t arg3) {
   std::string kernel_name =
       kernel_ptr.get_info<sycl::info::kernel::function_name>();
-  void *params[] = {&arg0, &arg1, &arg2, &arg3, &arg4,
-                    &arg5, &arg7, &arg9, &arg10};
+  void *params[] = {&arg0, &arg1, &arg2, &arg3};
   uint32_t num_params = sizeof(params) / sizeof(params[0]);
   uint32_t expected_num_params =
       kernel_ptr.get_info<sycl::info::kernel::num_args>();
@@ -228,36 +205,12 @@ static void sycl_kernel_launch(uint32_t gridX, uint32_t gridY, uint32_t gridZ,
   assert(num_params == expected_num_params &&
          "number of kernel param not matched");
 
-  auto a_dev = sycl::malloc_device<float>(256 * 160, stream);
-  auto b_dev = sycl::malloc_device<float>(160 * 512, stream);
-  auto output_dev = sycl::malloc_device<float>(256 * 512, stream);
-
-  params[0] = &a_dev;
-  params[1] = &b_dev;
-  params[2] = &output_dev;
-
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(a_dev, arg0, 256 * 160 * sizeof(float));
-  });
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(b_dev, arg1, 160 * 512 * sizeof(float));
-  });
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(output_dev, arg2, 256 * 512 * sizeof(float));
-  });
-  stream.wait();
-
   // Submit the imported kernel.
   auto cgf = [&](sycl::handler &cgh) {
     set_scalar_arg(cgh, 0, sizeof(void *), params[0]);
     set_scalar_arg(cgh, 1, sizeof(void *), params[1]);
     set_scalar_arg(cgh, 2, sizeof(void *), params[2]);
     set_scalar_arg(cgh, 3, sizeof(int32_t), params[3]);
-    set_scalar_arg(cgh, 4, sizeof(int32_t), params[4]);
-    set_scalar_arg(cgh, 5, sizeof(int32_t), params[5]);
-    set_scalar_arg(cgh, 6, sizeof(int32_t), params[6]);
-    set_scalar_arg(cgh, 7, sizeof(int32_t), params[7]);
-    set_scalar_arg(cgh, 8, sizeof(int32_t), params[8]);
     if (shared_memory) {
       using share_mem_t = sycl::local_accessor<int8_t, 1>;
       share_mem_t local_buffer = share_mem_t(shared_memory, cgh);
@@ -270,87 +223,90 @@ static void sycl_kernel_launch(uint32_t gridX, uint32_t gridY, uint32_t gridZ,
   auto event = stream.submit(cgf);
 
   stream.wait();
-
-  // copy back
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(arg0, a_dev, 256 * 160 * sizeof(float));
-  });
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(arg1, b_dev, 160 * 512 * sizeof(float));
-  });
-  stream.submit([&](sycl::handler &cgh) {
-    cgh.memcpy(arg2, output_dev, 256 * 512 * sizeof(float));
-  });
-  stream.wait();
 }
 
-at::Tensor launchKernel(sycl::queue *stream, sycl::kernel *kernel,
+at::Tensor launchKernel(sycl::queue stream, sycl::kernel kernel,
                         const torch::Tensor &a, const torch::Tensor &b) {
-  int gridX = 4;
+  int gridX = 97;
   int gridY = 1;
   int gridZ = 1;
 
-  int num_warps = 8;
+  int num_warps = 4;
   int num_ctas = 1;
-  int shared_memory = 98304;
-  int threads_per_warp = 16;
+  int shared_memory = 0;
+  int threads_per_warp = 32;
 
-  int _arg3 = 256;
-  int _arg4 = 512;
-  int _arg5 = 160;
-  int _arg6 = 1;
-  int _arg7 = 256;
-  int _arg8 = 1;
-  int _arg9 = 160;
-  int _arg10 = 512;
-  int _arg11 = 1;
-
+  int _arg3 = 98432;
+  
   torch::Tensor output =
-      torch::zeros({a.sizes()[0], b.sizes()[1]}, c10::nullopt,
-                   at::TensorOptions{c10::ScalarType::Float});
+      torch::zeros({a.sizes()[0]}, c10::nullopt,
+                   at::TensorOptions{c10::ScalarType::Float}); 
+   std::cout << "Tensor output: " << output.sizes() << ", " << output.scalar_type() << " (" << output.nbytes() << " bytes)"
+            << std::endl;
 
   auto tensor_ptr = [](const torch::Tensor &t) -> void * {
     return reinterpret_cast<void *>(t.data_ptr());
   };
 
+  auto a_dev = sycl::malloc_device<char>(a.nbytes(), stream);
+  auto b_dev = sycl::malloc_device<char>(b.nbytes(), stream);
+  auto output_dev = sycl::malloc_device<char>(output.nbytes(), stream);
+
+  stream.submit([&](sycl::handler &cgh) {
+    cgh.memcpy(a_dev, tensor_ptr(a), a.nbytes());
+  });
+  stream.submit([&](sycl::handler &cgh) {
+    cgh.memcpy(b_dev, tensor_ptr(b), b.nbytes());
+  });
+  stream.submit([&](sycl::handler &cgh) {
+    cgh.memcpy(output_dev, tensor_ptr(output), output.nbytes());
+  });
+  stream.wait();
+
   sycl_kernel_launch(gridX, gridY, gridZ, num_warps, threads_per_warp,
-                     shared_memory, *stream, *kernel, tensor_ptr(a),
-                     tensor_ptr(b), tensor_ptr(output), _arg3, _arg4, _arg5,
-                     _arg6, _arg7, _arg8, _arg9, _arg10, _arg11);
+                     shared_memory, stream, kernel, a_dev,
+                     b_dev, output_dev, _arg3);
+
+
+  // copy back
+  stream.submit([&](sycl::handler &cgh) {
+    cgh.memcpy(tensor_ptr(output), output_dev, output.nbytes());
+  });
+  stream.wait();
 
   return output;
 }
 
 int main() {
-  auto a = load_tensor("a.pt");
-  auto b = load_tensor("b.pt");
-  std::cout << "Tensor a: " << a.sizes() << ", " << a.scalar_type()
-            << std::endl;
-  std::cout << "Tensor b: " << b.sizes() << ", " << b.scalar_type()
-            << std::endl;
-
   // initialize sycl runtime
   sycl::default_selector d_selector;
-
-  sycl::queue q(d_selector, exception_handler);
+  sycl::queue q = sycl::queue(d_selector, exception_handler);
+  
   std::cout << "Running on device: "
             << q.get_device().get_info<sycl::info::device::name>() << "\n";
   auto context = initContext(&q);
   auto device_count = initDevices(&q);
 
+  auto a = load_tensor("x.pt");
+  auto b = load_tensor("y.pt");
+  std::cout << "Tensor a: " << a.sizes() << ", " << a.scalar_type() << " (" << a.nbytes() << " bytes)"
+            << std::endl;
+  std::cout << "Tensor b: " << b.sizes() << ", " << b.scalar_type() << " (" << b.nbytes() << " bytes)"
+            << std::endl;
+
   // read spirv
-  auto spirv = read_spirv("_kernel.spv");
+  auto spirv = read_spirv("add_kernel.spv");
   std::cout << "Read " << spirv.size() << " byte kernel." << std::endl;
 
   auto [kernel_bundle, kernel, n_regs, n_spills] =
-      loadBinary("_kernel", reinterpret_cast<uint8_t *>(spirv.data()),
+      loadBinary("add_kernel", reinterpret_cast<uint8_t *>(spirv.data()),
                  spirv.size() / sizeof(uint32_t), 0);
 
-  // TODO: this seems wrong from upstream code?
+  // TODO: missing number of registers 
   std::cout << "Loaded kernel with " << n_regs << " registers and " << n_spills
             << " register spills." << std::endl;
 
-  auto output = launchKernel(&q, kernel, a, b);
-  std::cout << "Kernel return output: " << output[53][508] << std::endl;
+  auto output = launchKernel(q, *kernel, a, b);
+  std::cout << "Kernel return output: " << output[0] << std::endl;
   write_tensor("cpp_outs.pt", output);
 }
