391247565dd729996ad6b325474c9603c3c8adf0
Lu, Chengjun <chengjun.lu@intel.com>
2024-08-16 11:30:17 +0000

    Hard code to jit kernel for NV

--- python/triton/_internal_testing.py
+++ python/triton/_internal_testing.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import triton
 import triton.language as tl
-from triton.backends.nvidia.compiler import _path_to_binary
 import pytest
 
 from numpy.random import RandomState
@@ -38,7 +37,8 @@ def is_cuda():
 
 
 def is_hopper():
-    return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
+    return True
+    # return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
 
 
 def is_hip():
@@ -161,11 +161,12 @@ def supports_tma(byval_only=False):
         return True
     if not is_cuda():
         return False
-    _, cuda_version = _path_to_binary("ptxas")
-    min_cuda_version = (12, 0) if byval_only else (12, 3)
-    cuda_version_tuple = tuple(map(int, cuda_version.split(".")))
-    assert len(cuda_version_tuple) == 2, cuda_version_tuple
-    return torch.cuda.get_device_capability()[0] >= 9 and cuda_version_tuple >= min_cuda_version
+    return True
+    # _, cuda_version = _path_to_binary("ptxas")
+    # min_cuda_version = (12, 0) if byval_only else (12, 3)
+    # cuda_version_tuple = tuple(map(int, cuda_version.split(".")))
+    # assert len(cuda_version_tuple) == 2, cuda_version_tuple
+    # return torch.cuda.get_device_capability()[0] >= 9 and cuda_version_tuple >= min_cuda_version
 
 
 def tma_skip_msg(byval_only=False):
