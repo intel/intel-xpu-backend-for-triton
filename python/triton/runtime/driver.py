2bda7cce2544f6f282ed5bb504ce5996ee378d44
Lu, Chengjun <chengjun.lu@intel.com>
2024-08-16 11:30:17 +0000

    Hard code to jit kernel for NV

--- python/triton/runtime/driver.py
+++ python/triton/runtime/driver.py
@@ -3,7 +3,8 @@ from ..backends import DriverBase
 
 
 def _create_driver():
-    actives = [x.driver for x in backends.values() if x.driver.is_active()]
+    # actives = [x.driver for x in backends.values() if x.driver.is_active()]
+    actives = [[x for x in backends.values()][2].driver]
     if len(actives) != 1:
         raise RuntimeError(f"{len(actives)} active drivers ({actives}). There should only be one.")
     return actives[0]()
