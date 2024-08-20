59539b0203aced8240f12a79074b747f7ea903a8
HyoungWook Nam <hyoungwook.nam@intel.com>
2024-08-20 01:28:27 +0000

    LinearLayout conversion interface (#1875)

--- third_party/intel/include/Dialect/TritonIntelGPU/IR/Attributes.h
+++ third_party/intel/include/Dialect/TritonIntelGPU/IR/Attributes.h
@@ -1,6 +1,8 @@
 #ifndef TRITON_DIALECT_TRITON_INTEL_GPU_IR_ATTRIBUTES_H
 #define TRITON_DIALECT_TRITON_INTEL_GPU_IR_ATTRIBUTES_H
 
+#include "triton/Dialect/TritonGPU/IR/Attributes.h"
+
 #define GET_ATTRDEF_CLASSES
 #include "intel/include/Dialect/TritonIntelGPU/IR/TritonIntelGPUAttrDefs.h.inc"
 
