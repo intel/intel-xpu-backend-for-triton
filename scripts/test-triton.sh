311f2d132a8b5aaaa268ff00fe2b6e67acd1f49d
Pavel Chekin <pavel.chekin@intel.com>
2024-09-11 11:00:58 -0700

    Clean up bash scripts

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -19,7 +19,6 @@ TRITON_TEST_WARNING_REPORTS=false
 TRITON_TEST_IGNORE_ERRORS=false
 SKIP_DEPS=false
 TEST_UNSKIP=false
-ARGS=
 for arg in "$@"; do
   case $arg in
     --unskip)
@@ -83,8 +82,8 @@ for arg in "$@"; do
       exit 1
       ;;
     *)
-      ARGS+="${arg} "
-      shift
+      echo "Unknown argument: $arg."
+      exit 1
       ;;
   esac
 done
@@ -138,36 +137,28 @@ run_unit_tests() {
   echo "***************************************************"
   echo "******      Running Triton CXX unittests     ******"
   echo "***************************************************"
-  UNIT_TEST_DIR=$TRITON_PROJ_BUILD/"$(ls $TRITON_PROJ_BUILD)"
-  if [ ! -d "${UNIT_TEST_DIR}" ]; then
-    echo "Not found '${UNIT_TEST_DIR}'. Build Triton please" ; exit 2
-  fi
-  cd $UNIT_TEST_DIR
+  cd $TRITON_PROJ_BUILD/* || {
+      echo "Triton build not found in $TRITON_PROJ_BUILD. Build Triton please."
+      exit 1
+  }
   ctest .
 
   echo "***************************************************"
   echo "******       Running Triton LIT tests        ******"
   echo "***************************************************"
-  LIT_TEST_DIR=$TRITON_PROJ_BUILD/"$(ls $TRITON_PROJ_BUILD)"/test
-  if [ ! -d "${LIT_TEST_DIR}" ]; then
-    echo "Not found '${LIT_TEST_DIR}'. Build Triton please" ; exit 4
-  fi
-  lit -v "${LIT_TEST_DIR}"
+  cd $TRITON_PROJ_BUILD/*/test || {
+      echo "Triton build not found in $TRITON_PROJ_BUILD. Build Triton please."
+      exit 1
+  }
+  lit -v .
 }
 
 run_core_tests() {
   echo "***************************************************"
   echo "******      Running Triton Core tests        ******"
   echo "***************************************************"
-  CORE_TEST_DIR=$TRITON_PROJ/python/test/unit
-
-  if [ ! -d "${CORE_TEST_DIR}" ]; then
-    echo "Not found '${CORE_TEST_DIR}'. Build Triton please" ; exit 3
-  fi
-
-  cd ${CORE_TEST_DIR}
+  cd $TRITON_PROJ/python/test/unit
   ensure_spirv_dis
-  export TEST_UNSKIP
 
   TRITON_DISABLE_LINE_INFO=1 TRITON_TEST_SUITE=language \
   pytest -vvv -n 8 --device xpu language/ --ignore=language/test_line_info.py --ignore=language/test_subprocess.py
@@ -188,13 +179,7 @@ run_regression_tests() {
   echo "***************************************************"
   echo "******   Running Triton Regression tests     ******"
   echo "***************************************************"
-  REGRESSION_TEST_DIR=$TRITON_PROJ/python/test/regression
-  export TEST_UNSKIP
-
-  if [ ! -d "${REGRESSION_TEST_DIR}" ]; then
-    echo "Not found '${REGRESSION_TEST_DIR}'. Build Triton please" ; exit 3
-  fi
-  cd ${REGRESSION_TEST_DIR}
+  cd $TRITON_PROJ/python/test/regression
 
   TRITON_DISABLE_LINE_INFO=1 TRITON_TEST_SUITE=regression \
   pytest -vvv -s --device xpu . --reruns 10 --ignore=test_performance.py
@@ -204,13 +189,8 @@ run_interpreter_tests() {
   echo "***************************************************"
   echo "******   Running Triton Interpreter tests    ******"
   echo "***************************************************"
-  INTERPRETER_TEST_DIR=$TRITON_PROJ/python/test/unit
+  cd $TRITON_PROJ/python/test/unit
 
-  if [ ! -d "${INTERPRETER_TEST_DIR}" ]; then
-    echo "Not found '${INTERPRETER_TEST_DIR}'. Build Triton please" ; exit 3
-  fi
-  cd ${INTERPRETER_TEST_DIR}
-  export TEST_UNSKIP
   TRITON_INTERPRET=1 TRITON_TEST_SUITE=interpreter \
   pytest -vvv -n 16 -m interpreter language/test_core.py language/test_standard.py \
   language/test_random.py --device cpu
@@ -221,12 +201,7 @@ run_tutorial_tests() {
   echo "**** Running Triton Tutorial tests           ******"
   echo "***************************************************"
   python3 -m pip install matplotlib pandas tabulate -q
-
-  TUTORIAL_TEST_DIR=$TRITON_PROJ/python/tutorials
-  if [ ! -d "${TUTORIAL_TEST_DIR}" ]; then
-    echo "Not found '${TUTORIAL_TEST_DIR}'." ; exit 5
-  fi
-  cd $TUTORIAL_TEST_DIR
+  cd $TRITON_PROJ/python/tutorials
 
   run_tutorial_test "01-vector-add"
   run_tutorial_test "02-fused-softmax"
@@ -244,71 +219,60 @@ run_microbench_tests() {
   echo "****************************************************"
   echo "*****   Running Triton Micro Benchmark tests   *****"
   echo "****************************************************"
-  BENCHMARK_TEST_DIR=$TRITON_PROJ/benchmarks/micro_benchmarks
-  if [ ! -d "${BENCHMARK_TEST_DIR}" ]; then
-    echo "Not found '${BENCHMARK_TEST_DIR}'." ; exit 5
-  fi
-  python ${BENCHMARK_TEST_DIR}/run_benchmarks.py
+  python $TRITON_PROJ/benchmarks/micro_benchmarks/run_benchmarks.py
 }
 
 run_benchmark_softmax() {
   echo "****************************************************"
   echo "*****             Running Softmax              *****"
   echo "****************************************************"
-  BENCHMARK_TEST_DIR=$TRITON_PROJ/benchmarks/triton_kernels_benchmark
-  if [ ! -d "${BENCHMARK_TEST_DIR}" ]; then
-    echo "Not found '${BENCHMARK_TEST_DIR}'." ; exit 5
-  fi
-  cd $TRITON_PROJ/benchmarks; python setup.py install
-  python ${BENCHMARK_TEST_DIR}/fused_softmax.py
+  cd $TRITON_PROJ/benchmarks
+  python setup.py install
+  python $TRITON_PROJ/benchmarks/triton_kernels_benchmark/fused_softmax.py
 }
 
 run_benchmark_gemm() {
   echo "****************************************************"
   echo "*****              Running GEMM                *****"
   echo "****************************************************"
-  BENCHMARK_TEST_DIR=$TRITON_PROJ/benchmarks/triton_kernels_benchmark
-  if [ ! -d "${BENCHMARK_TEST_DIR}" ]; then
-    echo "Not found '${BENCHMARK_TEST_DIR}'." ; exit 5
-  fi
-  cd $TRITON_PROJ/benchmarks; python setup.py install
+  cd $TRITON_PROJ/benchmarks
+  python setup.py install
+
   echo "Default path:"
   TRITON_INTEL_ADVANCED_PATH=0 \
   TRITON_INTEL_ENABLE_ADDRESS_PAYLOAD_OPT=1 \
   IGC_VISAOptions=" -enableBCR -nolocalra" \
   IGC_DisableLoopUnroll=1 \
-  python ${BENCHMARK_TEST_DIR}/gemm_benchmark.py
+  python $TRITON_PROJ/benchmarks/triton_kernels_benchmark/gemm_benchmark.py
 
   echo "Advanced path:"
   TRITON_INTEL_ADVANCED_PATH=1 \
   TRITON_INTEL_ENABLE_ADDRESS_PAYLOAD_OPT=1 \
   IGC_VISAOptions=" -enableBCR -nolocalra" \
   IGC_DisableLoopUnroll=1 \
-  python ${BENCHMARK_TEST_DIR}/gemm_benchmark.py
+  python $TRITON_PROJ/benchmarks/triton_kernels_benchmark/gemm_benchmark.py
 }
 
 run_benchmark_attention() {
   echo "****************************************************"
   echo "*****            Running ATTENTION             *****"
   echo "****************************************************"
-  BENCHMARK_TEST_DIR=$TRITON_PROJ/benchmarks/triton_kernels_benchmark
-  if [ ! -d "${BENCHMARK_TEST_DIR}" ]; then
-    echo "Not found '${BENCHMARK_TEST_DIR}'." ; exit 5
-  fi
-  cd $TRITON_PROJ/benchmarks; python setup.py install
+  cd $TRITON_PROJ/benchmarks
+  python setup.py install
+
   echo "Default path:"
   TRITON_INTEL_ADVANCED_PATH=0 \
   TRITON_INTEL_ENABLE_ADDRESS_PAYLOAD_OPT=1 \
   IGC_VISAOptions=" -enableBCR -nolocalra -printregusage -DPASTokenReduction -enableHalfLSC" \
   IGC_DisableLoopUnroll=1 \
-  python ${BENCHMARK_TEST_DIR}/flash_attention_fwd_benchmark.py
+  python $TRITON_PROJ/benchmarks/triton_kernels_benchmark/flash_attention_fwd_benchmark.py
 
   echo "Advanced path:"
   TRITON_INTEL_ADVANCED_PATH=1 \
   TRITON_INTEL_ENABLE_ADDRESS_PAYLOAD_OPT=1 \
   IGC_VISAOptions=" -enableBCR -nolocalra -printregusage -DPASTokenReduction -enableHalfLSC" \
   IGC_DisableLoopUnroll=1 \
-  python ${BENCHMARK_TEST_DIR}/flash_attention_fwd_benchmark.py
+  python $TRITON_PROJ/benchmarks/triton_kernels_benchmark/flash_attention_fwd_benchmark.py
 }
 
 test_triton() {
