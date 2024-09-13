4c852834209cee9e704ba6e93771ac64c80e6366
Anatoly Myachev <anatoly.myachev@intel.com>
2024-09-13 11:38:13 +0000

    Use 'sycl::half' for FA XeTLA
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/xetla_kernel/flash_attention/fmha_forward_v5.h
+++ benchmarks/xetla_kernel/flash_attention/fmha_forward_v5.h
@@ -23,7 +23,7 @@
 // Set to 1 to get raw output, not permuted
 #define _RAW_OUTPUT 0
 #define _USE_BFNH 1
-using T = sycl::ext::oneapi::bfloat16;
+using T = sycl::half;
 
 namespace gpu::xetla {
 
@@ -620,9 +620,9 @@ class FmhaForwardKernel;
 // The launcher of fmha forward kernel
 template <typename fmha_policy, typename T, bool kUseBias = false,
           bool kIsCausal = false, bool kIsTraining = false>
-sycl::event fmha_forward_impl(sycl::queue &q, void *_k, void *_v, void *_out,
-                              void *_dropout_mask, void *_bias, void *_m,
-                              void *_l, uint32_t num_batches,
+sycl::event fmha_forward_impl(sycl::queue &q, void *_q, void *_k, void *_v,
+                              void *_out, void *_dropout_mask, void *_bias,
+                              void *_m, void *_l, uint32_t num_batches,
                               uint32_t num_heads, uint32_t head_size,
                               uint32_t num_queries, uint32_t num_keys,
                               uint64_t seed = 0, uint64_t offset = 123) {
@@ -644,9 +644,10 @@ sycl::event fmha_forward_impl(sycl::queue &q, void *_k, void *_v, void *_out,
   uint32_t size_ml = shape.get_ml_size();
 
   // forward
-  T *query = sycl::malloc_shared<T>(size_query, q);
+  // T *query = sycl::malloc_shared<T>(size_query, q);
   // T *key = sycl::malloc_shared<T>(size_key, q);
   // T *value = sycl::malloc_shared<T>(size_key, q);
+  T *query = static_cast<T *>(_q);
   T *key = static_cast<T *>(_k);
   T *value = static_cast<T *>(_v);
 
@@ -686,7 +687,7 @@ sycl::event fmha_forward_impl(sycl::queue &q, void *_k, void *_v, void *_out,
           fmha_fwd_op(ei, args);
         });
   });
-  sycl::free(query, q);
+  // sycl::free(query, q);
   // sycl::free(key, q);
   // sycl::free(value, q);
   // sycl::free(bias, q);
