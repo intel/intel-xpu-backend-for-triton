8a380fd4634143b8a1a51372a9768450dfce16e9
Anatoly Myachev <anatoly.myachev@intel.com>
2024-09-20 11:30:44 +0000

    Use cpu version of torch sdpa until xpu version is fixed
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/triton_kernels_benchmark/flash_attention_fwd_benchmark.py
+++ benchmarks/triton_kernels_benchmark/flash_attention_fwd_benchmark.py
@@ -229,8 +229,11 @@ def benchmark(Z, H, N_CTX, D_HEAD, provider):
             # FIXME: use torch sdpa for result check after https://github.com/intel/intel-xpu-backend-for-triton/issues/2042 fixed
             torch_fn = lambda: torch.nn.functional.scaled_dot_product_attention(
                 q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False, scale=sm_scale).to(torch.float32)
-            atol = 1e-1 if N_CTX == 16384 else 1e-2
-            benchmark_suit.assert_close(triton_fn(), torch_fn(), atol=atol, rtol=1e-3, err_msg='triton to torch')
+        else:
+            torch_fn = lambda: torch.nn.functional.scaled_dot_product_attention(q.cpu(), k.cpu(), v.cpu(
+            ), attn_mask=None, dropout_p=0.0, is_causal=False, scale=sm_scale).to(torch.float32)
+        atol = 1e-1 if N_CTX == 16384 else 1e-2
+        benchmark_suit.assert_close(triton_fn(), torch_fn(), atol=atol, rtol=1e-3, err_msg='triton to torch')
         _, min_ms, max_ms, mean, cv = benchmark_suit.do_bench(triton_fn, warmup=10, rep=10, quantiles=quantiles,
                                                               fast_flush=False)
 
