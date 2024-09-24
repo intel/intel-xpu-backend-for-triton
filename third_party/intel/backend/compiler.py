750506c9822e2f3718a04718722b84dd71d79e3e
Lu, Chengjun <chengjun.lu@intel.com>
2024-09-24 09:34:36 +0000

    Add op scheduler.

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -281,6 +281,7 @@ class XPUBackend(BaseBackend):
         intel.passes.ttgpuir.add_remove_layout_conversions(pm)
         intel.passes.ttgpuir.add_reduce_data_duplication(pm)
         passes.ttgpuir.add_reorder_instructions(pm)
+        # intel.passes.ttgpuir.add_schedule_ops(pm)
         passes.common.add_cse(pm)
         passes.common.add_symbol_dce(pm)
         passes.common.add_sccp(pm)
--- third_party/intel/triton_xpu.cc
+++ third_party/intel/triton_xpu.cc
@@ -123,6 +123,8 @@ void init_triton_intel_passes_ttgpuir(py::module &&m) {
                      gpu::intel::createTritonIntelGPUOptimizeReductionLocality);
   ADD_PASS_WRAPPER_0("add_reduce_variable_liveness",
                      gpu::intel::createTritonIntelGPUReduceVariableLiveness);
+  ADD_PASS_WRAPPER_0("add_schedule_ops",
+                     gpu::intel::createTritonIntelGPUScheduleOps);
 }
 
 void init_triton_intel_passes_arith(py::module &&m) {
