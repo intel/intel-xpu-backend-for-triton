c093068daec4808540548df4f96d45fae0409812
Anatoly Myachev <anatoly.myachev@intel.com>
2024-09-24 15:05:13 +0000

    Try PyTorch PR#135406
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -18,3 +18,4 @@ cd "$REPO_ROOT"
 curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
 curl -sSL https://github.com/pytorch/pytorch/pull/126456.diff | git apply -
 curl -sSL https://github.com/pytorch/pytorch/pull/136280.diff | git apply -
+curl -sSL https://github.com/pytorch/pytorch/pull/135406.diff | git apply -
