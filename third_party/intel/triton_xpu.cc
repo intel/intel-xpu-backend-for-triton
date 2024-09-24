fc8b4f538f3e5d139da0b7305f8f9532208036c8
Lu, Chengjun <chengjun.lu@intel.com>
2024-09-24 09:34:36 +0000

    Add op scheduler.

--- third_party/intel/triton_xpu.cc
+++ third_party/intel/triton_xpu.cc
@@ -98,6 +98,8 @@ void init_triton_intel_passes_ttgpuir(py::module &&m) {
                      gpu::intel::createTritonIntelGPUReduceDataDuplication);
   ADD_PASS_WRAPPER_0("add_materialize_block_pointer",
                      gpu::intel::createTritonIntelGPUMaterializeBlockPointer);
+  ADD_PASS_WRAPPER_0("add_schedule_ops",
+                     gpu::intel::createTritonIntelGPUScheduleOps);
 }
 
 void init_triton_intel(py::module &&m) {
