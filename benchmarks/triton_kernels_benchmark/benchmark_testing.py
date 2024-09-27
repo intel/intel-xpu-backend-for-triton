343eff5c495d18c1b3c8f1f162d32604a0975f1e
Anatoly Myachev <anatoly.myachev@intel.com>
2024-09-27 17:00:30 +0000

    fixes
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/triton_kernels_benchmark/benchmark_testing.py
+++ benchmarks/triton_kernels_benchmark/benchmark_testing.py
@@ -126,8 +126,8 @@ def do_bench_ipex(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, fas
     return _summarize_statistics(times, quantiles, return_mode)
 
 
-def do_bench_elapsed_time(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, fast_flush=True, return_mode="mean",
-                          device="xpu"):
+def do_bench_elapsed_time(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, fast_flush=True,
+                          return_mode="mean", device="xpu"):
     """
     Benchmark the runtime of the provided function. By default, return the median runtime of :code:`fn` along with
     the 20-th and 80-th performance percentile.
@@ -155,7 +155,6 @@ def do_bench_elapsed_time(fn, warmup=25, rep=100, grad_to_none=None, quantiles=N
     return _summarize_statistics(times, quantiles, return_mode)
 
 
-do_bench = do_bench_no_ipex
 if USE_IPEX_OPTION:
     do_bench = do_bench_ipex
 elif BENCHMARKING_METHOD == "ELAPSED_TIME":
