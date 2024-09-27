c7f49ebfef59ef4fb2787449848166e3228ca12e
Anatoly Myachev <anatoliimyachev@mail.com>
2024-09-27 14:12:55 +0200

    Apply suggestions from code review

--- scripts/capture-hw-details.sh
+++ scripts/capture-hw-details.sh
@@ -58,9 +58,9 @@ else
     COMPILER_VERSION="Not installed"
 fi
 
-if [[ "${USE_IPEX}" == "1" ]]; then
+if [[ "${USE_IPEX:undefined}" == "1" ]]; then
   BENCHMARKING_METHOD="PYTORCH_LEGACY_PROFILER_USING_IPEX"
-elif [[ "${USE_IPEX}" == "0" ]]; then
+elif [[ "${USE_IPEX:undefined}" == "0" ]]; then
   BENCHMARKING_METHOD="ELAPSED_TIME"
 fi
 
