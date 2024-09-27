2d04f26f039d5eff416ea41cbbc42858acbd54d1
Egor Krivov <egor.krivov@intel.com>
2024-09-27 13:25:41 +0000

    Fixed style

--- scripts/build_report.py
+++ scripts/build_report.py
@@ -8,17 +8,27 @@ import pandas as pd
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Build report based on triton-benchmark run")
+    parser = argparse.ArgumentParser(
+        description="Build report based on triton-benchmark run"
+    )
     parser.add_argument("source", help="Path to source csv file with benchmark results")
     parser.add_argument(
         "target",
         help="Path to result csv file with benchmark results including host info and dates",
     )
-    parser.add_argument("--param_cols", help="Names of parameter columns, separated by commas.", required=True)
+    parser.add_argument(
+        "--param_cols",
+        help="Names of parameter columns, separated by commas.",
+        required=True,
+    )
     parser.add_argument("--benchmark", help="Name of the benchmark.", required=True)
-    parser.add_argument("--compiler", help="Name of the compiler, like `triton`.", required=True)
+    parser.add_argument(
+        "--compiler", help="Name of the compiler, like `triton`.", required=True
+    )
     parser.add_argument("--tflops_col", help="Column name with tflops.", required=True)
-    parser.add_argument("--hbm_col", help="Column name with HBM results.", required=False, default=None)
+    parser.add_argument(
+        "--hbm_col", help="Column name with HBM results.", required=False, default=None
+    )
     parser.add_argument("--tag", help="How to tag results", required=False, default="")
     return parser.parse_args()
 
@@ -26,7 +36,9 @@ def parse_args():
 def check_cols(target_cols, all_cols):
     diff = set(target_cols).difference(all_cols)
     if len(diff) != 0:
-        raise ValueError(f"Couldn't find required columns: '{diff}' among available '{all_cols}'")
+        raise ValueError(
+            f"Couldn't find required columns: '{diff}' among available '{all_cols}'"
+        )
 
 
 def transform_df(df, param_cols, tflops_col, hbm_col, benchmark, compiler, tag):
@@ -34,7 +46,9 @@ def transform_df(df, param_cols, tflops_col, hbm_col, benchmark, compiler, tag):
     check_cols([tflops_col] + [] if hbm_col is None else [hbm_col], df.columns)
     # Build json with parameters
     df_results = pd.DataFrame()
-    df_results["params"] = [json.dumps(j) for j in df[param_cols].astype(int).to_dict("records")]
+    df_results["params"] = [
+        json.dumps(j) for j in df[param_cols].astype(int).to_dict("records")
+    ]
     df_results["tflops"] = df[tflops_col]
     if hbm_col is not None:
         df_results["hbm_gbs"] = df[hbm_col]
@@ -47,10 +61,20 @@ def transform_df(df, param_cols, tflops_col, hbm_col, benchmark, compiler, tag):
 
     host_info = {
         n: os.getenv(n.upper(), default="")
-        for n in ["libigc1_version", "level_zero_version", "gpu_device", "agama_version", "torch_version", "compiler_version", "benchmarking_method"]
+        for n in [
+            "libigc1_version",
+            "level_zero_version",
+            "gpu_device",
+            "agama_version",
+            "torch_version",
+            "compiler_version",
+            "benchmarking_method",
+        ]
     }
     if not host_info["gpu_device"]:
-        raise RuntimeError("Could not find GPU device description, was `capture-hw-details.sh` called?")
+        raise RuntimeError(
+            "Could not find GPU device description, was `capture-hw-details.sh` called?"
+        )
     for name, val in host_info.items():
         df_results[name] = val
 
@@ -61,8 +85,15 @@ def main():
     args = parse_args()
     param_cols = args.param_cols.split(",")
     df = pd.read_csv(args.source)
-    result_df = transform_df(df, param_cols=param_cols, tflops_col=args.tflops_col, hbm_col=args.hbm_col,
-                             benchmark=args.benchmark, compiler=args.compiler, tag=args.tag)
+    result_df = transform_df(
+        df,
+        param_cols=param_cols,
+        tflops_col=args.tflops_col,
+        hbm_col=args.hbm_col,
+        benchmark=args.benchmark,
+        compiler=args.compiler,
+        tag=args.tag,
+    )
     result_df.to_csv(args.target, index=False)
 
 
