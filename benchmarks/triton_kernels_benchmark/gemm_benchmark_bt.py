a2b8c5a7a28115fefd9dee84305f2e1d9ef775bc
Egor Krivov <egor.krivov@intel.com>
2024-10-02 15:34:06 +0000

    update

--- benchmarks/triton_kernels_benchmark/gemm_benchmark_bt.py
+++ benchmarks/triton_kernels_benchmark/gemm_benchmark_bt.py
@@ -231,7 +231,7 @@ def matmul(a, b, c):
         # possible values for `line_arg``
         line_vals=['triton'],
         # label name for the lines
-        line_names=['Triton', 'XeTLA'],
+        line_names=['Triton'],
         # line styles
         styles=[('green', '-'), ('green', '--'), ('blue', '-'), ('blue', '--')],
         ylabel=['GB/s', 'TFlops'],  # label name for the y-axis
