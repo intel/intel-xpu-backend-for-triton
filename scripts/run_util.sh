f95f102a0a53e0583e51667b7bd2cfe16bdc2bd1
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-07 16:40:16 +0200

    Add conda runner, r

--- scripts/run_util.sh
+++ scripts/run_util.sh
@@ -6,15 +6,9 @@ while [ -v 1 ]; do
       python_version=$2
       shift 2
       ;;
-    --unit)
-      TEST_UNIT=true
-      shift
-      ;;
-    --core)
-      TEST_CORE=true
-      shift
     *)
       script_name=$1
       shift
+      ;;
   esac
 done
