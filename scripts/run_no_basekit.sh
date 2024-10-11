15a01cf5554d922991d3a20b86237969afbd720a
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-11 17:07:33 +0200

    Add no_basekit debug

--- scripts/run_no_basekit.sh
+++ scripts/run_no_basekit.sh
@@ -15,7 +15,9 @@ install_conda() {
     wget https://files.pythonhosted.org/packages/cc/1e/d74e608f0c040e4f72dbfcd3b183f39570f054d08de39cc431f153220d90/intel_sycl_rt-2024.1.2-py2.py3-none-manylinux1_x86_64.whl
     pip install ./intel_sycl_rt-2024.1.2-py2.py3-none-manylinux1_x86_64.whl dpcpp_cpp_rt==2024.1.2
 
-    ln -snf /opt/intel/oneapi/compiler/2024.1/include/sycl $HOME/miniforge3/envs/triton/x86_64-conda-linux-gnu/sysroot/usr/include/sycl
+    ln -snf /opt/intel/oneapi/compiler/2024.1/include/sycl $HOME/miniforge3/envs/triton/include/sycl
+    ls -l $HOME/miniforge3/envs/triton/include/sycl/
+    exit 1
 }
 
 script_dir=$(dirname "$0")
