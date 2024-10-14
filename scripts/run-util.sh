6e7343bd5178be0d480434fb425b25d15a1b65ca
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-14 17:16:41 +0200

    Split env installation into a separate step, r2

--- scripts/run-util.sh
+++ scripts/run-util.sh
@@ -2,17 +2,12 @@ print_env_info() {
     conda info
     conda list -n triton
 }
-
+set -vx
 while [ -v 1 ]; do
   case "$1" in
     --python-version)
       python_version=$2
       shift 2
-      ;;
-    install-env)
-      install_env
-      print_env_info
-      exit 0
       ;;
     *)
       script_name=$1
@@ -20,3 +15,10 @@ while [ -v 1 ]; do
       ;;
   esac
 done
+
+if [ $(head -1 "$script_name") = install-env ]
+then
+  install_env
+  print_env_info
+  exit 0
+fi
