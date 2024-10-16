25122b85a03c8c2ea33a42aace97b1dc3d0b5243
Gregory Shimansky <gregory.shimansky@intel.com>
2024-10-16 01:44:59 +0000

    Fixed print_helper cuda windows addition
    Signed-off-by: Gregory Shimansky <gregory.shimansky@intel.com>

--- python/test/unit/language/print_helper.py
+++ python/test/unit/language/print_helper.py
@@ -140,7 +140,8 @@ def test_print(func: str, data_type: str, device: str):
        func != "print_multiple_args" and func != "device_print_multiple_args" and \
        func != "device_print_pointer" and func != "device_print_scalar":
         assert_close(y, x)
-    torch.cuda.synchronize()
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
 
     # Wait until driver complete all the jobs for the device_print, especially test_subprocess
     # require this which captures stdout when child exits.
