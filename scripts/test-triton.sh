af275afaeeb9161ed648de32a92b8d13b293ba85
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-21 15:51:40 +0200

    Try adding sycl to includes, r6

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -322,7 +322,7 @@ run_inductor_tests() {
 
   pip install pyyaml pandas scipy numpy psutil pyre_extensions torchrec
 
-  ZE_AFFINITY_MASK=0 python pytorch/benchmarks/dynamo/huggingface.py --accuracy --float32 -dxpu -n10 --no-skip --dashboard --inference --freezing --total-partitions 1 --partition-id 0 --only AlbertForMaskedLM --backend=inductor --timeout=4800 --output=inductor_log.csv
+  ZE_AFFINITY_MASK=0 python pytorch/benchmarks/dynamo/huggingface.py --accuracy --float32 -dxpu -n10 --no-skip --dashboard --inference --freezing --total-partitions 1 --partition-id 0 --only AlbertForMaskedLM --backend=inductor --timeout=4800 --output=$(pwd -P)/inductor_log.csv
 
   cat inductor_log.csv
   grep AlbertForMaskedLM inductor_log.csv | grep -q ,pass,
