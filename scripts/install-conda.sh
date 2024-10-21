80663bcc06178a4bc7769d6522e1486f76e403e3
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-21 15:06:45 +0200

    Try adding sycl to includes, r2

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 install_env() {
+    set -vx
     export PATH="$HOME/miniforge3/bin:$PATH"
     conda create -n triton --override-channels -c conda-forge python=$python_version.*
     conda env update -f scripts/triton.yml
     find /opt/intel/oneapi/ \( -name '*.so' -or -name '*.so.*' \) -exec cp -n {} $HOME/miniforge3/envs/triton/lib \;
     ln -snf /usr/include/level_zero $HOME/miniforge3/envs/triton/bin/../x86_64-conda-linux-gnu/sysroot/usr/include/level_zero
     find /usr -name libze_\* -exec cp -n {} $HOME/miniforge3/envs/triton/lib \;
-    ln -snf /opt/intel/oneapi/compiler/2024.1/include/sycl $HOME/miniforge3/envs/triton/lib/python3.9/site-packages/triton/backends/intel/include
+    ln -snf /opt/intel/oneapi/compiler/2024.1/include/sycl $HOME/miniforge3/envs/triton/lib/python$python_version/site-packages/triton/backends/intel/include
+    ls -l /home/runner/miniforge3/envs/triton/lib/python$python_version/site-packages/triton/backends/intel/include/
+    ls -l /home/runner/miniforge3/envs/triton/lib/python$python_version/site-packages/triton/backends/intel/include
 }
 
 print_env_info() {
