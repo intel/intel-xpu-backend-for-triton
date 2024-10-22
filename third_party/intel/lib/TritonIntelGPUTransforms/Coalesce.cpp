547d6faed49fd892a83cb7abeb2b93330191d6ce
Tiotto, Ettore <ettore.tiotto@intel.com>
2024-10-22 19:19:57 +0000

    Fix pre_commit
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- third_party/intel/lib/TritonIntelGPUTransforms/Coalesce.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/Coalesce.cpp
@@ -373,7 +373,8 @@ public:
     });
 
     LLVM_DEBUG({
-      DBGS() << "\nlayoutMap:" << "\n";
+      DBGS() << "\nlayoutMap:"
+             << "\n";
       for (auto [op, encoding] : layoutMap) {
         DBGS() << "op: " << *op << "\n";
         DBGS() << "encoding: " << encoding << "\n\n";
