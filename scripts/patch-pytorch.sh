fe8df22df2db234da1483e5c4e5f527f4f8bed6e
Alex Baden <alexander.baden@intel.com>
2024-10-23 13:22:52 -0700

    use a different patch

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -17,4 +17,4 @@ cd "$REPO_ROOT"
 
 curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
 curl -sSL https://github.com/pytorch/pytorch/pull/126456.diff | git apply -
-curl -sSL https://github.com/pytorch/pytorch/pull/138456.diff | git apply -
+curl -sSL https://github.com/pytorch/pytorch/pull/138548.diff | git apply -
