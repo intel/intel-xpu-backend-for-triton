d132f2269ad12c440370415fcd8dd2acefbad814
Anatoly Myachev <anatoly.myachev@intel.com>
2024-11-18 22:26:11 +0000

    implement 'get_default_context' as a free function
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- third_party/intel/backend/driver.c
+++ third_party/intel/backend/driver.c
@@ -166,6 +166,28 @@ struct BuildFlags {
   }
 };
 
+#ifdef WIN32
+sycl::context get_default_context(const sycl::device &sycl_device) {
+  auto platform = sycl_device.get_platform();
+  sycl::context ctx;
+  try {
+    ctx = platform.ext_oneapi_get_default_context();
+  } catch (const std::runtime_error &ex) {
+    // This exception is thrown on Windows because
+    // ext_oneapi_get_default_context is not implemented. But it can be safely
+    // ignored it seems.
+#if _DEBUG
+    std::cout << "ERROR: " << ex.what() << std::endl;
+#endif
+  }
+  return ctx;
+}
+#else
+sycl::context get_default_context(const sycl::device &sycl_device) {
+  return sycl_device.get_platform().ext_oneapi_get_default_context();
+}
+#endif
+
 static PyObject *loadBinary(PyObject *self, PyObject *args) {
   const char *name, *build_flags_ptr;
   int shared;
@@ -194,18 +216,7 @@ static PyObject *loadBinary(PyObject *self, PyObject *args) {
     const size_t binary_size = PyBytes_Size(py_bytes);
 
     uint8_t *binary_ptr = (uint8_t *)PyBytes_AsString(py_bytes);
-    auto platform = sycl_device.get_platform();
-    sycl::context ctx;
-    try {
-      ctx = platform.ext_oneapi_get_default_context();
-    } catch (const std::runtime_error &ex) {
-      // This exception is thrown on Windows because
-      // ext_oneapi_get_default_context is not implemented. But it can be safely
-      // ignored it seems.
-#if _DEBUG
-      std::cout << "ERROR: " << ex.what() << std::endl;
-#endif
-    }
+    sycl::context ctx = get_default_context(sycl_device);
     const auto l0_device =
         sycl::get_native<sycl::backend::ext_oneapi_level_zero>(sycl_device);
     const auto l0_context =
