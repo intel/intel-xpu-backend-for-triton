3ef26d06b2182079306099bd7e370777b3017f44
Alex Baden <alexander.baden@intel.com>
2024-11-20 10:45:30 -0700

    LLVM freeze instruction between mask and div 4/?

--- third_party/intel/lib/LLVMIR/LLVMIRFreezeMaskedDivRem.cpp
+++ third_party/intel/lib/LLVMIR/LLVMIRFreezeMaskedDivRem.cpp
@@ -18,23 +18,37 @@ static bool processPhiNode(PHINode *phiNode, BasicBlock& BB) {
 
   bool Changed = false;
   if (phiHasNullValue) {
+    for (Instruction &I : BB) {
+      if (I.getOpcode() == Instruction::SDiv || I.getOpcode() == Instruction::SRem) {
+        const size_t OpIdx = 1; // I.getOpcode() == Instruction::SRem ? 0 : 1;
+        if (I.getOperand(OpIdx) == phiNode) {
+          auto *freezePhi = new FreezeInst(phiNode, phiNode->getName() + ".frozen", I.getIterator());
+          I.setOperand(OpIdx, freezePhi);
+          Changed = true;
+        }
+      }
+    }
+#if 0
         auto FindUse = llvm::find_if(phiNode->users(), [](auto *U) {
           auto *Use = cast<Instruction>(U);
           llvm::errs() << "User: " << *Use << "\n";
-          return (Use->getOpcode() == Instruction::SDiv);
+          return (Use->getOpcode() == Instruction::SDiv || Use->getOpcode() == Instruction::SRem);
         });
         if (FindUse == phiNode->user_end()) {
           llvm::errs() << "no div :(\n";
           return false; 
         }
         auto *Use = cast<Instruction>(*FindUse);
-        if (Use->getOperand(1) == phiNode) {
+        assert(Use->isIntDivRem());
+        const size_t OpIdx = Use->getOpcode() == Instruction::SRem ? 0 : 1;
+        if (Use->getOperand(OpIdx) == phiNode) {
           llvm::errs() << "Got our user! " << *Use << "\n";
           llvm::errs() << "Operand 1: " << *Use->getOperand(1) << "\n";
           auto *freezePhi = new FreezeInst(phiNode, phiNode->getName() + ".frozen", Use->getIterator());
-          Use->setOperand(1, freezePhi);
+          Use->setOperand(OpIdx, freezePhi);
           Changed = true;
         }
+#endif
   }
   return Changed; 
 }
