8381df4323a2ff1d265e02a51db84f81200971a6
Pavel Chekin <pavel.chekin@intel.com>
2024-11-21 12:26:58 -0800

    Replace USE_MKL_STATIC with USE_STATIC_MKL

--- scripts/install-pytorch.sh
+++ scripts/install-pytorch.sh
@@ -155,7 +155,7 @@ $SCRIPTS_DIR/patch-pytorch.sh
 echo "****** Building $PYTORCH_PROJ ******"
 pip install -r requirements.txt
 pip install cmake ninja
-USE_MKL_STATIC=1 python setup.py bdist_wheel
+USE_STATIC_MKL=1 python setup.py bdist_wheel
 
 echo "****** Installing PyTorch ******"
 pip install dist/*.whl
