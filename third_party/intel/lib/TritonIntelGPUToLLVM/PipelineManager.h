a056fd48f7a09f47c882eca2285725f803686bc0
victor-eds <victor.perez@codeplay.com>
2024-11-28 09:50:58 +0000

    Try diff constructor

--- third_party/intel/lib/TritonIntelGPUToLLVM/PipelineManager.h
+++ third_party/intel/lib/TritonIntelGPUToLLVM/PipelineManager.h
@@ -120,8 +120,9 @@ struct FuncOpConversion : public ConvertOpToLLVMPattern<triton::FuncOp> {
       addToOpenCLKernels(newFuncOp, rewriter);
     }
 
-    newFuncOp.setReqdWorkGroupSize(
-        ArrayRef<int>{1, 1, threadsPerWarp * numWarps});
+    constexpr size_t numDims = 3;
+    std::array<int, numDims> localSize{threadsPerWarp * numWarps, 1, 1};
+    newFuncOp.setReqdWorkGroupSize(localSize);
     newFuncOp.setIntelReqdSubGroupSize(threadsPerWarp);
 
     if (!LLVM::isKernel(funcOp)) {
