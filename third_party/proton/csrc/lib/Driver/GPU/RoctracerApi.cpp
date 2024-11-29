068e166d606e4574fc0552daa492d1dca415ccf1
Anatoly Myachev <anatoly.myachev@intel.com>
2024-11-29 16:13:55 +0100

    more guards for 'dlsym'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- third_party/proton/csrc/lib/Driver/GPU/RoctracerApi.cpp
+++ third_party/proton/csrc/lib/Driver/GPU/RoctracerApi.cpp
@@ -32,8 +32,13 @@ void start() {
   Dispatch<ExternLibRoctracer>::init(ExternLibRoctracer::name,
                                      &ExternLibRoctracer::lib);
   if (func == nullptr)
+#ifdef WIN32
+    func = reinterpret_cast<roctracer_start_t>(
+        GetProcAddress((HMODULE)ExternLibHip::lib, "roctracer_start"));
+#else
     func = reinterpret_cast<roctracer_start_t>(
         dlsym(ExternLibRoctracer::lib, "roctracer_start"));
+#endif
   if (func)
     func();
 }
@@ -44,8 +49,13 @@ void stop() {
   Dispatch<ExternLibRoctracer>::init(ExternLibRoctracer::name,
                                      &ExternLibRoctracer::lib);
   if (func == nullptr)
+#ifdef WIN32
+    func = reinterpret_cast<roctracer_stop_t>(
+        GetProcAddress((HMODULE)ExternLibHip::lib, "roctracer_stop"));
+#else
     func = reinterpret_cast<roctracer_stop_t>(
         dlsym(ExternLibRoctracer::lib, "roctracer_stop"));
+#endif
   if (func)
     func();
 }
@@ -56,8 +66,13 @@ char *getOpString(uint32_t domain, uint32_t op, uint32_t kind) {
   Dispatch<ExternLibRoctracer>::init(ExternLibRoctracer::name,
                                      &ExternLibRoctracer::lib);
   if (func == nullptr)
+#ifdef WIN32
+    func = reinterpret_cast<roctracer_op_string_t>(
+        GetProcAddress((HMODULE)ExternLibHip::lib, "roctracer_op_string"));
+#else
     func = reinterpret_cast<roctracer_op_string_t>(
         dlsym(ExternLibRoctracer::lib, "roctracer_op_string"));
+#endif
   return (func ? func(domain, op, kind) : NULL);
 }
 
