bef886bad7068a14721e0b5c3155a79de411bd70
Lu, Chengjun <chengjun.lu@intel.com>
2024-12-03 09:36:52 +0000

    Support to run the Triton kernel from cached IR.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -52,6 +52,7 @@ class XPUOptions:
     arch: str = None
     # FIXME: enable for XPU: https://github.com/intel/intel-xpu-backend-for-triton/issues/4954
     instrumentation_mode: str = ""
+    shared: int = 0
 
     def __post_init__(self):
         default_libdir = Path(__file__).parent / 'lib'
@@ -398,6 +399,8 @@ class XPUBackend(BaseBackend, metaclass=XPUBackendMeta):
     @classmethod
     @track
     def make_spv(cls, src, metadata, options):
+        if src is not str:
+            src = str(src)
         spirv, name = intel.translate_to_spirv(src)
         metadata["name"] = name
         metadata.setdefault("build_flags", "")
