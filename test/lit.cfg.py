40dd73227bcb1dfdfca33b33d41aa3ca93582ad2
Anatoly Myachev <anatoly.myachev@intel.com>
2024-12-04 17:53:43 +0000

    update lit configuration
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -45,6 +45,7 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.triton_obj_root, 'test')
 config.triton_tools_dir = os.path.join(config.triton_obj_root, 'bin')
+config.triton_intel_plugin_tools_dir = os.path.join(config.triton_obj_root, 'third_party', 'intel', 'bin')
 config.filecheck_dir = os.path.join(config.triton_obj_root, 'bin', 'FileCheck')
 
 # FileCheck -enable-var-scope is enabled by default in MLIR test
@@ -52,7 +53,7 @@ config.filecheck_dir = os.path.join(config.triton_obj_root, 'bin', 'FileCheck')
 # it can be explicitly opted-in by prefixing the variable name with $
 config.environment["FILECHECK_OPTS"] = "--enable-var-scope"
 
-tool_dirs = [config.triton_tools_dir, config.llvm_tools_dir, config.filecheck_dir]
+tool_dirs = [config.triton_tools_dir, config.triton_intel_plugin_tools_dir, config.llvm_tools_dir, config.filecheck_dir]
 
 # Tweak the PATH to include the tools dir.
 for d in tool_dirs:
