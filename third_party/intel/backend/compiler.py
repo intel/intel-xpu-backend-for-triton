d378a2ba1aac73ecae0abc4457e817003fa3ed8a
Whitney Tsang <whitney.tsang@intel.com>
2024-12-11 19:29:36 +0000

    Fix LTS failure
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -146,7 +146,15 @@ class XPUBackend(BaseBackend):
         dev_prop['max_num_sub_groups'] = tgt_prop.get('max_num_sub_groups', None)
         dev_prop['sub_group_sizes'] = tgt_prop.get('sub_group_sizes', None)
         dev_prop['has_fp64'] = tgt_prop.get('has_fp64', None)
+
         device_arch = self.parse_device_arch(tgt_prop.get('architecture', 0))
+        # Note: LTS driver does not support ocloc query CL_DEVICE_EXTENSIONS.
+        try:
+            ocloc_cmd = ['ocloc', 'query', 'CL_DEVICE_EXTENSIONS']
+            with tempfile.TemporaryDirectory() as temp_dir:
+                output = subprocess.check_output(ocloc_cmd, text=True, cwd=temp_dir)
+        except subprocess.CalledProcessError:
+            device_arch = ''
         if device_arch != '':
             try:
                 ocloc_cmd = ['ocloc', 'query', 'CL_DEVICE_EXTENSIONS', '-device', device_arch]
