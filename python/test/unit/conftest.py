650428be73bd574e5b3a9a1494b543bd5f39c104
Pavel Chekin <pavel.chekin@intel.com>
2024-12-16 22:39:04 -0800

    Ignore cleanup errors in cache teardown. (#3020)
    To avoid PermissionError on Windows where certain .pyd files are locked
    by current Python process and cannot be deleted.
    
    Fixes #3019.

--- python/test/unit/conftest.py
+++ python/test/unit/conftest.py
@@ -14,9 +14,13 @@ def device(request):
 
 @pytest.fixture
 def fresh_triton_cache():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        try:
-            os.environ["TRITON_CACHE_DIR"] = tmpdir
-            yield tmpdir
-        finally:
-            os.environ.pop("TRITON_CACHE_DIR", None)
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            try:
+                os.environ["TRITON_CACHE_DIR"] = tmpdir
+                yield tmpdir
+            finally:
+                os.environ.pop("TRITON_CACHE_DIR", None)
+    except OSError:
+        # Ignore errors, such as PermissionError, on Windows
+        pass
