aa84a03705ed47b0575a3b375eaf506fcbbe8faf
Whitney Tsang <whitney.tsang@intel.com>
2024-12-28 21:19:03 +0000

    [Intel] Fix test_zero_strided_tensors
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -6577,7 +6577,7 @@ def test_zero_strided_tensors(device):
 
     a, b, c = x.shape
     grid = (a, b, c)
-    with torch.cuda.device(x.device.index):
+    with device == 'cuda' and torch.cuda.device(x.device.index) or torch.xpu.device(x.device.index):
         _simple_add[grid](x, x.stride(0), x.stride(1))
 
     assert torch.allclose(x, torch.ones_like(x) * c_dim)
