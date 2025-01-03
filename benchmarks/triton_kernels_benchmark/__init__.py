82af4c73fccecb071d25f83036a214580ca1e5c7
Anatoly Myachev <anatoliimyachev@mail.com>
2025-01-03 12:23:53 +0100

    [NFC] Remove `benchmark_driver.py` (#3082)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/triton_kernels_benchmark/__init__.py
+++ benchmarks/triton_kernels_benchmark/__init__.py
@@ -1,2 +1,6 @@
+import os
+
 from .benchmark_testing import do_bench, assert_close, perf_report, Benchmark, BENCHMARKING_METHOD  # type: ignore # noqa: F401
-from . import benchmark_driver  # type: ignore # noqa: F401
+
+if BENCHMARKING_METHOD == "UPSTREAM_PYTORCH_PROFILER":
+    os.environ["INJECT_PYTORCH"] = "True"
