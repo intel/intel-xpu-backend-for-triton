0cb10dcc12ec6ff3ba8078aa636063dfdf867fca
Anatoly Myachev <anatoly.myachev@intel.com>
2025-01-03 21:02:56 +0100

    cleanup
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -17,17 +17,4 @@ echo "Applying PyTorch patches in $REPO_ROOT"
 cd "$REPO_ROOT"
 
 curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
-# outdated
-# curl -sSL https://github.com/pytorch/pytorch/pull/143154.diff | git apply -
-git apply "${SCRIPT_DIR}/pytorch2.patch"
-git apply "${SCRIPT_DIR}/triton_kernel_wrap.patch"
-git apply "${SCRIPT_DIR}/test_codegen_triton.patch"
-
-
-git apply "${SCRIPT_DIR}/test_triton_kernels.py.patch"
-
-
-git apply "${SCRIPT_DIR}/triton.py.patch"
-git apply "${SCRIPT_DIR}/wrapper.py.patch"
-# REVERT ME: it's just a trigger for pytorch rebuild
-# git apply "${SCRIPT_DIR}/pytorch.patch"
+git apply "${SCRIPT_DIR}/pytorch.patch"
