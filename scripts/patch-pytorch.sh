9a24ae1271cdce2c151b0508b4d4b87b65f14d60
Anatoly Myachev <anatoly.myachev@intel.com>
2025-01-03 12:29:42 +0100

    fix 'test_triton_kernels.py.patch'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -22,6 +22,7 @@ curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
 git apply "${SCRIPT_DIR}/pytorch2.patch"
 git apply "${SCRIPT_DIR}/triton_kernel_wrap.patch"
 git apply "${SCRIPT_DIR}/test_codegen_triton.patch"
+
 git apply "${SCRIPT_DIR}/test_triton_kernels.py.patch"
 git apply "${SCRIPT_DIR}/triton.py.patch"
 git apply "${SCRIPT_DIR}/wrapper.py.patch"
