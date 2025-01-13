0eb4d3c9e9efa88a1da0968ec4811e2fe69db72d
Lu,Chengjun <chengjun.lu@intel.com>
2025-01-13 14:29:05 +0000

    Revert "Hard code to jit kernel for NV"
    This reverts commit 2a6f7a12a67945ecb2331a69c668d4a49b25c821.

--- python/triton/_internal_testing.py
+++ python/triton/_internal_testing.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import triton
 import triton.language as tl
+from triton.backends.nvidia.compiler import _path_to_binary
 import pytest
 
 from numpy.random import RandomState
@@ -37,8 +38,7 @@ def is_cuda():
 
 
 def is_hopper():
-    return True
-    # return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
+    return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
 
 
 def is_hip():
@@ -161,12 +161,11 @@ def supports_tma(byval_only=False):
         return True
     if not is_cuda():
         return False
-    return True
-    # _, cuda_version = _path_to_binary("ptxas")
-    # min_cuda_version = (12, 0) if byval_only else (12, 3)
-    # cuda_version_tuple = tuple(map(int, cuda_version.split(".")))
-    # assert len(cuda_version_tuple) == 2, cuda_version_tuple
-    # return torch.cuda.get_device_capability()[0] >= 9 and cuda_version_tuple >= min_cuda_version
+    _, cuda_version = _path_to_binary("ptxas")
+    min_cuda_version = (12, 0) if byval_only else (12, 3)
+    cuda_version_tuple = tuple(map(int, cuda_version.split(".")))
+    assert len(cuda_version_tuple) == 2, cuda_version_tuple
+    return torch.cuda.get_device_capability()[0] >= 9 and cuda_version_tuple >= min_cuda_version
 
 
 def tma_skip_msg(byval_only=False):
