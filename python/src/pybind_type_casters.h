7c79399e6b6abd62f252dccc2ae2e0630bc0eb92
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2025-01-14 15:18:41 +0100

    Add Python bindings for llvm::SmallVector

---
+++ python/src/pybind_type_casters.h
@@ -0,0 +1,43 @@
+#ifndef PYBIND_TYPE_CASTERS_H
+#define PYBIND_TYPE_CASTERS_H
+
+#include "llvm/ADT/SmallVector.h"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
+namespace pybind11 {
+namespace detail {
+
+////////////////////////////////////////////////////////////////////////////////
+/// LLVM::SMALLVECTOR BINDING
+////////////////////////////////////////////////////////////////////////////////
+
+template <>
+struct type_caster<llvm::SmallVector<std::string>> {
+  PYBIND11_TYPE_CASTER(llvm::SmallVector<std::string>, _("List[str]"));
+
+  bool load(handle src, bool) {
+    if (!pybind11::isinstance<sequence>(src)) {
+      return false;
+    }
+
+    auto vec = pybind11::reinterpret_borrow<sequence>(src);
+    llvm::SmallVector<std::string> tmp;
+    for (auto s : vec) {
+      if (!pybind11::isinstance<str>(s)) {
+        return false;
+      }
+      tmp.push_back(pybind11::cast<std::string>(s));
+    }
+    value = std::move(tmp);
+
+    return true;
+  }
+};
+
+} // namespace detail
+} // namespace pybind11
+
+#endif // PYBIND_TYPE_CASTERS_H
