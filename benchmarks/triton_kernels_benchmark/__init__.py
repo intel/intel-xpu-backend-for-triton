f18528efbe5290993bc0cd36fdf7cf2ef8a69231
Anatoly Myachev <anatoliimyachev@mail.com>
2025-01-14 21:28:08 +0100

    Reuse `assert_close` for benchmarks from `triton.testing` (#3161)
    Part of #3160
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/triton_kernels_benchmark/__init__.py
+++ benchmarks/triton_kernels_benchmark/__init__.py
@@ -1,6 +1,16 @@
 import os
 
-from .benchmark_testing import do_bench, assert_close, perf_report, Benchmark, BENCHMARKING_METHOD  # type: ignore # noqa: F401
+from triton.testing import assert_close
+
+from .benchmark_testing import do_bench, perf_report, Benchmark, BENCHMARKING_METHOD
 
 if BENCHMARKING_METHOD == "UPSTREAM_PYTORCH_PROFILER":
     os.environ["INJECT_PYTORCH"] = "True"
+
+__all__ = [
+    "assert_close",
+    "do_bench",
+    "perf_report",
+    "Benchmark",
+    "BENCHMARKING_METHOD",
+]
