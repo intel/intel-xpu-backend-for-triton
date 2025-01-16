f9f2e995efe891bfd6ab74c2bd0fe9633277ac43
Anatoly Myachev <anatoly.myachev@intel.com>
2025-01-16 20:59:44 +0100

    fix
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -11,7 +11,7 @@ def is_xpu():
 
 def _cc_cmd(cc, src, out, include_dirs, library_dirs, libraries):
     if "cl" in cc or "clang-cl" in cc:
-        cc_cmd = [cc, src, "/nologo", "/O2", "/LD"]
+        cc_cmd = [cc, "/std:c++17", src, "/nologo", "/O2", "/LD", "/MD"]
         cc_cmd += [f"/I{dir}" for dir in include_dirs]
         cc_cmd += [f"/Fo{os.path.join(os.path.dirname(out), 'main.obj')}"]
         cc_cmd += ["/link"]
@@ -78,7 +78,8 @@ def _build(name, src, srcdir, library_dirs, include_dirs, libraries, extra_compi
         if cxx is icpx:
             extra_compile_args += ["-fsycl"]
         else:
-            extra_compile_args += ["--std=c++17"] if os.name != "nt" else ["/std:c++17"]
+            if os.name != "nt":
+                extra_compile_args += ["--std=c++17"]
         if os.name == "nt":
             library_dirs = library_dirs + [os.path.join(sysconfig.get_paths(scheme=scheme)["stdlib"], "..", "libs")]
     else:
