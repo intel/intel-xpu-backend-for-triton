5379cfa7ec4f8b5d4039ab48c958453b73d1af0c
Anatoly Myachev <anatoly.myachev@intel.com>
2025-01-22 20:32:16 +0100

    Provide more info in assert message in 'do_bench_upstream_pytorch_profiler'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- benchmarks/triton_kernels_benchmark/benchmark_testing.py
+++ benchmarks/triton_kernels_benchmark/benchmark_testing.py
@@ -158,7 +158,9 @@ def do_bench_upstream_pytorch_profiler(fn, n_warmup=25, n_repeat=100, grad_to_no
     # for correct registration of kernels.
     # For details: https://github.com/pytorch/pytorch/issues/144778
     kernels = [kernel for kernel in kernels if kernel != []]
-    assert len(kernels) == n_repeat, "the profiling number not match"
+    assert len(kernels) == n_repeat, (
+        f"the profiling number not match; {n_repeat=}, '{kernels=}', " +
+        f"top functions by xpu_time: {prof.key_averages(group_by_stack_n=5).table(sort_by='xpu_time')}")
     # Make the time to the milliseconds.
     times = torch.tensor([sum([k.duration for k in ks]) * 1e-3 for ks in kernels], dtype=torch.float)
     return _summarize_statistics(times, quantiles, return_mode)
