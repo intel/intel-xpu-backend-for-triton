179e7ba822ae89ad7632c539d08df5695df10ccf
dchigarev <dmitry.chigarev@intel.com>
2025-01-30 09:55:03 +0000

    back to o3
    Signed-off-by: dchigarev <dmitry.chigarev@intel.com>

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -327,7 +327,7 @@ class XPUBackend(BaseBackend):
             llvm.link_extern_libs(llvm_mod, paths)
         print("========== LLVM BEFORE OPT ===========")
         print(str(llvm_mod))
-        intel.optimize_module(llvm_mod, llvm.OPTIMIZE_O0)
+        intel.optimize_module(llvm_mod, llvm.OPTIMIZE_O3)
         print("=========== LLVM after OPT ========")
         print(str(llvm_mod))
         intel.post_process_llir(llvm_mod)
