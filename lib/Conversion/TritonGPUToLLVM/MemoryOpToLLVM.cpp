01168e9f100049e007e3c7f57c7f9ab9c41eb12c
Si, Yudong <yudong.si@intel.com>
2025-02-14 06:33:22 +0000

    revert changes in getScratchOnGlobalMemoryPtr

--- lib/Conversion/TritonGPUToLLVM/MemoryOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/MemoryOpToLLVM.cpp
@@ -32,9 +32,8 @@ void lowerDistributedToShared(
 struct GlobalScratchAllocOpConversion
     : public ConvertOpToLLVMPattern<triton::gpu::GlobalScratchAllocOp> {
   GlobalScratchAllocOpConversion(LLVMTypeConverter &converter,
-                                 const TargetInfoBase &targetInfo,
                                  PatternBenefit benefit)
-      : ConvertOpToLLVMPattern(converter, benefit), targetInfo(targetInfo) {}
+      : ConvertOpToLLVMPattern(converter, benefit) {}
 
   LogicalResult
   matchAndRewrite(triton::gpu::GlobalScratchAllocOp op, OpAdaptor adaptor,
@@ -51,14 +50,11 @@ struct GlobalScratchAllocOpConversion
     if (!funcOp) {
       return failure();
     }
-    Value ptr = targetInfo.getScratchOnGlobalMemoryPtr(loc, rewriter, funcOp,
-                                                       b.i32_val(opOffset));
+    Value ptr =
+        LLVM::getGlobalScratchPtr(loc, rewriter, funcOp, b.i32_val(opOffset));
     rewriter.replaceOp(op, ptr);
     return success();
   }
-
-private:
-  const TargetInfoBase &targetInfo;
 };
 
 struct LocalAllocOpConversion
@@ -193,8 +189,7 @@ private:
 void mlir::triton::populateMemoryOpToLLVMPatterns(
     LLVMTypeConverter &typeConverter, const TargetInfoBase &targetInfo,
     RewritePatternSet &patterns, PatternBenefit benefit) {
-  patterns.add<GlobalScratchAllocOpConversion>(typeConverter, targetInfo,
-                                               benefit);
+  patterns.add<GlobalScratchAllocOpConversion>(typeConverter, benefit);
   patterns.add<LocalAllocOpConversion>(typeConverter, targetInfo, benefit);
   patterns.add<LocalDeallocOpConversion>(typeConverter, benefit);
   patterns.add<LocalLoadOpConversion>(typeConverter, targetInfo, benefit);
--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.cpp
@@ -627,11 +627,4 @@ Value TargetInfo::getScratchOnSharedMemoryPtr(
   return LLVM::getStackPointer(rewriter, funcOp);
 }
 
-Value TargetInfo::getScratchOnGlobalMemoryPtr(Location loc,
-                                              RewriterBase &rewriter,
-                                              FunctionOpInterface funcOp,
-                                              Value allocOffset) const {
-  return LLVM::getGlobalScratchPtr(loc, rewriter, funcOp, allocOffset);
-}
-
 } // namespace mlir::triton::NVIDIA
