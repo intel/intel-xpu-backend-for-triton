43d846f8b54a4045647bcb412580bebec43c0aa3
Anatoly Myachev <anatoly.myachev@intel.com>
2025-02-17 15:27:29 +0100

    update clean procedure
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/test_pytorch_clean_cache.py
+++ python/test/unit/test_pytorch_clean_cache.py
@@ -1,6 +1,7 @@
 import torch
 from torch._inductor.utils import fresh_inductor_cache
 import os
+import sys
 
 
 def test_case():
@@ -18,6 +19,16 @@ def test_case():
 with fresh_inductor_cache():
     test_case()
     triton_cache_dir = os.environ["TRITON_CACHE_DIR"]
+
+    for m_name in list(sys.modules.keys()):
+        if m_name.startswith("torch._inductor.runtime.compile_tasks."):
+            m = sys.modules[m_name]
+            for attr_name in m.__dict__.keys():
+                if (attr_name.startswith("triton_poi")):
+                    kernel = getattr(m, attr_name)
+                    del kernel.compile_results[0].kernel
+                    del kernel.launchers
+            del sys.modules[m_name]
 
 print("triton cache dir:", triton_cache_dir)
 assert not os.path.exists(triton_cache_dir), f"{os.listdir(triton_cache_dir)}"
