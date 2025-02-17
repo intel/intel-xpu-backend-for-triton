2bf25b6aadf1e5086e82bf0ef17237e6bd20e77d
Anatoly Myachev <anatoly.myachev@intel.com>
2025-02-17 17:02:23 +0100

    update test
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/test_pytorch_clean_cache.py
+++ python/test/unit/test_pytorch_clean_cache.py
@@ -5,31 +5,29 @@ import sys
 
 
 def test_case():
+    with fresh_inductor_cache():
 
-    @torch.compile
-    def my_add(a, b):
-        return a + b
+        @torch.compile
+        def my_add(a, b):
+            return a + b
 
-    device = "xpu"
-    a = torch.rand(2, 3, device=device)
-    b = torch.rand(2, 3, device=device)
-    _ = my_add(a, b)
+        device = "xpu"
+        a = torch.rand(2, 3, device=device)
+        b = torch.rand(2, 3, device=device)
+        _ = my_add(a, b)
 
+        triton_cache_dir = os.environ["TRITON_CACHE_DIR"]
 
-with fresh_inductor_cache():
-    test_case()
-    triton_cache_dir = os.environ["TRITON_CACHE_DIR"]
+        for m_name in list(sys.modules.keys()):
+            if m_name.startswith("torch._inductor.runtime.compile_tasks."):
+                m = sys.modules[m_name]
+                for attr_name in m.__dict__.keys():
+                    if (attr_name.startswith("triton_poi")):
+                        kernel = getattr(m, attr_name)
+                        print(f"{kernel.compile_results[0]=}")
+                        del kernel.compile_results[0].kernel
+                        del kernel.launchers
+                del sys.modules[m_name]
 
-    for m_name in list(sys.modules.keys()):
-        if m_name.startswith("torch._inductor.runtime.compile_tasks."):
-            m = sys.modules[m_name]
-            for attr_name in m.__dict__.keys():
-                if (attr_name.startswith("triton_poi")):
-                    kernel = getattr(m, attr_name)
-                    print(f"{kernel.compile_results[0]=}")
-                    del kernel.compile_results[0].kernel
-                    del kernel.launchers
-            del sys.modules[m_name]
-
-print("triton cache dir:", triton_cache_dir)
-assert not os.path.exists(triton_cache_dir), f"{os.listdir(triton_cache_dir)}"
+    print("triton cache dir:", triton_cache_dir)
+    assert not os.path.exists(triton_cache_dir), f"{os.listdir(triton_cache_dir)}"
