4c301e149580f9a8e43cdf88b0e79fe2f589d266
Wang, Quintin <quintin.wang@intel.com>
2025-02-20 02:02:20 +0000

    support loop fusion

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -283,6 +283,9 @@ class XPUBackend(BaseBackend):
             intel.passes.ttgpuir.add_rewrite_tensor_pointer(pm)
         intel.passes.ttgpuir.add_pipeline(pm, opt.num_stages, False)
 
+        passes.ttgpuir.add_fuse_nested_loops(pm)
+        passes.common.add_canonicalizer(pm)
+        passes.common.add_licm(pm)
         passes.ttgpuir.add_optimize_thread_locality(pm)
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
         passes.common.add_cse(pm)
