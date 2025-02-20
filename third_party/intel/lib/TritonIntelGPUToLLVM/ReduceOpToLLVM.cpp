072a470be2c7af0abed53f77037f83b0f08a32d1
Whitney Tsang <whitney.tsang@intel.com>
2025-02-20 19:40:15 +0000

    [Intel] Fix build failure from `e1e287a`
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/lib/TritonIntelGPUToLLVM/ReduceOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/ReduceOpToLLVM.cpp
@@ -28,7 +28,7 @@ public:
   matchAndRewrite(triton::ReduceOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     ReduceOpHelper helper(op);
-    assert(helper.isSupportedLayout() &&
+    assert(helper.isReduceWithinCTA() &&
            "Unexpected srcLayout in ReduceOpConversion");
     Location loc = op->getLoc();
 
