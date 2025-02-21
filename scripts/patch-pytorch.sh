3bd78f9c423ed556bbe5042d916e66711b37f7a3
Anatoly Myachev <anatoly.myachev@intel.com>
2025-02-21 18:20:18 +0100

    update pin && remove patches
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -18,5 +18,3 @@ cd "$REPO_ROOT"
 
 # curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
 git apply "${SCRIPT_DIR}/pytorch_fp64.patch"
-
-git apply "${SCRIPT_DIR}/pytorch.patch"
