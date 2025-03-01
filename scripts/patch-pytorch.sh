d4abe5778121bc2f26a6ffee62cc1b4759811f12
Anatoly Myachev <anatoly.myachev@intel.com>
2025-03-01 16:59:39 +0100

    [PyTorch][Debug] Try fix for 'test/inductor/test_aot_inductor.py'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -18,3 +18,5 @@ cd "$REPO_ROOT"
 
 # curl -sSL https://github.com/pytorch/pytorch/pull/126516.diff | git apply -
 git apply "${SCRIPT_DIR}/pytorch_fp64.patch"
+# trigger build 3
+git apply "${SCRIPT_DIR}/pytorch.patch"
