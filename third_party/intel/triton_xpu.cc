a50144905386d86bec878d25f4cd75a323b4cf25
Whitney Tsang <whitney.tsang@intel.com>
2025-03-19 09:02:45 +0000

    Fix build failures from LLVM commit llvm/llvm-project@2619c2ed58
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/triton_xpu.cc
+++ third_party/intel/triton_xpu.cc
@@ -272,7 +272,7 @@ void init_triton_intel(py::module &&m) {
     std::string triple = "spir64-unknown-unknown";
     std::string layout = "e-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:"
                          "256-v256:256-v512:512-v1024:1024-n8:16:32:64";
-    mod->setTargetTriple(triple);
+    mod->setTargetTriple(llvm::Triple(triple));
     mod->setDataLayout(layout);
   });
 
