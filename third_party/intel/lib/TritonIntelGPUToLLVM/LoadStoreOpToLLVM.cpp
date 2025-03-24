699e547d4fa85e72bcfde00a3fe9a7c90ec36236
Alex Baden <alexander.baden@intel.com>
2025-03-24 19:53:06 +0000

    better inner B dim indexing

--- third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -1267,10 +1267,6 @@ struct LoadOpConversion
 
     const unsigned dpasTileToPackedIndicesRatio =
         elemsPerDPASInst[0] / packedElemsPerLanePerDPASInst;
-    llvm::errs() << "ratio of dpas elems to packed elems per lane: "
-                 << dpasTileToPackedIndicesRatio << "\n";
-    llvm::errs() << "usePackedType = " << usePackedType << "\n";
-    llvm::errs() << "opsPerChannel = " << opsPerChannel << "\n";
 
     // Create the linear layout for the load.
     // First, we create a tile layout corresponding to a single invocation of
@@ -1308,8 +1304,6 @@ struct LoadOpConversion
             (!isTransposeRequired && dim == 0)
                 ? tileShape[dim] / dpasTileToPackedIndicesRatio
                 : tileShape[dim];
-        llvm::errs() << dim << " offsetDimSize: " << offsetDimSize
-                     << " vs tileShape: " << tileShape[dim] << "\n";
 #else
         const unsigned offsetDimSize = tileShape[dim];
 #endif
@@ -1615,13 +1609,11 @@ struct LoadOpConversion
           if (isTransposeRequired)
             outerDimBStride /= dpasTileToPackedIndicesRatio;
 #endif
-#if 1
+#if 0
           const unsigned innerDimBStride =
               isTransposeRequired ? dpasTileToPackedIndicesRatio : 1;
 #else
-          const unsigned innerDimBStride =
-              repKStride /
-              (packedElemsPerLanePerDPASInst * numOperandsInnerDimPerLoad);
+          const unsigned innerDimBStride = tileHeight / (repKStride * vBlocks);
 #endif
           layoutOffsetX *= (isOperandA ? numRepOuter : outerDimBStride);
           layoutOffsetY *= (isOperandA ? outerDimWarpNum : innerDimBStride);
