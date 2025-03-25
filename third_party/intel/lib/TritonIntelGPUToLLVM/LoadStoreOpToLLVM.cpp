b249765a8556022041b9050ab72caedc77dea0b9
Alex Baden <alexander.baden@intel.com>
2025-03-25 00:43:58 +0000

    fixup inner dim indexing

--- third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -1609,9 +1609,14 @@ struct LoadOpConversion
           if (isTransposeRequired)
             outerDimBStride /= dpasTileToPackedIndicesRatio;
 #endif
-#if 0
+#if 1
           const unsigned innerDimBStride =
-              isTransposeRequired ? dpasTileToPackedIndicesRatio : 1;
+              isTransposeRequired
+                  ? (elemsPerDPASInst[dimInner] * dpasTileToPackedIndicesRatio *
+                     numOperandsInnerDimPerLoad) /
+                        repKStride
+                  : (elemsPerDPASInst[dimInner] * numOperandsInnerDimPerLoad) /
+                        (repKStride * vBlocks);
 #else
           const unsigned innerDimBStride = tileHeight / (repKStride * vBlocks);
 #endif
