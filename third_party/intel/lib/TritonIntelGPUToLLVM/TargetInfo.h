43153b0e7cf6a85db013647cc2be44ec5549fc00
Lu,Chengjun <chengjun.lu@intel.com>
2025-04-03 13:51:12 +0000

    Add horizontal batched reduce.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/lib/TritonIntelGPUToLLVM/TargetInfo.h
+++ third_party/intel/lib/TritonIntelGPUToLLVM/TargetInfo.h
@@ -51,6 +51,11 @@ public:
   Value programId(RewriterBase &rewriter, Location loc, ModuleOp moduleOp,
                   int axis) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       std::map<SmallVector<unsigned>, SmallVector<Value>> &acc,
+                       triton::ReduceOp op, unsigned numLaneToReduce,
+                       unsigned interleave) const override;
+
   bool warpReduce(RewriterBase &rewriter, Location loc, SmallVector<Value> &acc,
                   triton::ReduceOp op, unsigned numLaneToReduce,
                   unsigned interleave) const override;
@@ -93,5 +98,6 @@ private:
 };
 
 std::unique_ptr<TargetInfo> createTargetInfo(ModuleOp mod);
+
 } // namespace mlir::triton::intel
 #endif // TRITON_CONVERSION_TRITONGPU_TO_LLVM_TARGETINFOINTEL_H
