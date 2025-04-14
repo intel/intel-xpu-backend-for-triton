bf7bcd339b0cf199a7394e8b372eb3cb74a03cfa
Vadim Musin <vadim.musin@intel.com>
2025-04-14 20:38:31 +0200

    Set process name to test name for easier debugging

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -1,7 +1,23 @@
 # content of conftest.py
 import os
 import pytest
+import setproctitle
 
+def pytest_runtest_protocol(item, nextitem):
+    """
+    Hook to set the process title to the name of the currently running test.
+    """
+    # Set the process title to the current test name
+    test_name = item.nodeid
+    setproctitle.setproctitle(f"pytest: {test_name}")
+
+    # Run the test
+    outcome = yield
+
+    # Reset the process title after the test is done
+    setproctitle.setproctitle("pytest: idle")
+
+    return outcome
 
 def pytest_configure(config):
     if os.getenv('TEST_UNSKIP') == 'true':
