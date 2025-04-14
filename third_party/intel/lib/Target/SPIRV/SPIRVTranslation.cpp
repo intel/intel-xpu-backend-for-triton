12c439bf2b0f3bd7ebc38f6ee77dbd61782918ef
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2025-04-14 15:30:27 +0100

    Fix merge bug.

--- third_party/intel/lib/Target/SPIRV/SPIRVTranslation.cpp
+++ third_party/intel/lib/Target/SPIRV/SPIRVTranslation.cpp
@@ -107,7 +107,7 @@ public:
 
 static SPIRV::TranslatorOpts getSPIRVOopts() {
   SPIRV::TranslatorOpts SPIRVOpts;
-  static constexpr std::array<SPIRV::ExtensionID, 13> AllowedExtensions{
+  static constexpr std::array<SPIRV::ExtensionID, 14> AllowedExtensions{
       SPIRV::ExtensionID::SPV_EXT_shader_atomic_float_add,
       SPIRV::ExtensionID::SPV_INTEL_arbitrary_precision_integers,
       SPIRV::ExtensionID::SPV_INTEL_arithmetic_fence,
