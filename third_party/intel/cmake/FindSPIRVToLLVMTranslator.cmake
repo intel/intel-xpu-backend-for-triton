169f9092c9add49eb172854cbdd2c5d5e68cdf59
Pavel Chekin <pavel.chekin@intel.com>
2025-05-02 09:24:15 -0700

    Apply local patches instead of downloading from GitHub
    Signed-off-by: Pavel Chekin <pavel.chekin@intel.com>

--- third_party/intel/cmake/FindSPIRVToLLVMTranslator.cmake
+++ third_party/intel/cmake/FindSPIRVToLLVMTranslator.cmake
@@ -24,15 +24,7 @@ if (NOT SPIRVToLLVMTranslator_FOUND)
 
             # FIXME: Don't apply patch when Agama driver is updated.
             execute_process(
-                COMMAND curl -sSL https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/3122.diff
-                OUTPUT_FILE ${CMAKE_BINARY_DIR}/3122.diff
-                RESULT_VARIABLE CURL_RESULT
-            )
-            if(NOT CURL_RESULT EQUAL 0)
-                message(FATAL_ERROR "Failed to download patch from https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/3122.diff")
-            endif()
-            execute_process(
-                COMMAND git apply ${CMAKE_BINARY_DIR}/3122.diff
+                COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/3122.diff
                 WORKING_DIRECTORY ${spirv-llvm-translator_SOURCE_DIR}
                 RESULT_VARIABLE PATCH_RESULT
             )
@@ -42,15 +34,7 @@ if (NOT SPIRVToLLVMTranslator_FOUND)
 
             # FIXME: Don't apply patch when Agama LTS driver is updated or https://github.com/KhronosGroup/SPIRV-LLVM-Translator/issues/3137 is addressed.
             execute_process(
-                COMMAND curl -sSL https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/3138.diff
-                OUTPUT_FILE ${CMAKE_BINARY_DIR}/3138.diff
-                RESULT_VARIABLE CURL_RESULT
-            )
-            if(NOT CURL_RESULT EQUAL 0)
-                message(FATAL_ERROR "Failed to download patch from https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/3138.diff")
-            endif()
-            execute_process(
-                COMMAND git apply ${CMAKE_BINARY_DIR}/3138.diff
+                COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/3138.diff
                 WORKING_DIRECTORY ${spirv-llvm-translator_SOURCE_DIR}
                 RESULT_VARIABLE PATCH_RESULT
             )
