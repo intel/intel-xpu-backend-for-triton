ab8146c5ae05b3d7741357b5a5a8758e404da3ca
Pavel Chekin <pavel.chekin@intel.com>
2025-05-09 08:41:28 -0700

    Move PyTorch pin forward, apply Kineto patch
    Signed-off-by: Pavel Chekin <pavel.chekin@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -36,3 +36,4 @@ echo "Applying PyTorch patches in $REPO_ROOT"
 
 # put your patch applies here
 apply_patch https://github.com/pytorch/pytorch/pull/143553.diff
+apply_patch https://github.com/pytorch/pytorch/pull/152007.diff
