55c3e53cdc4ed97564ad4d8528fd044bf37b2f53
Whitney Tsang <whitney.tsang@intel.com>
2025-05-12 18:59:19 +0000

    Remove SPIRV translator version workaround
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- third_party/intel/cmake/FindSPIRVToLLVMTranslator.cmake
+++ third_party/intel/cmake/FindSPIRVToLLVMTranslator.cmake
@@ -32,16 +32,6 @@ if (NOT SPIRVToLLVMTranslator_FOUND)
                 message(FATAL_ERROR "Failed to apply 3122.patch to SPIRV-LLVM-Translator")
             endif()
 
-            # FIXME: Don't apply patch when Agama LTS driver is updated or https://github.com/KhronosGroup/SPIRV-LLVM-Translator/issues/3137 is addressed.
-            execute_process(
-                COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/3138.patch
-                WORKING_DIRECTORY ${spirv-llvm-translator_SOURCE_DIR}
-                RESULT_VARIABLE PATCH_RESULT
-            )
-            if(NOT PATCH_RESULT EQUAL 0)
-                message(FATAL_ERROR "Failed to apply 3138.patch to SPIRV-LLVM-Translator")
-            endif()
-
             set(LLVM_CONFIG ${LLVM_LIBRARY_DIR}/../bin/llvm-config)
             set(LLVM_DIR "${LLVM_LIBRARY_DIR}/cmake/llvm" CACHE PATH "Path to LLVM build dir " FORCE)
             set(LLVM_SPIRV_BUILD_EXTERNAL YES CACHE BOOL "Build SPIRV-LLVM Translator as external" FORCE)
