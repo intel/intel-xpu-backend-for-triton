2dac051512c7a1b8c5bb6731c9abc2a1bb604279
Alex Baden <alexander.baden@intel.com>
2025-05-13 14:11:51 +0000

    Introduce Subgroup 2D Block Encoding Layout 8/?
    support packed layouts with width > threads per warp

--- unittest/Dialect/TritonIntelGPU/LinearLayoutConversionsTest.cpp
+++ unittest/Dialect/TritonIntelGPU/LinearLayoutConversionsTest.cpp
@@ -19,6 +19,7 @@ class LinearLayoutConversionsTest : public ::testing::Test {
 public:
   void SetUp() { ctx.loadDialect<TritonGPUDialect, TritonIntelGPUDialect>(); }
 
+  // Create a Subgroup2DBlockEncoding layout based on a DPAS layout
   Subgroup2DBlockEncodingAttr sdb(ArrayRef<unsigned> instrShape,
                                   unsigned numBlocks, unsigned kWidth,
                                   ArrayRef<unsigned> warpsPerCTA,
@@ -29,25 +30,24 @@ public:
         /*opsPerChan=*/2, warpsPerCTA, /*repCluster=*/{4, 2},
         /*threadsPerWarp=*/16);
 
-    auto dpasReps = dpasLayout.getDPASRepetitions(blockShape, opIdx);
-    llvm::errs() << "dpas reps size = " << dpasReps.size() << "\n";
-    assert(dpasReps.size() == 3);
-    llvm::errs() << "dpas reps: ";
-    for (size_t i = 0; i < dpasReps.size(); i++) {
-      llvm::errs() << dpasReps[i] << " ";
-    }
-    llvm::errs() << "\n";
-    auto dpasRepsUnsigned = SmallVector<unsigned>{
-        static_cast<unsigned>(dpasReps[1]), static_cast<unsigned>(dpasReps[2])};
+    auto dpasRepsRef =
+        llvm::to_vector(dpasLayout.getDPASRepetitions(blockShape, opIdx));
+    assert(dpasRepsRef.size() == 3);
+    auto dpasReps =
+        SmallVector<unsigned>{static_cast<unsigned>(dpasRepsRef[1]),
+                              static_cast<unsigned>(dpasRepsRef[2])};
+
     // TODO: could put the getOrderForDotOperand in the builder?
-    return Subgroup2DBlockEncodingAttr::get(
+    auto layout = Subgroup2DBlockEncodingAttr::get(
         &ctx, warpsPerCTA,
-        CTALayoutAttr::get(&ctx, dpasLayout.getCTAsPerCGA(),
-                           dpasLayout.getCTASplitNum(),
-                           dpasLayout.getCTAOrder()),
-        instrShape, numBlocks, dpasRepsUnsigned,
+        CTALayoutAttr::get(
+            &ctx, dpasLayout.getCTAsPerCGA(), // TODO: add to DpasLayout?
+            dpasLayout.getCTASplitNum(), dpasLayout.getCTAOrder()),
+        instrShape, numBlocks, dpasReps,
         getOrderForDotOperand(opIdx, /*rank*/ 2, /*kContig*/ true), kWidth,
         dpasLayout.getThreadsPerWarp());
+    llvm::errs() << "sdb layout: " << layout << "\n";
+    return layout;
   }
 
   StringAttr S(StringRef str) { return StringAttr::get(&ctx, str); }
@@ -56,25 +56,17 @@ protected:
   MLIRContext ctx;
 };
 
-// TODO: is this valid, and how should this layout be created?
-TEST_F(LinearLayoutConversionsTest, DISABLED_FP16_32x32x1_M256_N32_K32_A) {
-
-  // Layout for A operand, warpsPerCTA is (8, 4). We have one tile per warp.
-  // The load should be 32 by 16 with 2 blocks --> 32 by 32
-  // There is one load per warp.
-
-  auto layout = subgroup2DBlockToLinearLayout(
-      /*blockShape*/ {256, 32},
-      sdb(/*instrShape*/ {32, 32}, /*numBlocks*/ 1, /*kWidth*/ 2,
-          /*warpsPerCTA*/ {8, 4},
-          /*blockShape*/ {256, 32}, /*opIdx*/ 0),
-      /*kWidth*/ 2);
-  llvm::errs() << "layout from conversion: " << layout << "\n";
+TEST_F(LinearLayoutConversionsTest, FP16_32x32x1_M256_N32_K32_A) {
   EXPECT_EQ(
-      layout,
+      subgroup2DBlockToLinearLayout(
+          /*blockShape*/ {256, 32},
+          sdb(/*instrShape*/ {32, 32}, /*numBlocks*/ 1, /*kWidth*/ 2,
+              /*warpsPerCTA*/ {8, 4},
+              /*blockShape*/ {256, 32}, /*opIdx*/ 0),
+          /*kWidth*/ 2),
       LinearLayout(
-          {{S("register"), {{1, 0}, {2, 0}, {4, 0}, {8, 0}, {16, 0}, {0, 16}}},
-           {S("lane"), {{0, 1}, {0, 2}, {0, 4}, {0, 8}}},
+          {{S("register"), {{0, 1}, {1, 0}, {2, 0}, {4, 0}, {8, 0}, {16, 0}}},
+           {S("lane"), {{0, 2}, {0, 4}, {0, 8}, {0, 16}}},
            {S("warp"), {{0, 0}, {0, 0}, {32, 0}, {64, 0}, {128, 0}}},
            {S("block"), {}}},
           {S("dim0"), S("dim1")}));
@@ -84,17 +76,14 @@ TEST_F(LinearLayoutConversionsTest, FP16_32x16x2_M256_N32_K32_A) {
   // Layout for A operand, warpsPerCTA is (8, 4). We have one tile per warp.
   // The load should be 32 by 16 with 2 blocks --> 32 by 32
   // There is one load per warp.
-  auto sdbEncoding = sdb(/*instrShape*/ {32, 16}, /*numBlocks*/ 2, /*kWidth*/ 2,
-                         /*warpsPerCTA*/ {8, 4},
-                         /*blockShape*/ {256, 32}, /*opIdx*/ 0);
-  llvm::errs() << "sdp: " << sdbEncoding << "\n";
 
-  auto layout = subgroup2DBlockToLinearLayout(
-      /*blockShape*/ {256, 32}, sdbEncoding,
-      /*kWidth*/ 2);
-  llvm::errs() << "layout from conversion: " << layout << "\n";
   EXPECT_EQ(
-      layout,
+      subgroup2DBlockToLinearLayout(
+          /*blockShape*/ {256, 32},
+          sdb(/*instrShape*/ {32, 16}, /*numBlocks*/ 2, /*kWidth*/ 2,
+              /*warpsPerCTA*/ {8, 4},
+              /*blockShape*/ {256, 32}, /*opIdx*/ 0),
+          /*kWidth*/ 2),
       LinearLayout(
           {{S("register"), {{1, 0}, {2, 0}, {4, 0}, {8, 0}, {16, 0}, {0, 16}}},
            {S("lane"), {{0, 1}, {0, 2}, {0, 4}, {0, 8}}},
@@ -108,13 +97,11 @@ TEST_F(LinearLayoutConversionsTest, FP16_32x16x2_M256_N32_K32_B) {
   // The load should be 32 by 16 with 2 blocks --> 32 by 32
   // There are two loads per warp.
 
-  auto sdbEncoding = sdb(/*instrShape*/ {32, 16}, /*numBlocks*/ 2, /*kWidth*/ 2,
-                         /*warpsPerCTA*/ {8, 4}, /*blockShape*/ {32, 256},
-                         /*opIdx*/ 1);
-  llvm::errs() << "sdp: " << sdbEncoding << "\n";
-
   auto layout = subgroup2DBlockToLinearLayout(
-      /*shape*/ {32, 256}, sdbEncoding,
+      /*shape*/ {32, 256},
+      sdb(/*instrShape*/ {32, 16}, /*numBlocks*/ 2, /*kWidth*/ 2,
+          /*warpsPerCTA*/ {8, 4}, /*blockShape*/ {32, 256},
+          /*opIdx*/ 1),
       /*kWidth*/ 2);
   llvm::errs() << "layout from conversion: " << layout << "\n";
   EXPECT_EQ(layout,
