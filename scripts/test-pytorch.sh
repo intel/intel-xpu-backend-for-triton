86585748e4568fb566e7e04727cbce7ddbf95d0d
Anatoly Myachev <anatoly.myachev@intel.com>
2025-05-16 23:17:56 +0200

    reduce scope
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/test-pytorch.sh
+++ scripts/test-pytorch.sh
@@ -7,15 +7,6 @@ pip install -r .ci/docker/requirements-ci.txt
 
 export
 
-test_cmd="python test/run_test.py --keep-going --include "
-if [[ "${1:-all}" = "all" ]]; then
-    for test in $(ls test/inductor | grep test); do
-        test_cmd="${test_cmd} inductor/$test"
-    done
-else
-    for test in "$@"; do
-        test_cmd="${test_cmd} $test"
-    done
-fi
+test_cmd="python test/run_test.py --keep-going --include inductor/test_select_algorithm.py -k test_convolution1"
 
 eval $test_cmd
