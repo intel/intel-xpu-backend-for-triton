7841ea5ce114acba6da2d906c571a73ee9bf957b
Ling, Liyang <liyang.ling@intel.com>
2025-05-22 08:21:32 +0000

    Update extended attention interface
    Address review comments
    
    Fix CI XPU not found

--- benchmarks/third_party/sglang/prefill_attention_benchmark.py
+++ benchmarks/third_party/sglang/prefill_attention_benchmark.py
@@ -6,8 +6,8 @@ import triton_kernels_benchmark as benchmark_suit
 
 
 def gen_args(B, SEQ_LENS, H_Q, H_KV, D, dtype, device):
-    max_seq_len = max(SEQ_LENS)
-    N_CTX = sum(SEQ_LENS)
+    max_seq_len = SEQ_LENS
+    N_CTX = SEQ_LENS
 
     # Create random input tensors
     q = torch.randn((B * N_CTX, H_Q, D), device=device, dtype=dtype)
@@ -16,8 +16,8 @@ def gen_args(B, SEQ_LENS, H_Q, H_KV, D, dtype, device):
     o = torch.zeros((B * N_CTX, H_Q, D), device=device, dtype=dtype)
 
     # Create b_start_loc and b_seq_len tensors
-    b_start_loc = torch.tensor([0, SEQ_LENS[0]], device=device)
-    b_seq_len = torch.tensor(SEQ_LENS, device=device)
+    b_start_loc = torch.tensor([0, SEQ_LENS], device=device)
+    b_seq_len = torch.tensor([SEQ_LENS], device=device)
 
     return (q, k, v, o, b_start_loc, b_seq_len, max_seq_len)
 
@@ -26,13 +26,13 @@ def gen_args(B, SEQ_LENS, H_Q, H_KV, D, dtype, device):
 @benchmark_suit.perf_report(
     benchmark_suit.Benchmark(
         # argument names to use as an x-axis for the plot
-        x_names=['B', 'SEQ_LENS', 'H_Q', 'H_KV', 'D', 'CAUSAL', 'MODE', 'VALIDATE'],
+        x_names=['B', 'SEQ_LENS', 'H_Q', 'H_KV', 'D', 'CAUSAL', 'MODE'],
         x_vals=[  #
-            [bs, [1024], 32, 8, 128, causal, 'fwd', False] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
+            [bs, 1024, 32, 8, 128, causal, 'fwd'] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
         ] + [  #
-            [bs, [1024], 32, 32, 96, causal, 'fwd', False] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
+            [bs, 1024, 32, 32, 96, causal, 'fwd'] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
         ] + [  #
-            [bs, [1024], 28, 4, 128, causal, 'fwd', False] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
+            [bs, 1024, 28, 4, 128, causal, 'fwd'] for causal in [True, False] for bs in [1, 16, 32, 64, 128]
         ],
         line_arg='provider',
         # argument name whose value corresponds to a different line in the plot
@@ -51,43 +51,21 @@ def gen_args(B, SEQ_LENS, H_Q, H_KV, D, dtype, device):
         # name for the plot. Used also as a file name for saving the plot.
         args={},
     ))
-def benchmark(B, SEQ_LENS, H_Q, H_KV, D, CAUSAL, MODE, VALIDATE, provider):
+def benchmark(B, SEQ_LENS, H_Q, H_KV, D, CAUSAL, MODE, provider):
     torch.manual_seed(0)
     dtype = torch.bfloat16
-    N_CTX = sum(SEQ_LENS)
 
     q, k, v, o, b_start_loc, b_seq_len, max_seq_len = gen_args(B, SEQ_LENS, H_Q, H_KV, D, dtype, 'xpu')
 
     quantiles = [0.5, 0.0, 1.0]
-    if provider == 'triton':
-
+    if provider == 'triton' and MODE == 'fwd':
         triton_fn = lambda: context_attention_fwd(q, k, v, o, b_start_loc, b_seq_len, max_seq_len, is_causal=CAUSAL)
-
-        if VALIDATE:
-            # FIXME: torch sdpa does not support different H_Q and H_KV
-            cu_seq_lens = [0] * (len(SEQ_LENS) + 1)
-            for i, seq_len in enumerate(SEQ_LENS):
-                cu_seq_lens[i + 1] = cu_seq_lens[i] + seq_len
-
-            for i in range(len(SEQ_LENS)):
-                start, end = cu_seq_lens[i], cu_seq_lens[i + 1]
-                o_torch = torch.nn.functional.scaled_dot_product_attention(
-                    q[start:end].permute(1, 0, 2),
-                    k[start:end].permute(1, 0, 2),
-                    v[start:end].permute(1, 0, 2),
-                    is_causal=CAUSAL,
-                ).permute(1, 0, 2)
-
-                cos_sim = torch.nn.functional.cosine_similarity(o[start:end].flatten(), o_torch.flatten(), dim=0)
-                assert cos_sim.item() > 1 - (1e-5)
-                assert torch.allclose(o[start:end], o_torch, atol=1e-2)
-
         _, min_ms, max_ms, mean_ms, cv = benchmark_suit.do_bench(triton_fn, n_warmup=10, n_repeat=10,
                                                                  quantiles=quantiles)
-
     else:
-        raise NotImplementedError(f'Unsupported provider {provider}')
+        raise NotImplementedError(f'Unsupported provider {provider} and mode {MODE}')
 
+    N_CTX = SEQ_LENS
     tflops = lambda ms: 2 * B * (H_Q + H_KV) * N_CTX * N_CTX * D * (1e-12) / (ms * 1e-3)
     gbps = lambda ms: 2 * B * (H_Q + H_KV) * N_CTX * D * 2 * (1e-9) / (ms * 1e-3)
 
