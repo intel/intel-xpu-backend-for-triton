1108d68a0f6426ba939e619c2a99529e9f4cf2d3
Anatoly Myachev <anatoly.myachev@intel.com>
2025-05-30 18:51:58 +0200

    Try run FlexAttn in parallel
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/test-pytorch.sh
+++ scripts/test-pytorch.sh
@@ -7,15 +7,4 @@ pip install -r .ci/docker/requirements-ci.txt
 
 export
 
-test_cmd="python test/run_test.py --keep-going --include "
-if [[ "${1:-all}" = "all" ]]; then
-    for test in $(ls test/inductor | grep test); do
-        test_cmd="${test_cmd} inductor/$test"
-    done
-else
-    for test in "$@"; do
-        test_cmd="${test_cmd} $test"
-    done
-fi
-
-eval $test_cmd
+pytest test/inductor/test_flex_attention.py -n 16
