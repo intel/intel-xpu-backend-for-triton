59e078dc09a54e9e8cbadd4dcd6ed49d8c6a22b2
Anatoly Myachev <anatoliimyachev@mail.com>
2025-06-03 13:51:30 +0200

    Update test-pytorch.sh

--- scripts/test-pytorch.sh
+++ scripts/test-pytorch.sh
@@ -8,7 +8,7 @@ pip install -r .ci/docker/requirements-ci.txt
 export
 
 # warmup cache
-pytest test/inductor/test_flex_attention.py -n 32 || true
+pytest test/inductor/test_flex_attention.py -n 16 || true
 echo PYTEST_FINISHED
 
 python test/run_test.py --keep-going --include inductor/test_flex_attention.py
