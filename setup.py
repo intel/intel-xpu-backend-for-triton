cf6500be2b960890470cb2a6df344d9f54b17d3e
Pavel Chekin <pavel.chekin@intel.com>
2025-06-05 09:02:55 -0700

    Install filecheck>=1.0.0 for python_version >= 3.10
    Signed-off-by: Pavel Chekin <pavel.chekin@intel.com>

--- setup.py
+++ setup.py
@@ -804,7 +804,7 @@ setup(
     description="A language and compiler for custom Deep Learning operations",
     long_description="",
     install_requires=[
-        "filecheck>=1.0.0",
+        "filecheck>=1.0.0; python_version >= '3.10'",
         "setuptools>=78.1.0",
         "importlib-metadata; python_version < '3.10'",
     ],
