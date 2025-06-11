c973c16d83367f44f8fed6a27984d1321ebc7360
Lu,Chengjun <chengjun.lu@intel.com>
2025-06-11 12:20:30 +0000

    Add knob REDUCE_VARIABLE_LIVENESS
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -66,6 +66,7 @@ class XPUOptions:
             raise AssertionError("num_warps must be a power of 2")
         self.generate_native_code = (knobs.intel.gen_native_code
                                      or knobs.intel.dump_shader_info) or self.generate_native_code
+        self.reduce_variable_liveness = knobs.intel.reduce_variable_liveness or self.reduce_variable_liveness
 
     def hash(self):
         key = '_'.join([f'{name}-{val}' for name, val in self.__dict__.items()])
