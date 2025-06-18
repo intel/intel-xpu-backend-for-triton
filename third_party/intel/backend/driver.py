fa4beff396e1d47a17a56e3e640684b7322c75db
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2025-06-18 09:52:59 +0100

    reduce cache size

--- third_party/intel/backend/driver.py
+++ third_party/intel/backend/driver.py
@@ -781,7 +781,7 @@ class XPUDriver(DriverBase):
         # We maintain a buffer of 256 MB that we clear
         # before each kernel call to make sure that the L2 cache
         # doesn't contain any input data before the run
-        cache_size = 256 * 1024 * 1024
+        cache_size = 18 * 1024 * 1024 #256 * 1024 * 1024
         return torch.empty(int(cache_size // 4), dtype=torch.int, device='xpu')
 
     def clear_cache(self, cache):
