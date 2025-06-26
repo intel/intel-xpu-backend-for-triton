f1a3ebbd79514ce2a807458e50de717be64e3262
Lu,Chengjun <chengjun.lu@intel.com>
2025-06-26 10:29:59 +0000

    Fix casting issue on regular pointer type.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/lib/TritonIntelGPUTransforms/OptimizeDotOperands.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/OptimizeDotOperands.cpp
@@ -147,9 +147,10 @@ private:
     if (!loadOp->hasOneUse() || !loadOpHasBlockIOAttr)
       return false;
 
-    auto ptrType = cast<tt::PointerType>(loadOp.getPtr().getType());
-    if (!isTensorPointerType(ptrType) ||
-        cast<RankedTensorType>(ptrType.getPointeeType()).getRank() != 2)
+    Type ptrType = loadOp.getPtr().getType();
+    if (!triton::isTensorPointerType(ptrType) ||
+        cast<RankedTensorType>(cast<tt::PointerType>(ptrType).getPointeeType())
+                .getRank() != 2)
       return false;
 
     std::optional<tt::MakeTensorPtrOp> defOp =
