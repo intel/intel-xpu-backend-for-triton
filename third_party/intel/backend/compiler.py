e8d8054993c99afbe18964cf7e0282cb311c8460
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2025-07-25 16:01:32 +0000

    Implemented compile time/size tracking and profiling utility

--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -1,6 +1,7 @@
 from triton.backends.compiler import BaseBackend, Language
 from triton._C.libtriton import ir, passes, llvm, intel
 from triton.backends.intel.driver import compile_module_from_src
+from triton.backends.intel.track import track
 from triton import knobs
 
 from dataclasses import dataclass
@@ -211,6 +212,7 @@ class XPUBackend(BaseBackend):
         return split_barriers_scope
 
     @staticmethod
+    @track
     def make_ttir(mod, metadata, opt):
         pm = ir.pass_manager(mod.context)
         pm.enable_debug()
@@ -230,6 +232,7 @@ class XPUBackend(BaseBackend):
         return mod
 
     @staticmethod
+    @track
     def make_ttgir(mod, metadata, opt, properties):
         cluster_info = intel.ClusterInfo()
         if opt.cluster_dims is not None:
@@ -307,6 +310,7 @@ class XPUBackend(BaseBackend):
         return mod
 
     @staticmethod
+    @track
     def make_llir(src, metadata, options):
         mod = src
         # TritonGPU -> LLVM-IR (MLIR)
@@ -348,7 +352,9 @@ class XPUBackend(BaseBackend):
             paths = [path for (name, path) in options.extern_libs]
             llvm.link_extern_libs(llvm_mod, paths)
 
-        intel.optimize_module(llvm_mod, llvm.OPTIMIZE_O3)
+        with track("optimize_module") as tr:
+            intel.optimize_module(llvm_mod, llvm.OPTIMIZE_O3, tr.callback("passes"))
+
         intel.post_process_llir(llvm_mod)
 
         # Get some metadata
@@ -367,6 +373,7 @@ class XPUBackend(BaseBackend):
         return ret
 
     @staticmethod
+    @track
     def make_spv(src, metadata, options, device_arch):
         spirv, name = intel.translate_to_spirv(src)
         metadata["name"] = name
@@ -394,7 +401,7 @@ class XPUBackend(BaseBackend):
         metadata["generate_native_code"] = options.generate_native_code
 
         if options.generate_native_code:
-            with tempfile.TemporaryDirectory() as temp_dir:
+            with track("generate_native_code"), tempfile.TemporaryDirectory() as temp_dir:
                 with tempfile.NamedTemporaryFile(mode='wb', suffix='.spv', dir=temp_dir, delete=False) as fsrc:
                     fsrc.write(spirv)
                 fbin = fsrc.name + '.o'
