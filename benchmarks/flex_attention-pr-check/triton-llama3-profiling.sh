59c1fe1587f68e45cb1869b04a081caf4e87f34b
jakub-sochacki <jakub.sochacki@intel.com>
2025-07-29 10:58:41 +0000

    start migration to thirt party

--- benchmarks/flex_attention-pr-check/triton-llama3-profiling.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IGC_VectorizerAllowEXP2=1 IGC_VectorizerAllowMAXNUM=1 IGC_VectorizerAllowWAVEALL=1 IGC_VectorizerAllowCMP=1 IGC_VectorizerAllowSelect=1 TRITON_INTEL_ENABLE_BLOCK_IO_STORE_ON_REGULAR_PTR=1 TRITON_INTEL_ONE_MATRIX_PER_LOAD_BT=1 IGC_DisableCodeScheduling=0 \
-  python run_llm_inductor_greedy.py -m meta-llama/Llama-3.1-8B --max-new-tokens 128 --input-tokens 1024 --num-warmup 2 --num-iter 7 --compile --profile | tee llama31.compile.xpu.profile.log
-
-echo "llama profiling log is stored into $PWD/llama31.compile.xpu.profile.log"
