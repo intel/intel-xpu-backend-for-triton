b35106eaf963c14d13ca08ee1832b743a5af870d
Whitney Tsang <whitney.tsang@intel.com>
2025-07-31 04:01:53 +0000

    Limit shape

--- benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
+++ benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
@@ -27,10 +27,6 @@ DEVICE = triton.runtime.driver.active.get_active_torch_device()
 def get_flex_attn_fwd_configs(*args, **kwargs):  # pylint: disable=unused-argument
     configs = [
         FlexConfig(32, 16, 2, 4),
-        FlexConfig(128, 64, 2, 16),
-        FlexConfig(128, 64, 2, 8),
-        FlexConfig(128, 32, 2, 16),
-        FlexConfig(128, 32, 2, 8),
     ]
     return configs
 
@@ -38,13 +34,6 @@ def get_flex_attn_fwd_configs(*args, **kwargs):  # pylint: disable=unused-argume
 def get_flex_decode_configs(*args, **kwargs):  # pylint: disable=unused-argument
     configs = [
         FlexDecodeConfig(32, 1, 2),
-        FlexDecodeConfig(32, 1, 1),
-        FlexDecodeConfig(32, 2, 2),
-        FlexDecodeConfig(32, 2, 1),
-        FlexDecodeConfig(64, 1, 2),
-        FlexDecodeConfig(64, 1, 1),
-        FlexDecodeConfig(64, 2, 2),
-        FlexDecodeConfig(64, 2, 1),
     ]
     return configs
 
@@ -81,56 +70,10 @@ batch_sizes = [16, 32, 64] if throughput_test else [1]
     benchmark_suit.Benchmark(
         x_names=['Z', 'H_q', 'H_kv', 'N_CTX_q', 'N_CTX_kv', 'D_HEAD_qk', 'D_HEAD_v', 'MODE'],
         x_vals=
-        # Multi-head attention. H_q equals H_kv
-        # Prefill shapes of Phi3-mini-3.8B
-        [[z, 32, 32, 1024, 1024, 96, 96, 'fwd'] for z in batch_sizes] +
-        # Prefill shapes of Deepseek-v3
-        [[z, 128, 128, 1024, 1024, 192, 128, 'fwd'] for z in batch_sizes] +
-        # Append shapes of Phi3-mini-3.8B
-        [[z, 32, 32, 512, 1024 + 128 + 512, 96, 96, 'fwd'] for z in batch_sizes] +
-
-        # Multi-query attention. H_kv equals 1.
-        # Append shapes of Deepseek-v3 (Nope)
-        [[z, 128, 1, 512, 1024 + 128 + 512, 64, 512, 'fwd'] for z in batch_sizes] +
-        # Append shapes of Deepseek-v3 (Rope)
-        [] +
-
-        # Grouped-query attention. H_q / H_kv > 1
-        # Prefill shapes of Llama-3.1-8B
-        [[z, 32, 8, 1024, 1024, 128, 128, 'fwd'] for z in batch_sizes] +
-        # Prefill shapes of Qwen2-7B
-        [[z, 28, 4, 1024, 1024, 128, 128, 'fwd'] for z in batch_sizes] +
-        # Append shapes of Llama-3.1-8B
-        [[z, 32, 8, 512, 1024 + 128 + 512, 128, 128, 'fwd'] for z in batch_sizes] +
-        # Append shapes of Qwen2-7B
-        [[z, 28, 4, 512, 1024 + 128 + 512, 128, 128, 'fwd'] for z in batch_sizes] +
-
-        # FlexDecoding configuration. N_CTX_q equals 1. N_CTX_kv >= 1k
-        # Decode shapes of Llama-3.1-8B
-        [[z, 32, 8, 1, 1024 + 64, 128, 128, 'fwd'] for z in batch_sizes] +
-        # Decode shapes of Phi3-mini-3.8B
-        [
-            # acc = acc.reshape(G, BLOCK_M_PER_HQ, V_HEAD_DIM)
-            # ValueError: Shape element 2 must be a power of 2
-            # [z, 32, 32, 1, 1024 + 64, 96, 96, 'fwd'] for z in batch_sizes
-        ] +
-        # Decode shapes of Qwen2-7B
-        [
-            # torch._inductor.exc.InductorError: LoweringException: ValueError: Number of shared query heads sharing the same KV head must be power of 2.
-            # [z, 28, 4, 1, 1024 + 64, 128, 128, 'fwd'] for z in batch_sizes
-        ] +
-        # Decode shapes of Deepseek-v3 (Nope)
-        [
-            # There is an known issue in IGC for kernel with extreme register pressure.
-            # Enable this case later with new IGC.
-            # RuntimeError: ZE_RESULT_ERROR_INVALID_KERNEL_NAME
-            # [z, 128, 1, 1, 1024, 64, 512, 'fwd'] for z in batch_sizes
-        ] +
-        # Decode shapes of Deepseek-v3 (Rope)
-        [],
+        [[z, 32, 8, 1, 1024 + 64, 128, 128, 'fwd'] for z in batch_sizes],
         line_arg='provider',
-        line_vals=['triton', 'torch'],
-        line_names=['Triton', 'Torch'],
+        line_vals=['triton'],
+        line_names=['Triton'],
         styles=[('green', '-'), ('green', '--'), ('blue', '-'), ('blue', '--')],
         ylabel=['GB/s', 'TFlops'],
         plot_name='flexAttnCausal-performance',
