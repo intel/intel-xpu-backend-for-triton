4afd331f020b6729dc87378bfd05d16b2fc347fa
Ettore Tiotto <ettore.tiotto@intel.com>
2025-07-31 11:48:46 -0400

    Avoid pass failure for descriptor load/store operations using a tensor descr. allocated on the host (#4812)
    When a tensor descriptor is allocated on the host and passed to a triton
    kernel as a argument, the pass that transforms descriptor load/store
    operations into equivalent block pointer operations currently fails.
    This PR rectifies the situation and allow the pass to complete so that
    subsequently those operations can be lowered to use the 'unwrapped'
    descriptor argument passed to the kernel.
    
    ---------
    
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- third_party/intel/lib/Dialect/Triton/Transforms/TensorDescToBlockPointer.cpp
+++ third_party/intel/lib/Dialect/Triton/Transforms/TensorDescToBlockPointer.cpp
@@ -232,18 +232,24 @@ private:
                              bool> = true>
   LogicalResult rewriteDescriptorLoadOrStoreOp(OpTy op) {
     assert(op && "Expecting a valid operation");
+
+    // At this point we expect to have transformed `make_tensor_descriptor` into
+    // a `make_block_ptr` operation, except when the tensor descriptor is
+    // allocated on the host and passed to the kernel as an argument.
+    Value operand = op.getOperand(0);
+    if (isa<tt::TensorDescType>(operand.getType()))
+      return failure();
+
     LLVM_DEBUG(llvm::dbgs() << "Rewriting: " << op << "\n");
 
     OpBuilder builder(op);
     Location loc = op.getLoc();
-    Value ptr = op.getOperand(0);
-    assert(triton::isTensorPointerType(ptr.getType()) &&
+    assert(triton::isTensorPointerType(operand.getType()) &&
            "Expecting a block ptr");
-    auto ptrType = cast<tt::PointerType>(ptr.getType());
+    auto ptrType = cast<tt::PointerType>(operand.getType());
     auto tensorType = cast<RankedTensorType>(ptrType.getPointeeType());
-
-    ptr =
-        builder.create<tt::AdvanceOp>(loc, ptr.getType(), ptr, op.getIndices());
+    Value ptr =
+        builder.create<tt::AdvanceOp>(loc, ptrType, operand, op.getIndices());
 
     SmallVector<int32_t> boundaryCheck;
     for (size_t i = 0; i < tensorType.getRank(); ++i)
