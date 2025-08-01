eb3bf932dd7080afcb970ae97fbd5277efc55a47
Lu, Chengjun <chengjun.lu@intel.com>
2025-08-01 08:26:17 +0800

    [BENCHMARK] Update the number of flops of the flex attn of causal which is align to the tutorial. (#4805)
    Only the lower triangle is computed when the causal=True for attention.
    
    ---------
    
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
+++ benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
@@ -184,8 +184,8 @@ def benchmark(Z, H_q, H_kv, N_CTX_q, N_CTX_kv, D_HEAD_qk, D_HEAD_v, MODE, provid
     else:
         raise NotImplementedError(f'Unsupported provider {provider}')
 
-    qk_flops = H_q * N_CTX_q * N_CTX_kv * D_HEAD_qk * 2  # mul + add
-    pv_flops = H_q * N_CTX_q * D_HEAD_v * N_CTX_kv * 2  # mul + add
+    qk_flops = H_q * N_CTX_q * N_CTX_kv * D_HEAD_qk  # mul + add, causal=True. Only the lower triangle is computed.
+    pv_flops = H_q * N_CTX_q * D_HEAD_v * N_CTX_kv  # mul + add, causal=True. Only the lower triangle is computed.
     tflops = lambda mean: Z * (qk_flops + pv_flops) * (1e-12) / (mean * 1e-3)
 
     q_elems = H_q * N_CTX_q * D_HEAD_qk
