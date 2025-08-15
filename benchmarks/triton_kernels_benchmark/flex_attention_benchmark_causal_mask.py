604b350e4f72fdf8a21d6a2474ff14d5434306e5
Egor Krivov <egor.krivov@intel.com>
2025-08-15 16:36:14 +0000

    Updated warmup schedule

--- benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
+++ benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
@@ -164,8 +164,10 @@ def benchmark(Z, H_q, H_kv, N_CTX_q, N_CTX_kv, D_HEAD_qk, D_HEAD_v, MODE, provid
             triton_fn = lambda: triton_o.backward(triton_do, retain_graph=True)
 
         benchmark_suit.assert_close(triton_fn, torch_fn, atol=1e-2, rtol=1e-3, err_msg='triton to torch')
-        _, min_ms, max_ms, mean, cv = benchmark_suit.do_bench(triton_fn, n_warmup=100, n_repeat=10, quantiles=quantiles,
-                                                              device=DEVICE)
+        # Need more warmups due to the torch.compile
+        is_bmg = any(name in torch.xpu.get_device_name().lower() for name in ('b570', 'b580'))
+        _, min_ms, max_ms, mean, cv = benchmark_suit.do_bench(triton_fn, n_warmup=50 if is_bmg else 10, n_repeat=10,
+                                                              quantiles=quantiles, device=DEVICE)
 
     elif provider == 'onednn':
         # OneDNN only supports MHA.
