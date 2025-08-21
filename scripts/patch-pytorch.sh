ad26015eff30d5f9747e6751d85fd64a181b968d
Anatoly Myachev <anatoly.myachev@intel.com>
2025-08-21 10:18:06 +0200

    Remove fp64 patch
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- scripts/patch-pytorch.sh
+++ scripts/patch-pytorch.sh
@@ -36,6 +36,5 @@ echo "Applying PyTorch patches in $REPO_ROOT"
 
 # put your patch applies here
 apply_patch ./patch/flex_attn_143553.patch
-apply_patch ./patch/pytorch_fp64.patch
 apply_patch ./patch/pytorch_global_scratch.patch
 apply_patch ./patch/flex_decoding.patch
