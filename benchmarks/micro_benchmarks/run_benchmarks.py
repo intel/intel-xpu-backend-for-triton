1659ad96fdc97c6180626fbcedd23530ba7086d3
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2025-09-04 16:53:07 +0200

    [benchmarks] Reworked the conversion benchmark and added more tests for up/down casts (#4800)
    Here is an example of the benchmark report:
    ```TSV
    float-upcast:
                N  fp8e4nv->fp16  fp8e4nv->bf16  fp8e4nv->fp32  fp8e5->fp16  fp8e5->bf16  fp8e5->fp32  fp16->fp32  bf16->fp32
    0      4096.0       0.010524       0.010252       0.010187     0.080630     0.010488     0.043948    0.139510    0.193939
    1     16384.0       0.042079       0.041001       0.040740     0.322013     0.041959     0.175794    0.556522    0.771375
    2     65536.0       0.168248       0.163971       0.162928     1.286028     0.167731     0.701971    2.223066    3.079699
    3    262144.0       0.507677       0.501730       0.493866     5.088199     0.503581     2.786395    8.820458   12.158813
    4   1048576.0       1.202495       1.163017       1.182960    12.700775     1.169868     8.868200   30.411137   26.532794
    5   4194304.0       1.623786       1.587861       1.594501    21.531335     1.587212    15.298745   50.582537   42.538580
    6  16777216.0       1.961367       1.927211       1.924611    33.078107     1.923428    23.340590   83.022644   69.788752
    7  67108864.0       2.121850       2.079672       2.081887    35.847220     2.076763    25.215246   95.001223   78.473379
    float-downcast:
                N  fp16->fp8e4nv/rtne  fp16->fp8e5/rtne  bf16->fp8e4nv/rtne  bf16->fp8e5/rtne  fp32->fp8e4nv/rtne  fp32->fp8e4nv/rtz  fp32->fp8e5/rtne  fp32->fp8e5/rtz  fp32->fp16/rtne  fp32->fp16/rtz  fp32->bf16/rtne  fp32->bf16/rtz
    0      4096.0            0.023953          0.071309            0.020427          0.020877            0.028330           0.202172          0.051561         0.304762         0.436674        0.508189         0.432981        0.135899
    1     16384.0            0.095768          0.285237            0.081626          0.083490            0.113290           0.807094          0.206036         1.213630         1.731924        2.017734         1.724632        0.542517
    2     65536.0            0.382893          1.138173            0.326310          0.333618            0.452534           3.222026          0.823523         4.825920         6.898526        8.031373         6.855230        2.167196
    3    262144.0            1.490979          3.903276            1.226577          1.254518            1.786452          11.270163          3.193762        17.558205        26.969547       29.721542        26.586613        8.594885
    4   1048576.0            4.111418          5.797081            2.281100          2.298904            6.448014          29.355431         10.123344        53.335504        60.401843       72.067079        57.174264       18.035363
    5   4194304.0            7.612720         12.685410            4.564980          4.603662           12.267993          45.939803         18.057103        85.667974       103.768036      114.161786        97.000555       28.142136
    6  16777216.0           10.127255         14.296733            5.533675          5.576271           18.747797          55.520604         26.028944       111.877941       179.781569      162.617195       168.074694       46.045713
    7  67108864.0           10.861711         15.523535            5.929267          5.961534           18.644044          57.701748         27.834568       117.730718       204.201753      181.876698       191.695795       49.386877
    ```
    
    ---------
    
    Co-authored-by: Ettore Tiotto <ettore.tiotto@intel.com>
    Co-authored-by: Anatoly Myachev <anatoliimyachev@mail.com>

--- benchmarks/micro_benchmarks/run_benchmarks.py
+++ benchmarks/micro_benchmarks/run_benchmarks.py
@@ -1,16 +1,6 @@
-import argparse
-
 from conversion import float_conversion
 from core_ops import dot_scaled
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--reports',
-        type=str,
-        default='',
-        help='directory to save reports',
-    )
-    args = parser.parse_args()
-    float_conversion.benchmark.run(print_data=True, save_path=args.reports)
-    dot_scaled.benchmark.run(print_data=True, save_path=args.reports)
+    for mod in (float_conversion, dot_scaled):
+        mod.run_benchmarks()
