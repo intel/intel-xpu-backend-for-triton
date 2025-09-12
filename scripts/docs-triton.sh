3ba85f75127c141b691e28fdda12e8c6497a70fd
Anatoly Myachev <anatoly.myachev@intel.com>
2025-09-12 15:28:15 +0200

    Revert "[CI] Pin `pytest-rerunfailures<16` (#4994)"
    This reverts commit a57ac88cffa32a3619b073422743a907f97e90e8.

--- scripts/docs-triton.sh
+++ scripts/docs-triton.sh
@@ -36,7 +36,7 @@ export TRITON_PROJ=$BASE/intel-xpu-backend-for-triton
 export TRITON_PROJ_BUILD=$TRITON_PROJ/python/build
 export SCRIPTS_DIR=$(cd $(dirname "$0") && pwd)
 
-python3 -m pip install lit pytest pytest-xdist 'pytest-rerunfailures<16' pytest-skip
+python3 -m pip install lit pytest pytest-xdist pytest-rerunfailures pytest-skip
 
 source $SCRIPTS_DIR/pytest-utils.sh
 $SCRIPTS_DIR/install-pytorch.sh $([ $VENV = true ] && echo "--venv")
