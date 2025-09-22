43468928af0ca06cf01711714352b9a8bf45d1f2
Anatoly Myachev <anatoliimyachev@mail.com>
2025-09-22 15:35:39 +0200

    Update scripts/test-triton.sh

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -584,6 +584,8 @@ run_triton_kernels_tests() {
   # available after `capture_runtime_env` call
   gpu_file="$TRITON_TEST_REPORTS_DIR/gpu.txt"
   if [[ -f "$gpu_file" ]] && grep -q "B580" "$gpu_file"; then
+    # Using any other number of processes results in an error on the BMG due to insufficient resources.
+    # FIXME: reconsider in the future
     max_procs=${PYTEST_MAX_PROCESSES:-1}
   else
     max_procs=${PYTEST_MAX_PROCESSES:-4}
