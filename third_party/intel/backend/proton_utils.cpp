f674b764ee32fce6e0d7bb19ad9966dab5500652
Anatoly Myachev <anatoly.myachev@intel.com>
2025-10-23 11:53:44 +0200

    [PROTON][XPU] Remove l0 build-time dependency (#5369)
    Now code that depends on level-zero will be compiled at Triton runtime
    using `compile_module_from_src`.
    
    ---------
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- third_party/intel/backend/proton_utils.cpp
+++ third_party/intel/backend/proton_utils.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+#include <level_zero/ze_api.h>
 #include <sycl/sycl.hpp>
 
 extern "C" void waitOnSyclQueue(void *syclQueue) {
@@ -34,3 +36,74 @@ enumDeviceUUIDs(std::vector<std::array<uint8_t, 16>> deviceUUIDs_) {
     }
   }
 }
+
+namespace proton {
+
+namespace xpu {
+
+void check(ze_result_t ret, const char *functionName) {
+  if (ret != ZE_RESULT_SUCCESS) {
+    throw std::runtime_error("Failed to execute " + std::string(functionName) +
+                             " with error " + std::to_string(ret));
+  }
+}
+
+// FIXME: for this initialization is needed
+// ref: initDevices
+// static std::vector<std::pair<sycl::device, ze_device_handle_t>>
+//    g_sycl_l0_device_list;
+
+// FIXME: rewrite with
+// sycl::device.get_info<sycl::ext::intel::info::device::architecture>; cache
+// the result
+extern "C" void getDeviceProperties(uint64_t index, uint32_t *clockRate,
+                                    uint32_t *memoryClockRate,
+                                    uint32_t *busWidth, uint32_t *numSms,
+                                    char arch[256]) {
+  // ref: getDeviceProperties
+
+  // FIXME: double check that initialization is needed
+  // At the very least, it shouldn't be for every call
+  check(zeInit(ZE_INIT_FLAG_GPU_ONLY), "zeInit");
+
+  // FIXME: For now I use the naive approach that the device index from PTI
+  // record coincides with the default numbering of all devices
+  uint32_t driverCount = 1;
+  ze_driver_handle_t driverHandle;
+  check(zeDriverGet(&driverCount, &driverHandle), "zeDriverGet");
+  uint32_t deviceCount = 1;
+  // Get device handle
+  ze_device_handle_t phDevice;
+  check(zeDeviceGet(driverHandle, &deviceCount, &phDevice), "zeDeviceGet");
+  // create a struct to hold device properties
+  ze_device_properties_t device_properties = {};
+  device_properties.stype = ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES;
+  check(zeDeviceGetProperties(phDevice, &device_properties),
+        "zeDeviceGetProperties");
+  *clockRate = device_properties.coreClockRate;
+  *numSms =
+      device_properties.numSlices * device_properties.numSubslicesPerSlice;
+  // create a struct to hold device memory properties
+  uint32_t memoryCount = 0;
+  check(zeDeviceGetMemoryProperties(phDevice, &memoryCount, nullptr),
+        "zeDeviceGetMemoryProperties");
+  auto pMemoryProperties = new ze_device_memory_properties_t[memoryCount];
+  for (uint32_t mem = 0; mem < memoryCount; ++mem) {
+    pMemoryProperties[mem].stype = ZE_STRUCTURE_TYPE_DEVICE_MEMORY_PROPERTIES;
+    pMemoryProperties[mem].pNext = nullptr;
+  }
+  check(zeDeviceGetMemoryProperties(phDevice, &memoryCount, pMemoryProperties),
+        "zeDeviceGetMemoryProperties");
+
+  *memoryClockRate = pMemoryProperties[0].maxClockRate;
+  *busWidth = pMemoryProperties[0].maxBusWidth;
+
+  delete[] pMemoryProperties;
+
+  // FIXME: there should be architecture, but not a name
+  memcpy(arch, device_properties.name, 256);
+}
+
+} // namespace xpu
+
+} // namespace proton
