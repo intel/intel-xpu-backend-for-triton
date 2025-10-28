f7e86ccd2fd1254fa4b78dd24603e9d186addffa
Egor Krivov <egor.krivov@intel.com>
2025-10-28 14:39:39 +0000

    Update for extrnal dependency

--- benchmarks/third_party/sglang/scaled_mm_benchmark.py
+++ benchmarks/third_party/sglang/scaled_mm_benchmark.py
@@ -10,6 +10,8 @@ import triton.language as tl
 
 import triton_kernels_benchmark as benchmark_suite
 
+from sglang.srt.layers.quantization.fp8_kernel import triton_scaled_mm
+
 
 def is_weak_contiguous(x: torch.Tensor):
     strides = x.stride()
@@ -38,124 +40,6 @@ def get_matmul_batched_autotune_configs() -> List[triton.Config]:
         for s in [2]
     ]
     return configs
-
-
-# @triton.autotune(
-#     configs=get_matmul_batched_autotune_configs(),
-#     key=['M', 'N', 'K'])
-@triton.jit
-def scaled_mm_kernel(
-    a_ptr,
-    b_ptr,
-    scale_a_ptr,
-    scale_b_ptr,
-    c_ptr,
-    bias_ptr,
-    M,
-    N,
-    K,
-    stride_am: tl.int64,
-    stride_ak: tl.constexpr,
-    stride_bk: tl.constexpr,
-    stride_bn: tl.constexpr,
-    stride_cm: tl.constexpr,
-    stride_cn: tl.constexpr,
-    ACCUMULATOR_DTYPE: tl.constexpr,
-    BLOCK_SIZE_M: tl.constexpr,
-    BLOCK_SIZE_N: tl.constexpr,
-    BLOCK_SIZE_K: tl.constexpr,
-    BLOCK_SIZE_SCALE_A: tl.constexpr,
-    BLOCK_SIZE_SCALE_B: tl.constexpr,
-):
-    pid = tl.program_id(axis=0)
-
-    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
-
-    pid_m = pid // num_pid_n
-    pid_n = pid % num_pid_n
-
-    accumulator_dtype = ACCUMULATOR_DTYPE
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=accumulator_dtype)
-
-    # NOTE: Some tensor inputs are so large, they will cause int32 overflow
-    # so it is necessary to use tl.int64 for all the offsets, else SEGV will
-    # eventually occur.
-
-    # Offsets and masks.
-    offsets_am = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M).to(tl.int64)
-    masks_am = offsets_am < M
-
-    offsets_bn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N).to(tl.int64)
-    masks_bn = offsets_bn < N
-
-    offsets_k = tl.arange(0, BLOCK_SIZE_K).to(tl.int64)
-    offsets_a = stride_am * offsets_am[:, None] + stride_ak * offsets_k[None, :]
-    offsets_b = stride_bk * offsets_k[:, None] + stride_bn * offsets_bn[None, :]
-
-    # NOTE: BLOCK_SIZE_SCALE_A could be 1 or BLOCK_SIZE_M, so need to create
-    # appropriate offsets and masks for each case. Same goes for
-    # BLOCK_SIZE_SCALE_B.
-    offsets_scale_am = tl.arange(0, BLOCK_SIZE_SCALE_A) + (BLOCK_SIZE_SCALE_A > 1) * pid_m * BLOCK_SIZE_M
-    masks_scale_am = offsets_scale_am < M
-
-    offsets_scale_bn = tl.arange(0, BLOCK_SIZE_SCALE_B) + (BLOCK_SIZE_SCALE_B > 1) * pid_n * BLOCK_SIZE_N
-    masks_scale_bn = offsets_scale_bn < N
-
-    a_ptrs = a_ptr + offsets_a
-    b_ptrs = b_ptr + offsets_b
-
-    scale_a_ptrs = scale_a_ptr + offsets_scale_am
-    scale_b_ptrs = scale_b_ptr + offsets_scale_bn
-
-    for _ in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
-        masks_k = offsets_k < K
-        masks_a = masks_am[:, None] & masks_k[None, :]
-        a = tl.load(a_ptrs, mask=masks_a)
-
-        masks_b = masks_k[:, None] & masks_bn[None, :]
-        b = tl.load(b_ptrs, mask=masks_b)
-
-        # Accumulate results.
-        accumulator = tl.dot(a, b, accumulator, out_dtype=accumulator_dtype)
-
-        offsets_k += BLOCK_SIZE_K
-        a_ptrs += BLOCK_SIZE_K * stride_ak
-        b_ptrs += BLOCK_SIZE_K * stride_bk
-
-    # Apply scale at end.
-    masks_scale_a = masks_scale_am[:, None] & (tl.arange(0, 1) < 1)[:, None]
-    scale_a = tl.load(scale_a_ptrs[:, None], masks_scale_a)
-    # Need to broadcast to the appropriate size, if scale_a is already
-    # (BLOCK_SIZE_M, 1) then it will broadcast to its own shape. Same goes
-    # for scale_b below.
-    scale_a = scale_a.broadcast_to((BLOCK_SIZE_M, 1))
-    accumulator = scale_a * accumulator.to(tl.float32)
-
-    masks_scale_b = masks_scale_bn[:, None] & (tl.arange(0, 1) < 1)[None, :]
-    scale_b = tl.load(scale_b_ptrs[:, None], masks_scale_b)
-    scale_b = scale_b.broadcast_to((BLOCK_SIZE_N, 1))
-    accumulator = scale_b.T * accumulator.to(tl.float32)
-
-    # Convert to output format.
-    c = accumulator.to(c_ptr.type.element_ty)
-
-    # Add bias, it's already in output format, so add it after conversion.
-    if bias_ptr:
-        offsets_bias = offsets_bn
-        bias_ptrs = bias_ptr + offsets_bias
-        bias_mask = offsets_bias < N
-        bias = tl.load(bias_ptrs, bias_mask)
-        c += bias
-
-    # Save output
-    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M).to(tl.int64)
-    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N).to(tl.int64)
-    offs_cm = offs_cm.to(tl.int64)
-    offs_cn = offs_cn.to(tl.int64)
-    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
-    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-
-    tl.store(c_ptrs, c, mask=c_mask)
 
 
 @triton.jit
@@ -290,10 +174,9 @@ def scaled_mm_kernel_td(
 # input  - [M, K]
 # weight - [K, N]
 # Adapted from https://github.com/vllm-project/vllm/blob/main/vllm/model_executor/layers/quantization/compressed_tensors/triton_scaled_mm.py
-def triton_scaled_mm(
+def triton_scaled_mm_td(
     input: torch.Tensor,  # pylint: disable=redefined-builtin
     weight: torch.Tensor,
-    result: torch.Tensor,
     scale_a: torch.Tensor,
     scale_b: torch.Tensor,
     out_dtype: type[torch.dtype],
@@ -302,7 +185,6 @@ def triton_scaled_mm(
     block_size_n: int = 32,
     block_size_k: int = 32,
     use_heuristic=True,
-    use_td_kernel=False,
 ) -> torch.Tensor:
     M, K = input.shape
     N = weight.shape[1]
@@ -324,7 +206,7 @@ def triton_scaled_mm(
 
     grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
 
-    # result = torch.empty((M, N), dtype=out_dtype, device=input.device)
+    result = torch.empty((M, N), dtype=out_dtype, device=input.device)
 
     has_scalar = lambda x: x.shape[0] == 1 and x.shape[1] == 1
 
@@ -349,10 +231,9 @@ def triton_scaled_mm(
 
     accumulator_dtype = tl.float32 if input.is_floating_point() else tl.int32
 
-    kernel = scaled_mm_kernel if not use_td_kernel else scaled_mm_kernel_td
     # A = input, B = weight, C = result
     # A = M x K, B = K x N, C = M x N
-    kernel[grid](
+    scaled_mm_kernel_td[grid](
         input,
         weight,
         scale_a,
@@ -385,7 +266,6 @@ device = 'xpu'
 def torch_scaled_mm(
     a: torch.Tensor,
     b: torch.Tensor,
-    result: torch.Tensor,
     scale_a: torch.Tensor,
     scale_b: torch.Tensor,
     out_dtype: torch.dtype,
@@ -396,9 +276,7 @@ def torch_scaled_mm(
     out = scale_a.to(torch.float32) * out * scale_b.to(torch.float32).T
     if bias is not None:
         out = out + bias.to(torch.float32)
-    result[:] = out.to(out_dtype)
-    return result
-    # return out.to(out_dtype)
+    return out.to(out_dtype)
 
 
 def _make_inputs(M, K, N, in_dtype):
@@ -429,11 +307,8 @@ def get_scaled_mm_benchmark(
     supported_providers = {
         'triton': 'triton',
         'triton-td': 'triton-td',
-        'pytorch': 'pytorch',
+        'pytorch': 'pytorch-deqmm',
     }
-    if fp8:
-        pass
-
     providers = benchmark_suite.filter_providers(supported_providers, providers_filter)
 
     @benchmark_suite.perf_report(
@@ -464,10 +339,8 @@ def get_scaled_mm_benchmark(
         scale_b = 0.1 + 0.05 * torch.rand((N, 1), dtype=torch.float32, device=device)
         bias = (0.01 * torch.randn((M, N), dtype=out_dtype, device=device) if with_bias else None)
 
-        ref = torch.empty((M, N), dtype=out_dtype, device=x.device)
-
         def torch_fn():
-            return torch_scaled_mm(x, weight, ref, scale_a, scale_b, out_dtype, bias)
+            return torch_scaled_mm(x, weight, scale_a, scale_b, bias)
 
         # Use relaxed tolerances
         rtol = 0.15 if in_dtype == torch.int8 else 0.25
@@ -483,12 +356,10 @@ def get_scaled_mm_benchmark(
             )
 
         elif provider in ('triton', 'triton-td'):
-            result = torch.empty((M, N), dtype=out_dtype, device=x.device)
+            invoke_kernel = triton_scaled_mm if provider == 'triton' else triton_scaled_mm_td
 
-            # invoke_kernel = invoke_moe_batched_triton_kernel if provider == 'triton' else invoke_moe_batched_triton_kernel_td
             def triton_fn():
-                return triton_scaled_mm(x, weight, result, scale_a, scale_b, out_dtype, bias,
-                                        use_td_kernel=provider == 'triton-td')
+                return invoke_kernel(x, weight, scale_a, scale_b, out_dtype, bias)
 
             benchmark_suite.assert_close(triton_fn, torch_fn, atol=atol, rtol=rtol, err_msg='triton to torch')
 
