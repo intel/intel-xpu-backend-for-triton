e5a4abe43aac7eeb6d8d896d78f0e2825264af6f
Anatoly Myachev <anatoly.myachev@intel.com>
2025-11-20 20:48:44 +0100

    Adapt code to use `uv` as a package manager (#5521)
    Closes #4465
    
    `Test with pip` is expected to fail until
    https://github.com/pytorch/pytorch/pull/166829 is merged.
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>
    (cherry picked from commit 146c37ed618f4141778fa1b5ebad7b311177096d)

--- scripts/pti_lib.py
+++ scripts/pti_lib.py
@@ -13,7 +13,7 @@ def get_pti_lib_path() -> pathlib.Path:
     """
     files = importlib.metadata.files('intel-pti') or []
     for f in files:
-        if f.name == 'libpti_view.so':
+        if 'libpti_view.so' in f.name:
             return pathlib.Path(f.locate()).parent.resolve()
     raise AssertionError('libpti_view.so not found')
 
