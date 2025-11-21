5ba680bea71482e7dd93f8acc7f63cae9b65ee80
Ettore Tiotto <ettore.tiotto@intel.com>
2025-11-21 20:36:18 +0000

    Stride Versioning pass
    Signed-off-by: Ettore Tiotto <ettore.tiotto@intel.com>

---
+++ test/Triton/Intel/StrideVersioning/stride-versioning.mlir
@@ -0,0 +1,64 @@
+// RUN: triton-opt %s -triton-intel-stride-versioning | FileCheck %s
+
+module {
+  tt.func public @matmul_kernel_with_tensor_descriptors(%arg0: !tt.ptr<bf16> {tt.divisibility = 16 : i32}, %arg1: !tt.ptr<bf16> {tt.divisibility = 16 : i32}, %arg2: !tt.ptr<f32> {tt.divisibility = 16 : i32}, %arg3: i64 {tt.divisibility = 16 : i32}, %arg4: i64, %arg5: i64 {tt.divisibility = 16 : i32}, %arg6: i64, %arg7: i64 {tt.divisibility = 16 : i32}, %arg8: i64) attributes {noinline = false} {
+    %c64_i32 = arith.constant 64 : i32
+    %cst = arith.constant dense<0.000000e+00> : tensor<256x256xf32>
+    %c256_i32 = arith.constant 256 : i32
+    %c32_i32 = arith.constant 32 : i32
+    %c0_i32 = arith.constant 0 : i32
+    %c4096_i32 = arith.constant 4096 : i32
+    %c8192_i32 = arith.constant 8192 : i32
+    %c4_i32 = arith.constant 4 : i32
+    %0 = tt.get_program_id x : i32
+    %1 = arith.divsi %0, %c64_i32 : i32
+    %2 = arith.muli %1, %c4_i32 : i32
+    %3 = arith.subi %c32_i32, %2 : i32
+    %4 = arith.minsi %3, %c4_i32 : i32
+    %5 = arith.remsi %0, %c64_i32 : i32
+    %6 = arith.remsi %5, %4 : i32
+    %7 = arith.addi %2, %6 : i32
+    %8 = arith.divsi %5, %4 : i32
+    %9 = arith.extsi %c8192_i32 : i32 to i64
+    %10 = arith.extsi %c4096_i32 : i32 to i64
+    %11 = tt.make_tensor_ptr %arg0, [%9, %10], [%arg3, %arg4], [%c0_i32, %c0_i32] {order = array<i32: 1, 0>} : <tensor<256x32xbf16>>
+    %12 = tt.make_tensor_ptr %arg1, [%10, %10], [%arg5, %arg6], [%c0_i32, %c0_i32] {order = array<i32: 1, 0>} : <tensor<32x256xbf16>>
+    %13 = arith.muli %7, %c256_i32 : i32
+    %14 = arith.muli %8, %c256_i32 : i32
+    %15:2 = scf.for %arg9 = %c0_i32 to %c4096_i32 step %c32_i32 iter_args(%arg10 = %cst, %arg11 = %c0_i32) -> (tensor<256x256xf32>, i32)  : i32 {
+      %20 = tt.advance %11, [%13, %arg11] : <tensor<256x32xbf16>>
+      %21 = tt.load %20 {boundaryCheck = array<i32: 0, 1>} : !tt.ptr<tensor<256x32xbf16>>
+      %22 = tt.advance %12, [%arg11, %14] : <tensor<32x256xbf16>>
+      %23 = tt.load %22 {boundaryCheck = array<i32: 0, 1>} : !tt.ptr<tensor<32x256xbf16>>
+      %24 = tt.dot %21, %23, %cst, inputPrecision = tf32 : tensor<256x32xbf16> * tensor<32x256xbf16> -> tensor<256x256xf32>
+      %25 = arith.addf %arg10, %24 : tensor<256x256xf32>
+      %26 = arith.addi %arg11, %c32_i32 : i32
+      scf.yield %25, %26 : tensor<256x256xf32>, i32
+    }
+    %16 = tt.make_tensor_ptr %arg2, [%9, %10], [%arg7, %arg8], [%c0_i32, %c0_i32] {order = array<i32: 1, 0>} : <tensor<256x256xf32>>
+    %17 = arith.muli %7, %c256_i32 : i32
+    %18 = arith.muli %8, %c256_i32 : i32
+    %19 = tt.advance %16, [%17, %18] : <tensor<256x256xf32>>
+    tt.store %19, %15#0 {boundaryCheck = array<i32: 0, 1>} : !tt.ptr<tensor<256x256xf32>>
+    tt.return
+  }
+
+  // CHECK: tt.func public @matmul_kernel_with_tensor_descriptors
+  // CHECK:   [[CST_1_i64:%.+]] = arith.constant 1 : i64
+  // CHECK:   [[NEW_TDESC1:%.+]] = tt.make_tensor_ptr %arg0, {{.*}}, [%arg3, %c1_i64], {{.*}} {order = array<i32: 1, 0>} : <tensor<256x32xbf16>>
+  // CHECK:   [[ORIG_TDESC1:%.+]] = tt.make_tensor_ptr %arg0, {{.*}}, [%arg3, %arg4], {{.*}} {order = array<i32: 1, 0>} : <tensor<256x32xbf16>>
+  // CHECK:   [[NEW_TDESC2:%.+]] = tt.make_tensor_ptr %arg1, {{.*}}, [%arg5, %c1_i64], {{.*}} {order = array<i32: 1, 0>} : <tensor<32x256xbf16>>
+  // CHECK:   [[ORIG_TDESC2:%.+]] = tt.make_tensor_ptr %arg1, {{.*}}, [%arg5, %arg6], {{.*}} {order = array<i32: 1, 0>} : <tensor<32x256xbf16>>  
+  // CHECK:   [[CMP1:%.+]] = arith.cmpi eq, %arg4, [[CST_1_i64]] : i64
+  // CHECK:   [[CMP2:%.+]] = arith.cmpi eq, %arg6, [[CST_1_i64]] : i64
+  // CHECK:   [[VER_COND:%.+]] = arith.andi [[CMP1]], [[CMP2]] : i1
+  // CHECK:   [[LOOP_VER:%.+]]:2 = scf.if [[VER_COND]] 
+  // CHECK:     [[THEN_LOOP_RES:%.+]]:2 = scf.for {{.*}} 
+  // CHECK:       [[ADV_1:%.+]] = tt.advance [[NEW_TDESC1]]
+  // CHECK:       [[ADV_2:%.+]] = tt.advance [[NEW_TDESC2]]
+  // CHECK:   } else {
+  // CHECK:     [[ELSE_LOOP_RES:%.+]]:2 = scf.for {{.*}} 
+  // CHECK:       [[ADV_1:%.+]] = tt.advance [[ORIG_TDESC1]]
+  // CHECK:       [[ADV_2:%.+]] = tt.advance [[ORIG_TDESC2]]
+  // CHECK:   }  
+}
---
+++ third_party/intel/lib/Dialect/Triton/Transforms/StrideVersioning.cpp
@@ -0,0 +1,271 @@
+#include "intel/include/Dialect/Triton/Transforms/Passes.h"
+#include "intel/include/Utils/Utility.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Value.h"
+#include "mlir/IR/Verifier.h"
+#include "mlir/Support/LLVM.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/Triton/IR/Types.h"
+#include "triton/Dialect/Triton/IR/Utility.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+#include <unordered_map>
+
+#define DEBUG_TYPE "triton-intel-stride-versioning"
+
+using namespace mlir;
+namespace tt = mlir::triton;
+
+namespace mlir::triton::intel {
+#define GEN_PASS_DEF_TRITONINTELSTRIDEVERSIONING
+#include "intel/include/Dialect/Triton/Transforms/Passes.h.inc"
+} // namespace mlir::triton::intel
+
+namespace {
+
+class OpSelector {
+public:
+  // Select load operations that use a block ptr defined by a
+  // make_tensor_ptr operation with at least one stride that has unknown value
+  // at compile time.
+  virtual bool isCandidate(scf::ForOp &forOp, Operation *op) const {
+    assert(op->getParentOfType<scf::ForOp>() == forOp &&
+           "Operation should be contains in the loop");
+
+    return TypeSwitch<Operation *, bool>(op)
+        .Case<tt::LoadOp>([](auto loadOp) {
+          Value ptr = loadOp.getPtr();
+          if (!tt::isTensorPointerType(ptr.getType()))
+            return false;
+
+          auto tensorType = cast<RankedTensorType>(
+              cast<tt::PointerType>(ptr.getType()).getPointeeType());
+          if (tensorType.getRank() > 2)
+            return false;
+
+          auto isOneConst = [](Value v) {
+            auto constantOp = v.getDefiningOp<arith::ConstantOp>();
+            if (!constantOp)
+              return false;
+            if (auto denseAttr =
+                    dyn_cast<DenseIntElementsAttr>(constantOp.getValueAttr()))
+              return denseAttr.isSplat() &&
+                     denseAttr.getSplatValue<APInt>().isOne();
+            return false;
+          };
+
+          // If no stride has value equal to one we have found a candidate
+          // operation.
+          OperandRange strides = tt::getMakeTensorPtrOp(ptr).getStrides();
+          bool isCandidate = true;
+          for (Value stride : strides) {
+            Value finalVal = tt::intel::getFinalValue(stride);
+            assert(finalVal && "Expecting a valid mask");
+            if (!finalVal.getDefiningOp())
+              continue;
+            if (isOneConst(finalVal)) {
+              isCandidate = false;
+              break;
+            }
+          }
+
+          return isCandidate;
+        })
+        .Default([](auto) { return false; });
+  }
+
+  std::string getName() const { return "OpSelector"; };
+};
+
+// Collects operations in a loop that satisfy the condition imposed by
+// the operation selector associated with this class.
+template <typename OpSelector> class OpsCollector {
+public:
+  using Operations = SmallPtrSet<Operation *, 8>;
+
+  OpsCollector(scf::ForOp &forOp, OpSelector &selector)
+      : forOp(forOp), selector(selector) {}
+
+  bool collectOps() {
+    auto collectOps = [&](auto ops, Operations &selectedOps) {
+      for (Operation *op : ops) {
+        if (selector.isCandidate(forOp, op)) {
+          selectedOps.insert(op);
+          LLVM_DEBUG(llvm::dbgs()
+                     << selector.getName() << ": collected: " << *op << "\n");
+        }
+      }
+    };
+
+    collectOps(forOp.getOps<tt::LoadOp>(), ops);
+    return ops.size();
+  }
+
+  const Operations &getOps() const { return ops; };
+  const OpSelector &getSelector() const { return selector; }
+
+private:
+  scf::ForOp &forOp;
+  OpSelector &selector;
+  Operations ops;
+};
+
+class LoopVersioner {
+public:
+  void
+  version(scf::ForOp &forOp, ArrayRef<Operation *> makeTensorPtrOps,
+          std::unordered_map<Operation *, Value> makeTensorPtrToStride) const {
+    assert(makeTensorPtrOps.size() == makeTensorPtrToStride.size() &&
+           "Sizes should match");
+
+    Location loc = forOp.getLoc();
+    auto funcOp = forOp->getParentOfType<tt::FuncOp>();
+    OpBuilder builder(&funcOp.front().front());
+    auto oneVal =
+        arith::ConstantOp::create(builder, loc, builder.getI64IntegerAttr(1));
+
+    // Build the versioning condition for the loop.
+    builder.setInsertionPoint(forOp);
+    SmallVector<Value> versioningConds;
+    for (Operation *makeTensorPtrOp : makeTensorPtrOps) {
+      Value stride = makeTensorPtrToStride[makeTensorPtrOp];
+      versioningConds.emplace_back(builder.create<arith::CmpIOp>(
+          loc, arith::CmpIPredicate::eq, stride, oneVal));
+    }
+
+    Value verCond = versioningConds.front();
+    for (unsigned i = 1; i < versioningConds.size(); ++i)
+      verCond = builder.create<arith::AndIOp>(loc, verCond, versioningConds[i]);
+
+    // Version the loop.
+    auto ifOp = builder.create<scf::IfOp>(loc, forOp.getResultTypes(), verCond,
+                                          /*withThenRegion=*/true);
+    IRMapping map;
+    OpBuilder thenB = ifOp.getThenBodyBuilder();
+    Operation *thenForLoop = thenB.clone(*forOp.getOperation(), map);
+    OpBuilder elseB = ifOp.getElseBodyBuilder();
+    Operation *elseForLoop = elseB.clone(*forOp.getOperation());
+
+    // Create the yield operations for the two if branches.
+    if (!thenForLoop->getResults().empty()) {
+      thenB.create<scf::YieldOp>(loc, thenForLoop->getResults());
+      elseB.create<scf::YieldOp>(loc, elseForLoop->getResults());
+    }
+
+    // Now that the loop has been versioned, replace the uses of the original
+    // loop results.
+    for (const auto &[i, v] : llvm::enumerate(forOp.getResults()))
+      if (!v.getUsers().empty())
+        v.replaceAllUsesWith(ifOp->getResult(i));
+
+    // Clone the makeTensorPtrOps and replace 'versioned' stride with one.
+    for (Operation *makeTensorPtrOp : makeTensorPtrOps) {
+      auto newOp = cast<tt::MakeTensorPtrOp>(makeTensorPtrOp->clone());
+      Value versionedStride = makeTensorPtrToStride[makeTensorPtrOp];
+      for (OpOperand &stride : newOp.getStridesMutable()) {
+        if (stride.get() == versionedStride) {
+          stride.set(oneVal);
+          break;
+        }
+      }
+
+      builder.setInsertionPoint(makeTensorPtrOp);
+      builder.insert(newOp);
+      makeTensorPtrOp->replaceUsesWithIf(newOp, [&](OpOperand &use) {
+        return use.getOwner()->getParentOfType<scf::ForOp>() == thenForLoop;
+      });
+    }
+
+    forOp.erase();
+  }
+};
+
+struct TritonIntelStrideVersioning
+    : tt::intel::impl::TritonIntelStrideVersioningBase<
+          TritonIntelStrideVersioning> {
+public:
+  using Base::Base;
+  using IndexMapSet = std::map<int, std::set<int>>;
+
+  void runOnOperation() final {
+    ModuleOp moduleOp = getOperation();
+
+    // Version loops containing masked operation in canonical form.
+    moduleOp->walk<WalkOrder::PreOrder>([&](Operation *op) {
+      if (auto forOp = dyn_cast<scf::ForOp>(op)) {
+        // Nested loop aren't currently handled.
+        if (forOp->getParentOfType<scf::ForOp>())
+          return WalkResult::advance();
+
+        // Consider only loops with a single IV.
+        if (!forOp.getSingleInductionVar())
+          return WalkResult::advance();
+
+        // Collect candidate operations. These are load/store operations that
+        // use a block ptr with no stride equal to one (at compile time).
+        OpSelector selector;
+        OpsCollector collector(forOp, selector);
+        if (collector.collectOps()) {
+          OpBuilder builder(forOp);
+          Location loc = forOp->getLoc();
+
+          SmallVector<Operation *> makeTensorPtrOps;
+          std::unordered_map<Operation *, Value> makeTensorPtrToStride;
+          for (Operation *op : collector.getOps()) {
+            TypeSwitch<Operation *>(op)
+                .Case<tt::LoadOp>([&](auto loadOp) {
+                  Value ptr = loadOp.getPtr();
+                  assert(tt::isTensorPointerType(ptr.getType()) &&
+                         "Expecting a block ptr");
+
+                  auto makeTensorPtrOp = tt::getMakeTensorPtrOp(ptr);
+                  makeTensorPtrOps.push_back(makeTensorPtrOp);
+                  OperandRange strides = makeTensorPtrOp.getStrides();
+
+                  for (Value stride : strides) {
+                    Value finalVal = tt::intel::getFinalValue(stride);
+                    assert(finalVal && "Expecting a valid mask");
+
+                    Operation *defOp = finalVal.getDefiningOp();
+                    if (defOp)
+                      continue;
+
+                    auto blockArg = cast<BlockArgument>(finalVal);
+                    Operation *parentOp = blockArg.getOwner()->getParentOp();
+                    // TODO: handle block args for things that aren't a
+                    // function.
+                    auto funcOp = cast<tt::FuncOp>(parentOp);
+                    auto divisibilityAttr =
+                        funcOp.getArgAttrOfType<IntegerAttr>(
+                            blockArg.getArgNumber(), "tt.divisibility");
+                    if (divisibilityAttr) {
+                      assert(divisibilityAttr.getValue().isStrictlyPositive() &&
+                             "Unexpected divisibility value");
+                      continue;
+                    }
+
+                    makeTensorPtrToStride[makeTensorPtrOp] = blockArg;
+                  }
+                })
+                .Default([](auto) { return false; });
+          }
+
+          LoopVersioner loopVersioner;
+          loopVersioner.version(forOp, makeTensorPtrOps, makeTensorPtrToStride);
+        }
+      }
+
+      return WalkResult::advance();
+    });
+
+    LLVM_DEBUG(llvm::dbgs() << "After versioning:\n" << moduleOp << "\n");
+    assert(succeeded(verify(moduleOp)) && "Module verification failed");
+  }
+};
+
+} // namespace
