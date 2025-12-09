09372c9fdaa7c6e5498ffc133c4834ce163cf8fd
Ettore Tiotto <ettore.tiotto@intel.com>
2025-12-09 18:56:39 +0000

    WIP: linear layour conversion, scaled_dot, DPAS layout.
    Signed-off-by: Ettore Tiotto <ettore.tiotto@intel.com>

--- third_party/intel/lib/Dialect/TritonIntelGPU/IR/Dialect.cpp
+++ third_party/intel/lib/Dialect/TritonIntelGPU/IR/Dialect.cpp
@@ -267,6 +267,8 @@ DpasEncodingAttr::getDPASCapability(ModuleOp mod) {
   return DPASCapability();
 }
 
+#if 0
+// TODO: on CRI this is no longer applied. is it applied there getOpsPerChannel was called ? Do we still need it?
 unsigned DpasEncodingAttr::getOpsPerChannel(Type elemType) {
   assert(elemType.isIntOrFloat() && "unsupported type for DpasEncodingAttr");
 
@@ -276,12 +278,14 @@ unsigned DpasEncodingAttr::getOpsPerChannel(Type elemType) {
 
   return DPASCapability::opsChanBitWidths / dpasElemBitWidths;
 }
+#endif
 
 LogicalResult DpasEncodingAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
     unsigned repeatCount, unsigned systolicDepth, unsigned executionSize,
     unsigned opsPerChan, ::llvm::ArrayRef<unsigned> warpsPerCTA,
-    ::llvm::ArrayRef<unsigned> repCluster, unsigned subGroupSize) {
+    ::llvm::ArrayRef<unsigned> repCluster, unsigned subGroupSize,
+    std::optional<unsigned> fp4KPack) {
   if (repeatCount > 8 || repeatCount < 1) {
     return emitError() << "repeatCount must be in the range [1, 8], but was:"
                        << repeatCount;
@@ -307,6 +311,14 @@ LogicalResult DpasEncodingAttr::verify(
                        << subGroupSize << ", executionSize:" << executionSize;
   }
 
+  if (fp4KPack) {
+    if (*fp4KPack != 2)
+      return emitError() << "fp4KPack must be 2 but was:" << *fp4KPack;
+    if (opsPerChan != 4)
+      return emitError() << "opsPerChannel must be 4 for fp4 type, but was:"
+                         << opsPerChan;
+  }
+
   return success();
 }
 
@@ -325,6 +337,7 @@ Attribute DpasEncodingAttr::parse(AsmParser &parser, Type type) {
   unsigned executionSize = 0;
   unsigned opsPerChan = 0;
   unsigned threadsPerWarp = 0;
+  std::optional<unsigned> fp4KPack = std::nullopt;
 
   for (const NamedAttribute &attr : dict) {
     if (attr.getName() == "repeatCount") {
@@ -355,11 +368,17 @@ Attribute DpasEncodingAttr::parse(AsmParser &parser, Type type) {
       if (parseUInt(parser, attr, threadsPerWarp, "threadsPerWarp").failed())
         return {};
     }
+    if (attr.getName() == "fp4KPack") {
+      unsigned pack;
+      if (parseUInt(parser, attr, pack, "fp4KPack").failed())
+        return {};
+      fp4KPack = pack;
+    }
   }
 
   return parser.getChecked<DpasEncodingAttr>(
       parser.getContext(), repeatCount, systolicDepth, executionSize,
-      opsPerChan, warpsPerCTA, repCluster, threadsPerWarp);
+      opsPerChan, warpsPerCTA, repCluster, threadsPerWarp, fp4KPack);
 }
 
 void DpasEncodingAttr::print(AsmPrinter &printer) const {
@@ -371,12 +390,16 @@ void DpasEncodingAttr::print(AsmPrinter &printer) const {
   ArrayRef<unsigned> rC = shapeC;
   auto warpsPerCTA = getWarpsPerCTA();
   ArrayRef<unsigned> repCluster = getRepCluster();
+  std::optional<unsigned> fp4KPack = getFp4KPack();
   printer << "<{"
           << "repeatCount = " << getRepeatCount() << ", "
           << "systolicDepth = " << getSystolicDepth() << ", "
           << "executionSize = " << getExecutionSize() << ", "
-          << "opsPerChan = " << getOpsPerChannel() << ", "
-          << "threadsPerWarp = " << getThreadsPerWarp() << ", "
+          << "opsPerChan = " << getOpsPerChannel() << ", ";
+  if (fp4KPack) {
+    printer << "fp4KPack = " << *fp4KPack << ", ";
+  }
+  printer << "threadsPerWarp = " << getThreadsPerWarp() << ", "
           << "warpsPerCTA = [" << llvm::ArrayRef<unsigned>(warpsPerCTA) << "], "
           << "repCluster = [" << repCluster << "], "
           << "A = [" << rA << "], "
