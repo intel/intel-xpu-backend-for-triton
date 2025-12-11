155bd3932d5d202d7cd77420bf2452d97c26a2f9
Lu,Chengjun <chengjun.lu@intel.com>
2025-12-11 14:18:05 +0000

    [LoadStoreOpToLLVM] Enable the block 2D io for all layout if possible.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/intel/lib/TritonIntelGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -363,7 +363,7 @@ struct BlockIOConversionBase : public LoadStoreConversionBase {
             std::enable_if_t<
                 llvm::is_one_of<OpTy, triton::LoadOp, triton::StoreOp>::value,
                 bool> = true>
-  static bool isBlockIOCandidate(OpTy op, bool allLayout = false) {
+  static bool isBlockIOCandidate(OpTy op) {
     ModuleOp mod = op->template getParentOfType<ModuleOp>();
     if (!mod->hasAttr(triton::gpu::intel::TritonIntelGPUDialect::
                           getSupport2DBlockIOAttrName()))
@@ -374,11 +374,7 @@ struct BlockIOConversionBase : public LoadStoreConversionBase {
     if (!blockIOAttr)
       return false;
 
-    // Only lower operation with dpas layout encoding.
-    auto tensorTy =
-        cast<RankedTensorType>(getPointeeType(op.getPtr().getType()));
-    return allLayout || hasDpasEncoding(tensorTy) ||
-           hasDotDpasEncoding(tensorTy);
+    return true;
   }
 
   static bool
@@ -1341,9 +1337,7 @@ struct LoadOpToBlockIOConversion
     if (op.getPadding() && op.getPadding() == PaddingOption::PAD_NAN)
       return failure();
 
-    static const bool enableBlockIOForAllLayout =
-        triton::tools::getBoolEnv("TRITON_INTEL_ENABLE_BLOCK_IO_ALL_LAYOUTS");
-    if (!isBlockIOCandidate(op, enableBlockIOForAllLayout))
+    if (!isBlockIOCandidate(op))
       return failure();
 
     // FIXME: Remove once IGC can split large 2D block loads.
@@ -2165,9 +2159,7 @@ struct StoreOpToBlockIOConversion
   LogicalResult
   matchAndRewrite(triton::StoreOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const final {
-    static const bool enableBlockIOForAllLayout =
-        triton::tools::getBoolEnv("TRITON_INTEL_ENABLE_BLOCK_IO_ALL_LAYOUTS");
-    if (!isBlockIOCandidate(op, enableBlockIOForAllLayout))
+    if (!isBlockIOCandidate(op))
       return failure();
 
     // Get the max tile shape supported by the layout.
