ad89cb4b5ec54a14daa37a46f8879eee16125d67
Mieszko Dziadowiec <mieszko.dziadowiec@intel.com>
2025-12-16 21:25:48 +0100

    Remove redundant store/load for accumulator, just use created tdescriptor

--- benchmarks/triton_kernels_benchmark/gluon_gemm_benchmark.py
+++ benchmarks/triton_kernels_benchmark/gluon_gemm_benchmark.py
@@ -6,6 +6,18 @@ from triton.experimental.gluon.language.intel import IntelDPASLayout
 from utils.dpas_layout_analyzer import calculate_optimal_warps_per_cta, calculate_optimal_rep_clusters
 
 
+@gluon.constexpr_function
+def get_dpas_layout(num_warps: ttgl.constexpr, m_shape: ttgl.constexpr, n_shape: ttgl.constexpr,
+                    k_shape: ttgl.constexpr) -> ttgl.constexpr:
+    threads_per_warp = 16
+    warps_per_cta = calculate_optimal_warps_per_cta(num_warps, m_shape, n_shape)
+
+    return IntelDPASLayout(
+        repeatCount=8, systolic_depth=8, execution_size=16, ops_per_chan=2, warps_per_cta=warps_per_cta,
+        rep_cluster=calculate_optimal_rep_clusters(m_shape, n_shape, k_shape, threads_per_warp,
+                                                   warps_per_cta), threads_per_warp=threads_per_warp)
+
+
 def get_gluon_matmul_autotune_configs() -> List[triton.Config]:
     configs = [
         triton.Config(
@@ -31,59 +43,6 @@ def get_gluon_matmul_autotune_configs() -> List[triton.Config]:
                 'BLOCK_SIZE_M': 8, 'BLOCK_SIZE_N': 512, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 1, 'grf_mode': m,
                 'NUM_STAGES': s, 'NUM_WARPS': w
             }, num_stages=s, num_warps=w) for s in [2, 3] for (m, w) in ([('256', 32), ('128', 64)])
-    ]
-    return configs
-
-
-@gluon.constexpr_function
-def get_dpas_layout(num_warps: ttgl.constexpr, m_shape: ttgl.constexpr, n_shape: ttgl.constexpr,
-                    k_shape: ttgl.constexpr) -> ttgl.constexpr:
-    threads_per_warp = 16
-    warps_per_cta = calculate_optimal_warps_per_cta(num_warps, m_shape, n_shape)
-
-    return IntelDPASLayout(
-        repeatCount=8, systolic_depth=8, execution_size=16, ops_per_chan=2, warps_per_cta=warps_per_cta,
-        rep_cluster=calculate_optimal_rep_clusters(m_shape, n_shape, k_shape, threads_per_warp,
-                                                   warps_per_cta), threads_per_warp=threads_per_warp)
-
-
-def get_gluon_matmul_batched_autotune_configs() -> List[triton.Config]:
-    configs = [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
-                'NUM_STAGES': s, 'NUM_WARPS': 32
-            }, num_stages=s, num_warps=32) for s in [2, 3]
-    ] + [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': m,
-                'NUM_STAGES': s, 'NUM_WARPS': w
-            }, num_stages=s, num_warps=w) for s in [2] for (m, w) in ([('256', 32), ('128', 64)])
-    ] + [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 1024, 'BLOCK_SIZE_K': 16, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
-                'NUM_STAGES': s, 'NUM_WARPS': 32
-            }, num_stages=s, num_warps=32) for s in [2, 3]
-    ] + [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
-                'NUM_STAGES': s, 'NUM_WARPS': 32
-            }, num_stages=s, num_warps=32) for s in [2]
-    ] + [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 8, 'BLOCK_SIZE_N': 512, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 1, 'grf_mode': '256',
-                'NUM_STAGES': s, 'NUM_WARPS': 32
-            }, num_stages=s, num_warps=32) for s in [2]
-    ] + [
-        triton.Config(
-            {
-                'BLOCK_SIZE_M': 8, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 1, 'grf_mode': '256',
-                'NUM_STAGES': s, 'NUM_WARPS': 4
-            }, num_stages=s, num_warps=4) for s in [2]
     ]
     return configs
 
@@ -128,11 +87,7 @@ def gluon_matmul_kernel_with_tensor_descriptors(
     c_desc = ttgl.intel.make_tensor_descriptor(c_ptr, (M, N), (stride_cm, stride_cn), (BLOCK_SIZE_M, BLOCK_SIZE_N),
                                                layout)
 
-    # Clear accumulator
-    zero_tensor = ttgl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=ttgl.float32, layout=layout)
-    ttgl.intel.xe.store_2d(c_desc, [pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N], zero_tensor)
-
-    accumulator = ttgl.intel.xe.load_2d(c_desc, [pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N])
+    accumulator = ttgl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=ttgl.float32, layout=layout)
 
     # Prefetch first blocks for A and B matrices (pre-loop prefetches)
     for i in range(NUM_STAGES):
@@ -153,6 +108,47 @@ def gluon_matmul_kernel_with_tensor_descriptors(
         accumulator = ttgl.intel.xe.dpas(a, b, accumulator)
 
     ttgl.intel.xe.store_2d(c_desc, [pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N], accumulator)
+
+
+def get_gluon_matmul_batched_autotune_configs() -> List[triton.Config]:
+    configs = [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
+                'NUM_STAGES': s, 'NUM_WARPS': 32
+            }, num_stages=s, num_warps=32) for s in [2, 3]
+    ] + [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': m,
+                'NUM_STAGES': s, 'NUM_WARPS': w
+            }, num_stages=s, num_warps=w) for s in [2] for (m, w) in ([('256', 32), ('128', 64)])
+    ] + [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 1024, 'BLOCK_SIZE_K': 16, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
+                'NUM_STAGES': s, 'NUM_WARPS': 32
+            }, num_stages=s, num_warps=32) for s in [2, 3]
+    ] + [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 4, 'grf_mode': '256',
+                'NUM_STAGES': s, 'NUM_WARPS': 32
+            }, num_stages=s, num_warps=32) for s in [2]
+    ] + [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 8, 'BLOCK_SIZE_N': 512, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 1, 'grf_mode': '256',
+                'NUM_STAGES': s, 'NUM_WARPS': 32
+            }, num_stages=s, num_warps=32) for s in [2]
+    ] + [
+        triton.Config(
+            {
+                'BLOCK_SIZE_M': 8, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 1, 'grf_mode': '256',
+                'NUM_STAGES': s, 'NUM_WARPS': 4
+            }, num_stages=s, num_warps=4) for s in [2]
+    ]
+    return configs
 
 
 @triton.autotune(
@@ -202,11 +198,7 @@ def gluon_matmul_kernel_with_tensor_descriptors_batched(
     c_desc = ttgl.intel.make_tensor_descriptor(c_ptr + offset_c, (M, N), (stride_cm, stride_cn),
                                                (BLOCK_SIZE_M, BLOCK_SIZE_N), layout)
 
-    # Clear accumulator
-    zero_tensor = ttgl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=ttgl.float32, layout=layout)
-    ttgl.intel.xe.store_2d(c_desc, [pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N], zero_tensor)
-
-    accumulator = ttgl.intel.xe.load_2d(c_desc, [pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N])
+    accumulator = ttgl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=ttgl.float32, layout=layout)
 
     # Prefetch first blocks for A and B matrices (pre-loop prefetches)
     for i in range(NUM_STAGES):
