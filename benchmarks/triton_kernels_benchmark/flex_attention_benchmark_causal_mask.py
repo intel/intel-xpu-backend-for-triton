5951697efe6f6055fb6348763c504ed0d14c08ac
Whitney Tsang <whitney.tsang@intel.com>
2025-12-24 21:02:12 +0000

    [FlexAttn] Enable more bwd shapes
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>

--- benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
+++ benchmarks/triton_kernels_benchmark/flex_attention_benchmark_causal_mask.py
@@ -116,9 +116,8 @@ if 'B580' in torch.xpu.get_device_name():
             [h, h, seq_len, seq_len, 128, 128]
             for h in [1, 2, 4, 16, 24, 32]
             for seq_len in [4096, 8192]
-            # FIXME: OutOfMemoryError: XPU out of memory (#5725)
             # FIXME: UR_RESULT_ERROR_DEVICE_LOST on BMG (#5735)
-            if not (h in [1, 16, 24, 32] and seq_len == 8192) and 'B580' not in torch.xpu.get_device_name()
+            if 'B580' not in torch.xpu.get_device_name()
         ] if fa_kernel_mode == 'bwd' else [])],
         line_arg='provider',
         line_vals=['triton', 'torch'],
