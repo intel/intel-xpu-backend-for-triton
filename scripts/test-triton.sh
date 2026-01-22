fa3ad5241ca74243577895db4625eee594c3685e
Egor Krivov <egor.krivov@intel.com>
2026-01-22 14:34:51 +0000

    Enable empty diff

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -667,7 +667,7 @@ run_liger_install() {
   if ! [ -d "./Liger-Kernel" ]; then
     git clone https://github.com/linkedin/Liger-Kernel
     cd Liger-Kernel
-    git apply ../benchmarks/third_party/liger/liger-fix.patch
+    git apply ../benchmarks/third_party/liger/liger-fix.patch --allow-empty
     cd ..
   fi
 
