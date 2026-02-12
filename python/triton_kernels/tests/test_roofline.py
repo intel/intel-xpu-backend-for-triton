1cc07d6fab3945083192aea91785f5c15604d23b
Sławomir Błauciak <slawomir.blauciak@intel.com>
2026-02-12 14:01:41 +0000

    [TEST] XPU BLAS TFLOPS support for roofline

--- python/triton_kernels/tests/test_roofline.py
+++ python/triton_kernels/tests/test_roofline.py
@@ -1,6 +1,6 @@
 import pytest
 from triton_kernels.roofline import get_memset_tbps, get_blas_tflops
-from triton_kernels.target_info import cuda_capability_geq, is_cuda, is_xpu
+from triton_kernels.target_info import cuda_capability_geq, is_cuda
 
 
 def test_get_memset_tbps(device):
@@ -8,10 +8,9 @@ def test_get_memset_tbps(device):
     assert tbps > 0
 
 
-@pytest.mark.skipif(is_xpu(), reason="FIXME: #5723")
 @pytest.mark.parametrize("dtype", ["fp16", "bf16", "fp8"])
-def test_get_blas_tflops(dtype):
+def test_get_blas_tflops(device, dtype):
     if dtype in ["fp8"] and is_cuda() and not cuda_capability_geq(9, 0):
         pytest.skip("FP8 not supported on this GPU")
-    tflops = get_blas_tflops(dtype)
+    tflops = get_blas_tflops(dtype=dtype, device=device)
     assert tflops > 0
