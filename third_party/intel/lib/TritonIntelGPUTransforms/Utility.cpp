7f70a7638105ffca57a7166b9d80211e3adbf51f
Sławomir Błauciak <slawomir.blauciak@intel.com>
2026-02-24 10:38:01 +0000

    Reapply "[BACKEND] Refactor forward layout propagation  (#9463)"
    This reverts commit 6deee6189e77ab21add333b3cffc6a215df83e10.
    Closes #6167

--- third_party/intel/lib/TritonIntelGPUTransforms/Utility.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/Utility.cpp
@@ -279,7 +279,7 @@ LogicalResult getConvertBackwardSlice(
         enqueue(definingOp->getOpOperand(0), encoding);
         continue;
       }
-      if (canFoldIntoConversion(definingOp, encoding))
+      if (canUseResultEncoding(definingOp, encoding))
         continue;
       if (stopPropagation && stopPropagation(definingOp))
         continue;
