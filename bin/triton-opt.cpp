8e83d5ee58c1052971326a64cf008d3b019c4087
Quinn Pham <quinn.pham@intel.com>
2026-02-25 22:38:52 +0000

    formatting

--- bin/triton-opt.cpp
+++ bin/triton-opt.cpp
@@ -2,17 +2,19 @@
 
 #include "mlir/Dialect/SPIRV/IR/SPIRVDialect.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-#include "llvm/Support/Path.h"
 #include "llvm/Support/DynamicLibrary.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Path.h"
 
 int main(int argc, char **argv) {
   if (std::string filename =
-      mlir::triton::tools::getStrEnv("TRITON_PASS_PLUGIN_PATH");
+          mlir::triton::tools::getStrEnv("TRITON_PASS_PLUGIN_PATH");
       !filename.empty()) {
-    auto executablePath = llvm::sys::fs::getMainExecutable(argv[0], (void*)&main);
+    auto executablePath =
+        llvm::sys::fs::getMainExecutable(argv[0], (void *)&main);
     std::string fakeTriton =
-      llvm::sys::path::parent_path(executablePath).str() + "/faketriton/libtriton.so";
+        llvm::sys::path::parent_path(executablePath).str() +
+        "/faketriton/libtriton.so";
     std::string error;
     llvm::sys::DynamicLibrary::getPermanentLibrary(fakeTriton.c_str(), &error);
   }
