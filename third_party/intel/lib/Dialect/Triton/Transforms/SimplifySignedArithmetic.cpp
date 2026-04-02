eb9fe9ebee0d937542c89d9c54cc097692ee8dbd
Whitney Tsang <whitney.tsang@intel.com>
2026-04-02 04:31:06 +0000

    Revert "[SimplifySignedArithmetic] Optimize XOR-based floor division pattern"
    This reverts commit 1d7d2e4ce866bab34ad9e030b5ec4c2c3d9e3cf3.

--- third_party/intel/lib/Dialect/Triton/Transforms/SimplifySignedArithmetic.cpp
+++ third_party/intel/lib/Dialect/Triton/Transforms/SimplifySignedArithmetic.cpp
@@ -16,28 +16,9 @@ namespace mlir::triton::intel {
 
 namespace {
 
-/// Check if a constant is a splat of the given signed integer value.
-static bool isSplatValue(Value value, int64_t expected) {
-  auto constOp = value.getDefiningOp<arith::ConstantOp>();
-  if (!constOp)
-    return false;
-  if (auto intAttr = dyn_cast<IntegerAttr>(constOp.getValue()))
-    return intAttr.getValue().getSExtValue() == expected;
-  if (auto denseAttr = dyn_cast<DenseElementsAttr>(constOp.getValue())) {
-    if (denseAttr.getElementType().isSignlessInteger()) {
-      return denseAttr.isSplat() &&
-             denseAttr.getSplatValue<APInt>().getSExtValue() == expected;
-    }
-  }
-  return false;
-}
-
 class SignedArithmeticSimplifier {
 public:
   void run(ModuleOp moduleOp) {
-    // First, optimize XOR-based floor division patterns.
-    optimizeFloorDivPatterns(moduleOp);
-
     SmallVector<arith::RemSIOp> remOpsToConvert;
     SmallVector<arith::DivSIOp> divOpsToConvert;
 
@@ -202,125 +183,6 @@ private:
       return isPositiveConstant(splatOp.getSrc());
 
     return false;
-  }
-
-  /// Optimize XOR-based floor division patterns.
-  ///
-  /// Detects the pattern:
-  ///   %cond   = arith.cmpi slt, %x, 0
-  ///   %not_x  = arith.xori %x, -1
-  ///   %abs_x  = arith.select %cond, %not_x, %x
-  ///   %quot   = arith.divsi %abs_x, %divisor
-  ///   %not_q  = arith.xori %quot, -1
-  ///   %result = arith.select %cond, %not_q, %quot
-  ///
-  /// When %x is provably non-negative and %divisor is a positive constant,
-  /// the entire pattern simplifies to: %result = arith.divsi %x, %divisor
-  void optimizeFloorDivPatterns(ModuleOp moduleOp) {
-    SmallVector<arith::SelectOp> selectOps;
-    moduleOp.walk(
-        [&](arith::SelectOp selectOp) { selectOps.push_back(selectOp); });
-
-    unsigned count = 0;
-    for (auto outerSelect : selectOps) {
-      // Match: %result = select(%cond, %not_q, %quot)
-      Value cond = outerSelect.getCondition();
-      Value trueVal = outerSelect.getTrueValue();
-      Value falseVal = outerSelect.getFalseValue();
-
-      // %not_q = xori(%quot, -1)
-      auto notQuotOp = trueVal.getDefiningOp<arith::XOrIOp>();
-      if (!notQuotOp)
-        continue;
-
-      // Check which operand of xori is -1
-      Value quotVal;
-      if (isSplatValue(notQuotOp.getRhs(), -1))
-        quotVal = notQuotOp.getLhs();
-      else if (isSplatValue(notQuotOp.getLhs(), -1))
-        quotVal = notQuotOp.getRhs();
-      else
-        continue;
-
-      // falseVal must be the same %quot
-      if (falseVal != quotVal)
-        continue;
-
-      // %quot = divsi(%abs_x, %divisor)
-      auto divOp = quotVal.getDefiningOp<arith::DivSIOp>();
-      if (!divOp)
-        continue;
-
-      Value absX = divOp.getLhs();
-      Value divisor = divOp.getRhs();
-
-      // %abs_x = select(%same_cond, %not_x, %x)
-      auto innerSelect = absX.getDefiningOp<arith::SelectOp>();
-      if (!innerSelect || innerSelect.getCondition() != cond)
-        continue;
-
-      Value innerTrue = innerSelect.getTrueValue();
-      Value innerFalse = innerSelect.getFalseValue();
-
-      // %not_x = xori(%x, -1)
-      auto notXOp = innerTrue.getDefiningOp<arith::XOrIOp>();
-      if (!notXOp)
-        continue;
-
-      Value x;
-      if (isSplatValue(notXOp.getRhs(), -1))
-        x = notXOp.getLhs();
-      else if (isSplatValue(notXOp.getLhs(), -1))
-        x = notXOp.getRhs();
-      else
-        continue;
-
-      // innerFalse must be the same %x
-      if (innerFalse != x)
-        continue;
-
-      // %cond = cmpi slt, %x, 0
-      auto cmpOp = cond.getDefiningOp<arith::CmpIOp>();
-      if (!cmpOp || cmpOp.getPredicate() != arith::CmpIPredicate::slt)
-        continue;
-      if (cmpOp.getLhs() != x || !isSplatValue(cmpOp.getRhs(), 0))
-        continue;
-
-      // Full pattern matched! Now check safety conditions.
-      if (!isNonNegative(x) || !isPositiveConstant(divisor))
-        continue;
-
-      // Safe to replace: floordiv(x, d) == truncdiv(x, d) when x >= 0, d > 0
-      LLVM_DEBUG(llvm::dbgs() << "Replacing XOR floor division with divsi: "
-                              << outerSelect << "\n");
-
-      OpBuilder builder(outerSelect);
-      auto newDiv =
-          arith::DivSIOp::create(builder, outerSelect.getLoc(), x, divisor);
-      outerSelect.replaceAllUsesWith(newDiv.getResult());
-
-      // Erase dead ops in reverse dependency order.
-      if (outerSelect->use_empty())
-        outerSelect.erase();
-      if (notQuotOp->use_empty())
-        notQuotOp.erase();
-      if (divOp->use_empty())
-        divOp.erase();
-      if (innerSelect->use_empty())
-        innerSelect.erase();
-      if (notXOp->use_empty())
-        notXOp.erase();
-      if (cmpOp->use_empty())
-        cmpOp.erase();
-
-      ++count;
-    }
-
-    LLVM_DEBUG({
-      if (count > 0)
-        llvm::dbgs() << "Replaced " << count
-                     << " XOR floor division pattern(s)\n";
-    });
   }
 };
 
