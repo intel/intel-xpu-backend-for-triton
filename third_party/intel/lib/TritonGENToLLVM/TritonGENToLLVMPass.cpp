09b048cf4e1b945d1b2be14e0cf4a741b531ea48
Lu,Chengjun <chengjun.lu@intel.com>
2026-04-02 15:33:45 +0000

    Temp add column major B matrix prefetching.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/lib/TritonGENToLLVM/TritonGENToLLVMPass.cpp
+++ third_party/intel/lib/TritonGENToLLVM/TritonGENToLLVMPass.cpp
@@ -212,6 +212,11 @@ static bool isSPVBuiltinAvailableImpl(TritonGEN::Matrix2DBlockPrefetchOp op) {
       op.getVBlocks() == 1)
     return false;
 
+  // intel_sub_group_2d_block_prefetch_8b_?r16x1c
+  if (op.getElemSizeInBits() == 8 && op.getTileWidth() == 16 &&
+      op.getVBlocks() == 1)
+    return false;
+
   return true;
 }
 
