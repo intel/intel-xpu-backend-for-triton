f82d798e1e533388bdb2dc9eb1bfc62d8e3da566
Tiotto, Ettore <ettore.tiotto@intel.com>
2026-06-18 21:40:58 +0000

    [TritonIntelGPU] Add CanonicalizePointers pass (ported from AMD)
    Decompose loop-carried tensor<Nx!tt.ptr> iter_args into scalar base +
    tensor offset to reduce GRF pressure. Enabled by default
    (TRITON_INTEL_DISABLE_CANONICALIZE_POINTERS=1 to disable).

--- benchmarks/micro_benchmarks/run_benchmarks.py
+++ benchmarks/micro_benchmarks/run_benchmarks.py
@@ -1,7 +1,8 @@
 from conversion import float_conversion
 from core_ops import dot_scaled
 from core_ops import descriptor_load
+from core_ops import canonicalize_pointers
 
 if __name__ == '__main__':
-    for mod in (float_conversion, dot_scaled, descriptor_load):
+    for mod in (float_conversion, dot_scaled, descriptor_load, canonicalize_pointers):
         mod.run_benchmarks()
