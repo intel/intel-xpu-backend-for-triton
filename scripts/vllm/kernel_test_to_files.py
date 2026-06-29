df2a7f330a96a5624ffb098b00ab4e1008ec1117
Quinn Pham <quinn.pham@intel.com>
2026-06-29 21:20:50 +0000

    pre-commit

--- scripts/vllm/kernel_test_to_files.py
+++ scripts/vllm/kernel_test_to_files.py
@@ -8,10 +8,10 @@ import sys
 if len(sys.argv) != 3:
     sys.exit(f"usage: {sys.argv[0]} INPUT OUTPUT")
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], encoding="utf-8") as f:
     data = json.load(f)
 
-with open(sys.argv[2], "w") as f:
+with open(sys.argv[2], "w", encoding="utf-8") as f:
     json.dump(
         {kernel: sorted({test.split("::", 1)[0]
                          for test in tests})
