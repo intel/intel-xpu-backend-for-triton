26ec35c96fc5f0e63bca971ac3330b1e1feeedc5
Quinn Pham <quinn.pham@intel.com>
2026-07-02 14:50:55 +0000

    moving to moe

--- scripts/test-triton.sh
+++ scripts/test-triton.sh
@@ -983,7 +983,8 @@ run_vllm_moe_tests() {
       tests/kernels/moe/test_triton_moe_ptpc_fp8.py \
       tests/kernels/moe/test_silu_mul_fp8_quant_deep_gemm.py \
       tests/kernels/moe/test_batched_deepgemm.py \
-      tests/kernels/moe/test_gpt_oss_triton_kernels.py
+      tests/kernels/moe/test_gpt_oss_triton_kernels.py \
+      tests/kernels/moe/test_silu_mul_per_token_group_quant_fp8_colmajor.py
 }
 
 
@@ -1063,8 +1064,7 @@ run_vllm_quant_tests() {
       tests/kernels/quantization/test_block_int8.py \
       tests/kernels/quantization/test_fp8_quant.py \
       tests/kernels/quantization/test_fp8_quant_group.py \
-      tests/kernels/quantization/test_block_fp8.py \
-      tests/kernels/moe/test_silu_mul_per_token_group_quant_fp8_colmajor.py
+      tests/kernels/quantization/test_block_fp8.py
 }
 
 
