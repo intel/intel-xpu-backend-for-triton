eb1054f2a1c9ce42914d5e4d9eeec92ec5355e91
Lu,Chengjun <chengjun.lu@intel.com>
2026-07-07 10:06:40 +0000

    Address review comments.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/triton_xpu.cc
+++ third_party/intel/triton_xpu.cc
@@ -170,7 +170,7 @@ void init_triton_intel_passes_ttgpuir(py::module_ &&m) {
   ADD_PASS_WRAPPER_0("add_annotate_cache_control",
                      gpu::intel::createTritonIntelGPUAnnotateCacheControl);
   ADD_PASS_WRAPPER_0("add_widen_load_store_encoding",
-                      gpu::intel::createTritonIntelGPUWidenLoadStoreEncoding);
+                     gpu::intel::createTritonIntelGPUWidenLoadStoreEncoding);
   ADD_PASS_WRAPPER_0(
       "add_rewrite_tensor_descriptor_gather_scatter",
       gpu::intel::createTritonIntelGPURewriteDescriptorGatherScatter);
