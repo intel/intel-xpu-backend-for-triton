646d1cf754822dd767a46300df53dc3c99565b46
Lu,Chengjun <chengjun.lu@intel.com>
2026-07-23 13:15:51 +0000

    Address review comments
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

---
+++ third_party/intel/backend/arch_parser.c
@@ -0,0 +1,77 @@
+//===- arch_parser.c ------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <iostream>
+
+#include <sycl/sycl.hpp>
+
+#if defined(_WIN32)
+#define EXPORT_FUNC __declspec(dllexport)
+#else
+#define EXPORT_FUNC __attribute__((visibility("default")))
+#endif
+
+extern "C" EXPORT_FUNC const char *parse_device_arch(uint64_t dev_arch) {
+  sycl::ext::oneapi::experimental::architecture sycl_arch =
+      static_cast<sycl::ext::oneapi::experimental::architecture>(dev_arch);
+  // FIXME: Add support for more architectures.
+  const char *arch = "";
+  switch (sycl_arch) {
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_arl_h:
+    arch = "arl_h";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_arl_s:
+    arch = "arl_s";
+    break;
+#if __SYCL_COMPILER_VERSION >= 20251010
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_bmg_g31:
+#endif
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_bmg_g21:
+    arch = "bmg";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_dg2_g10:
+    arch = "dg2";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_lnl_m:
+    arch = "lnl";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_mtl_h:
+    arch = "mtl";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_pvc:
+    arch = "pvc";
+    break;
+#if __SYCL_COMPILER_VERSION >= 20250000
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_ptl_h:
+    arch = "ptl_h";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_ptl_u:
+    arch = "ptl_u";
+    break;
+#endif
+#if __SYCL_COMPILER_VERSION >= 20260331
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_nvl_s:
+    arch = "nvl_s";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_nvl_u:
+    arch = "nvl_u";
+    break;
+  case sycl::ext::oneapi::experimental::architecture::intel_gpu_nvl_p:
+    arch = "nvl_p";
+    break;
+#endif
+  case sycl::ext::oneapi::experimental::architecture::unknown:
+    arch = "unknown";
+    break;
+  default:
+    std::cerr << "device architecture not recognized: " << (uint64_t)sycl_arch
+              << std::endl;
+  }
+
+  return arch;
+}
--- third_party/intel/backend/compiler.py
+++ third_party/intel/backend/compiler.py
@@ -1,6 +1,6 @@
 from triton.backends.compiler import BaseBackend, GPUTarget, Language
 from triton._C.libtriton import ir, passes, llvm, intel
-from triton.backends.intel.driver import get_spirv_utils_module, is_lts
+from triton.backends.intel.driver import get_arch_utils_module, is_lts
 from triton.backends.intel.track import track
 from triton.backends.intel.extension_utils import query_device_extensions
 from triton import knobs
@@ -144,7 +144,7 @@ class XPUBackend(BaseBackend, metaclass=XPUBackendMeta):
         if (impl := cls.arch_to_impl.get(arch, None)) is None:
             # Try to find an arch-specific implementation in the .arch.<name> submodule.
             if not (dev_arch := knobs.intel.device_arch):
-                dev_arch = get_spirv_utils_module().parse_device_arch(target.arch.get('architecture', 0))
+                dev_arch = get_arch_utils_module().parse_device_arch(target.arch.get('architecture', 0))
             mod_name = f"{__package__}.arch.{dev_arch}"
             try:
                 impl = __import__(mod_name, fromlist=["XPUBackendImpl"]).XPUBackendImpl
